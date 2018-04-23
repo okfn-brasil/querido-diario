157d4673f5fc67163e347fe76c6628ff19eb73a3
Bruno Lellis <brunolellis@gmail.com>
2018-04-22 23:35:31 -0300

    changing power to executive

--- processing/data_collection/gazette/spiders/sp_franca.py
+++ processing/data_collection/gazette/spiders/sp_franca.py
@@ -50,7 +50,7 @@ class SpFrancaSpider(scrapy.Spider):
                 is_extra_edition=False,
                 municipality_id=self.MUNICIPALITY_ID,
                 scraped_at=dt.datetime.utcnow(),
-                power='executive_legislature'
+                power='executive'
             )
         )
 
