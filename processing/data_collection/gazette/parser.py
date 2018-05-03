e3692e1f751d37ec024b57045102cbd5a25918f1
Bruno Lellis <brunolellis@gmail.com>
2018-05-03 18:07:34 -0300

    polish

--- processing/data_collection/gazette/parser.py
+++ processing/data_collection/gazette/parser.py
@@ -9,7 +9,7 @@ class GazetteFilesPipeline(FilesPipeline):
         url = request.url
         media_guid = hashlib.sha1(to_bytes(url)).hexdigest()
         media_ext = os.path.splitext(url)[1]
+        # scrapy bug. for more details see issue #15
         if not media_ext.isalnum():
-            # scrapy bug. for more details see issue #15
             media_ext = os.path.splitext(urlparse(url).path)[1]
         return 'full/%s%s' % (media_guid, media_ext)
