21b1756ccc6ea3b565ef37da6302610e0f580ad4
Júnior Carvalho <joseadolfojr@gmail.com>
2018-05-08 12:57:07 -0300

    mg_uberlandia initial (WP)

--- processing/data_collection/gazette/spiders/mg_uberlandia.py
+++ processing/data_collection/gazette/spiders/mg_uberlandia.py
@@ -18,26 +18,26 @@ class MgUberlandiaSpider(scrapy.Spider):
     def parse(self, response):
 
         for url in self.urls:
-            yield scrapy.Request(url, self.parse_ano)
+            yield scrapy.Request(url, self.parse_year)
 
     
-    def parse_ano(self, response):
-        url_meses = response.xpath('//a[contains(@target,"_blank") and contains(@href,"http://www.uberlandia.mg.gov.br")]')
-        for url in url_meses:
-            url_mes = url.xpath('@href').extract_first()
-            yield scrapy.Request(url_mes, self.parse_mes)
+    def parse_year(self, response):
+        url_months = response.xpath('//a[contains(@target,"_blank") and contains(@href,"http://www.uberlandia.mg.gov.br")]')
+        for url in url_months:
+            url_month = url.xpath('@href').extract_first()
+            yield scrapy.Request(url_month, self.parse_month)
             
 
-    def parse_mes(self,response):
-        url_edicoes = response.xpath('//a[contains(@href,"http://www.uberlandia.mg.gov.br/uploads")]')
+    def parse_month(self,response):
+        url_issues = response.xpath('//a[contains(@href,"http://www.uberlandia.mg.gov.br/uploads")]')
         items = []
-        for url in url_edicoes:
-            url_edicao = url.xpath('@href').extract_first()
+        for url in url_issues:
+            url_issue = url.xpath('@href').extract_first()
             power = 'executive_legislature'
             items.append(
                 Gazette(
                     date=dt.date(2018, 5, 5), # temporário
-                    file_urls=[url_edicao],
+                    file_urls=[url_issue],
                     is_extra_edition=False,
                     municipality_id=self.MUNICIPALITY_ID,
                     power=power,
