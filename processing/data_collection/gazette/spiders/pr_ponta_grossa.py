5bb70531d1c493bc0ae217d7cd82f57d8394b5d0
antoniovendramin <drjedison@gmail.com>
2018-05-18 20:04:24 -0300

    Get the correct file name

--- processing/data_collection/gazette/spiders/pr_ponta_grossa.py
+++ processing/data_collection/gazette/spiders/pr_ponta_grossa.py
@@ -21,28 +21,30 @@ class PrPontaGrossaSpider(scrapy.Spider):
         self.scrape_page(response)
 
     def scrape_page(self, response):
-        pdf_links = response.css(".view-content .field a").extract()
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        pdf_links = response.css(".view-content .field a")
 
         pdf_infos = []
-        current_page = response.page-current
         for pdf_link in pdf_links:
-            pdf_file_name = pdf_link.css("::attr(href)")
-            pdf_link_text = pdf_link.css("::text")
+            pdf_file_name = pdf_link.css("::attr(href)").extract_first()
+            pdf_link_text = pdf_link.css("::text").extract_first()
+            print(pdf_file_name)
             if "sem_atos" in pdf_file_name:
                 continue
             pdf_link_info = re.search('.*/diario-oficial/_?(\d{4})-(\d{2})-(\d{2}).*.pdf', pdf_file_name)
-            ano = pdf_link_info.group(1)
-            if ano < ano_minimo:
+            ano = int(pdf_link_info.group(1))
+            if ano < self.ano_minimo:
                 continue
             mes = pdf_link_info.group(2)
             dia = pdf_link_info.group(3)
-            is_extra = True#Dapaende do texto 
-            pdf_infos.append({ "ano" : ano, "mes" : mes, "dia":dia })
+        #is_extra = True#Dapaende do texto
+            pdf_infos.append({ "ano" : ano, "mes" : mes, "dia":dia, "url": pdf_file_name })
         menor_ano_da_pagina =  min(map(lambda p: p.ano, pdf_infos))
-        if menor_ano_da_pagina >= self.ano_minimo:
-            next_page_url = response.css(".pager-next::attr(href)").extract_first()
-            yield scrapy.Request(next_page_url, self.scrape_page)
+        print(menor_ano_da_pagina)
+        #if menor_ano_da_pagina >= self.ano_minimo:
+        #    next_page_url = response.css(".pager-next::attr(href)").extract_first()
+        #    yield scrapy.Request(next_page_url, self.scrape_page)
 
-        for pdf_info in pdf_infos:
-            yield None
+        #for pdf_info in pdf_infos:
+        #    print(pdf_info.url)
 
