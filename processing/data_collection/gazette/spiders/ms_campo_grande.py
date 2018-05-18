00a7893d494337c466f537e64a2a5904ac68d452
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2018-05-18 19:14:05 -0300

    Implementing changes suggested by @rennerocha

--- processing/data_collection/gazette/spiders/ms_campo_grande.py
+++ processing/data_collection/gazette/spiders/ms_campo_grande.py
@@ -11,14 +11,8 @@ class MsCampoGrandeSpider(scrapy.Spider):
     MUNICIPALITY_ID = '5002704'
     name = 'ms_campo_grande'
     allowed_domains = ['portal.capital.ms.gov.br']
-    start_urls = ['http://portal.capital.ms.gov.br/diogrande/diarioOficial']
-    ms_campo_grande_url = 'http://portal.capital.ms.gov.br/diogrande/diarioOficial'
 
-    def parse(self, response):
-        """
-        @url http://portal.capital.ms.gov.br/diogrande/diarioOficial
-        @returns requests 4
-        """
+    def start_requests(self):
         today = dt.date.today()
         next_year = today.year + 1
         for year in range(2015, next_year):
@@ -27,8 +21,7 @@ class MsCampoGrandeSpider(scrapy.Spider):
                     return
 
                 yield scrapy.FormRequest(
-                    url=self.ms_campo_grande_url,
-                    callback=self.parse_month_page,
+                    url='http://portal.capital.ms.gov.br/diogrande/diarioOficial',
                     method='POST',
                     formdata={
                         'mes': str(month),
@@ -36,30 +29,27 @@ class MsCampoGrandeSpider(scrapy.Spider):
                     }
                 )
 
-    def parse_month_page(self, response):
+    def parse(self, response):
         """
         @url http://portal.capital.ms.gov.br/diogrande/diarioOficial
         @returns items 1
         @scrapes date file_urls is_extra_edition municipality_id power scraped_at
         """
-        items = []
-        year = response.css('#leftToRight > h3').extract_first().split('/')[1]  # "2018"
+        year = response.css('#leftToRight > h3').extract_first().split('/')[1]
         docs = response.css(".arquivos li")
         for doc in docs:
             url = doc.css('.inner-detail a::attr(href)').extract_first()
             day = doc.css('.day strong::text').extract_first()
             month = doc.css('.month::text').extract_first()
             date = parse(f'{day}/{month}/{year}', languages=['pt']).date()
-            is_extra_edition = 'Edição Extra' in doc.css('.inner-detail a p:last-child::text')
+            doc_title = doc.css('.inner-detail::text').extract_first().lower()
+            is_extra_edition = 'extra' in doc_title or 'suplemento' in doc_title
             power = 'executive_legislature'
-            items.append(
-                Gazette(
-                    date=date,
-                    file_urls=[url],
-                    is_extra_edition=is_extra_edition,
-                    municipality_id=self.MUNICIPALITY_ID,
-                    power=power,
-                    scraped_at=dt.datetime.utcnow(),
-                )
+            yield Gazette(
+                date=date,
+                file_urls=[url],
+                is_extra_edition=is_extra_edition,
+                municipality_id=self.MUNICIPALITY_ID,
+                power=power,
+                scraped_at=dt.datetime.utcnow(),
             )
-        return items
