2ebba54e786125aedb9be041827cc43f085e591b
Renne Rocha <renne@rennerocha.com>
2018-05-22 17:33:17 -0300

    Include item pipeline that tries to identify extra editions

--- processing/data_collection/gazette/spiders/ba_salvador.py
+++ processing/data_collection/gazette/spiders/ba_salvador.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
+import re
 import urllib.parse
 
 import dateparser
 import scrapy
 
 from gazette.items import Gazette
+from gazette.settings import ITEM_PIPELINES
 from gazette.spiders.base import BaseGazetteSpider
 
 
@@ -14,6 +16,13 @@ class BaSalvadorSpider(BaseGazetteSpider):
     name = 'ba_salvador'
     allowed_domains = ['salvador.ba.gov.br']
     power = 'executive'
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            **ITEM_PIPELINES,
+            'gazette.spiders.ba_salvador.BaSalvadorExtraEditionItemPipeline': 1000,
+        }
+    }
 
     def start_requests(self):
         # According to their website, they have gazettes available from 2001-01-01
@@ -53,8 +62,6 @@ class BaSalvadorSpider(BaseGazetteSpider):
         parsed_date = dateparser.parse(
             response.meta.get('gazette_date'), settings={'DATE_ORDER': 'YMD'})
         pdf_url = response.css('#PDFId embed::attr(src)').extract_first()
-        if not pdf_url:
-            import ipdb; ipdb.set_trace()
 
         yield Gazette(
             date=parsed_date.date(),
@@ -63,3 +70,20 @@ class BaSalvadorSpider(BaseGazetteSpider):
             power=self.power,
             scraped_at=datetime.datetime.utcnow(),
         )
+
+
+class BaSalvadorExtraEditionItemPipeline(object):
+
+    def process_item(self, item, spider):
+        item['is_extra_edition'] = False
+
+        # The only indication if the gazette is an extra edition is a
+        # red strip over the first page of the PDF with the text 'EDIÇÃO EXTRA'
+        # Unfortunately pdftotext is unable to get the whole word and after some
+        # tests comparing some gazettes, this regex seems to be enough to identify an
+        # extra edition. But I can't guarantee this is 100% accurate. Maybe older
+        # editions has a different pattern.
+        if re.findall('T[\s]+RA', item.get('source_text', '')):
+            item['is_extra_edition'] = True
+
+        return item
