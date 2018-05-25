89afeee1e33a2dc7c69277eca9a41677ad3515ba
D <dannnylo@gmail.com>
2018-05-25 16:02:29 -0300

    Refac class

--- processing/data_collection/gazette/spiders/ce_fortaleza.py
+++ processing/data_collection/gazette/spiders/ce_fortaleza.py
@@ -5,43 +5,27 @@ from datetime import datetime
 from scrapy import Request, Spider
 from gazette.items import Gazette
 
-class GazetteElement:
-    MUNICIPALITY_ID = '2304400'
-    PDF_URL = 'http://apps.fortaleza.ce.gov.br/diariooficial/{}'
+PDF_URL = 'http://apps.fortaleza.ce.gov.br/diariooficial/{}'
 
-    def __init__(self, element):
-        self.element = element
+def generate_year_urls():
+    urls = []
+    for year in range(2015, datetime.now().year):
+        urls.append(PDF_URL.format('?num-diario=&content-diario=&ano-diario=' + str(year) + '&mes-diario=todos&current=1'))
 
-    def date(self):
-        date_str = self.element.css('td:nth-child(2)::text').extract_first()
-        return dateparser.parse(date_str, languages=['pt']).date()
-
-    def url(self):
-        path = self.element.css('a::attr(href)').extract_first()
-        return self.PDF_URL.format(path)
-
-    def to_gazette(self):
-        return Gazette(
-            date=self.date(),
-            file_urls=[self.url()],
-            is_extra_edition=False,
-            municipality_id=self.MUNICIPALITY_ID,
-            power='executive_legislature',
-            scraped_at=datetime.utcnow(),
-        )
-
+    return urls
 
 class CeFortalezaSpider(Spider):
+    DATE_CSS = 'td:nth-child(2)::text'
     GAZETTE_ELEMENT_CSS = '.diarios-oficiais .table-responsive tbody tr'
     NEXT_PAGE_CSS = 'ul.pagination .page-link'
+    NEXT_PAGE_LINK_CSS = '.page-link::attr(href)'
+
+    MUNICIPALITY_ID = '2304400'
 
     allowed_domains = ['apps.fortaleza.ce.gov.br']
     name = 'ce_fortaleza'
 
-    start_urls = []
-
-    for year in range(2015, datetime.now().year):
-        start_urls.append('http://apps.fortaleza.ce.gov.br/diariooficial/?num-diario=&content-diario=&ano-diario=' + str(year) + '&mes-diario=todos&current=1')
+    start_urls = generate_year_urls()
 
     def parse(self, response):
         """
@@ -51,11 +35,29 @@ class CeFortalezaSpider(Spider):
         """
 
         for element in response.css(self.GAZETTE_ELEMENT_CSS):
-           yield GazetteElement(element).to_gazette()
+            url = self.extract_url(element)
+            date = self.extract_date(element)
+
+            yield Gazette(
+                date=date,
+                file_urls=[url],
+                is_extra_edition=False,
+                municipality_id=self.MUNICIPALITY_ID,
+                power='executive_legislature',
+                scraped_at=datetime.utcnow(),
+            )
 
         next_link = response.css(self.NEXT_PAGE_CSS)
 
         if next_link:
-            page_number = next_link[-1].css('.page-link::attr(href)').extract_first().split('#')[1]
-            url = response.url.split('current')[0] + '&current=' + str(page_number)
+            page_number = next_link[-1].css(self.NEXT_PAGE_LINK_CSS).extract_first().split('#')[1]
+            url = response.url.split('&current')[0] + '&current=' + str(page_number)
             yield Request(url)
+
+    def extract_url(self, element):
+        path = element.css('a::attr(href)').extract_first()
+        return PDF_URL.format(path)
+
+    def extract_date(self, element):
+        date_str = element.css(self.DATE_CSS).extract_first()
+        return dateparser.parse(date_str, languages=['pt']).date()
