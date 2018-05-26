f916e2e797b263dcd336fd12680fb8cda9af757d
Vinícius Aguiar Figueiredo <vinoba_@hotmail.com>
2018-05-26 15:39:28 -0300

    PEP8ify file

--- processing/data_collection/gazette/spiders/rj_niteroi.py
+++ processing/data_collection/gazette/spiders/rj_niteroi.py
@@ -9,7 +9,8 @@ class RjNiteroiSpider(scrapy.Spider):
     allowed_domains = ['niteroi.rj.gov.br']
     start_urls = ['http://www.niteroi.rj.gov.br']
     download_url = 'http://www.niteroi.rj.gov.br/downloads/do/{}/{}/{:02d}'
-    month_names = ['01_Jan','02_Fev','03_Mar','04_Abr','05_Mai','06_Jun','07_Jul','08_Ago','09_Set','10_Out','11_Nov','12_Dez']
+    month_names = ['01_Jan', '02_Fev', '03_Mar', '04_Abr', '05_Mai',
+                   '06_Jun', '07_Jul', '08_Ago', '09_Set', '10_Out', '11_Nov', '12_Dez']
 
     def parse(self, response):
         """
@@ -21,7 +22,8 @@ class RjNiteroiSpider(scrapy.Spider):
         parsing_date = dt.date.today()
         while parsing_date >= start_date:
             month = self.month_names[parsing_date.month-1]
-            url = self.download_url.format(parsing_date.year, month, parsing_date.day)
+            url = self.download_url.format(
+                parsing_date.year, month, parsing_date.day)
             yield Gazette(
                 date=parsing_date,
                 file_urls=[url],
