a7dd37845765a6eb7aa78a2720d68dca7851ab09
Chico Venancio <chicocvenancio@gmail.com>
2018-05-27 20:40:16 -0300

    start spider with splash

--- processing/data_collection/gazette/settings.py
+++ processing/data_collection/gazette/settings.py
@@ -10,3 +10,16 @@ ITEM_PIPELINES = {
     'gazette.pipelines.PostgreSQLPipeline': 300,
 }
 FILES_STORE = '/mnt/data/'
+# Splash settings
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
+SPLASH_URL = 'http://splash:8050'
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
