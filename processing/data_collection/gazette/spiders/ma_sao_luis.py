ee6b08fe976333eb4ae41971e9d5b0d92a9bad81
Chico Venancio <chicocvenancio@gmail.com>
2018-05-30 22:20:45 -0300

    refactoring to remove start_requests

--- processing/data_collection/gazette/spiders/ma_sao_luis.py
+++ processing/data_collection/gazette/spiders/ma_sao_luis.py
@@ -14,31 +14,6 @@ class MaSaoLuisSpider(BaseGazetteSpider):
     allowed_domains = ['www.semad.saoluis.ma.gov.br']
     start_urls = ['http://www.semad.saoluis.ma.gov.br:8090/easysearch/']
 
-    def start_requests(self):
-        lua_script = """
-function main(splash, args)
-    splash:init_cookies(splash.args.cookies)
-    splash:go(args.url)
-    getElementByXPath = splash:jsfunc([[
-        function (path) {
-            return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        }
-    ]])
-    splash:wait(12)
-    tamanho_pagina = getElementByXPath("//tbody/tr/td/div[@class='GPFMNGWKN' and text()='20']")
-    tamanho_pagina:mouse_click()
-    splash:wait(3)
-    tamanho_1 = getElementByXPath("//div[1]/div/span[@class='GPFMNGWKGC']")
-    tamanho_1:mouse_click()
-    splash:wait(4)
-    return {html = splash:html(), cookies = splash:get_cookies()}
-end
-"""
-        for url in self.start_urls:
-            yield SplashRequest(
-                url=url, callback=self.parse, endpoint='execute',
-                args={'lua_source': lua_script, 'timeout': 90})
-
     def parse(self, response):
         """
         @url
@@ -46,28 +21,6 @@ end
         @scrapes date file_urls is_extra_edition
                  municipality_id power scraped_at
         """
-        # dates with gazettes available inside the following hidden textarea:
-        date = response.xpath(("//body/div/div/div/div/div/div/div/div/div"
-                               "/div/div/div/div/div/"
-                               "table/tbody/tr[1]/td[2]/div/text()"))
-        date = date.extract_first()
-        date = dateparser.parse(date)
-        if date.year < 2015:
-            print('ano de 2015, parando. data: {}'.format(date))
-            raise StopIteration
-
-        url = response.xpath(("//tbody/tr[4]/td[1]/a"
-                              "[@class='campoResultadoDownload']"
-                              "/@href")).extract_first()
-        url = response.url[:39] + url
-        yield Gazette(
-            date=date,
-            file_urls=[url],
-            is_extra_edition=False,
-            municipality_id=self.MUNICIPALITY_ID,
-            scraped_at=datetime.utcnow(),
-            power='executive'
-        )
         lua_script = """
 function main(splash, args)
     splash:init_cookies(splash.args.cookies)
@@ -92,8 +45,31 @@ function main(splash, args)
     return {html = splash.html(), cookies = splash:get_cookies()}
 end
 """
-        for page_number in range(2, 7000):
+        for page_number in range(1, 1300):
             yield SplashRequest(
                 url=response.url, callback=self.parse, endpoint='execute',
                 args={'lua_source': lua_script, 'timeout': 90,
-                      'page_number': str(page_number)})
+                      'page_number': str(page_number)}
+                )
+
+        date = response.xpath(("//body/div/div/div/div/div/div/div/div/div"
+                               "/div/div/div/div/div/"
+                               "table/tbody/tr[1]/td[2]/div/text()"))
+        date = date.extract_first()
+        date = dateparser.parse(date)
+        if date.year < 2015:
+            print('ano de 2015, parando. data: {}'.format(date))
+            raise StopIteration
+
+        url = response.xpath(("//tbody/tr[4]/td[1]/a"
+                              "[@class='campoResultadoDownload']"
+                              "/@href")).extract_first()
+        url = response.url[:39] + url
+        yield Gazette(
+            date=date,
+            file_urls=[url],
+            is_extra_edition=False,
+            municipality_id=self.MUNICIPALITY_ID,
+            scraped_at=datetime.utcnow(),
+            power='executive'
+        )
