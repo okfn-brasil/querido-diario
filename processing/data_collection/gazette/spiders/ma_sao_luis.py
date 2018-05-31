52bad15861a941a8b12224638d526a750a9f51ad
Chico Venancio <chicocvenancio@gmail.com>
2018-05-31 13:36:38 -0300

    use default page size

--- processing/data_collection/gazette/spiders/ma_sao_luis.py
+++ processing/data_collection/gazette/spiders/ma_sao_luis.py
@@ -30,46 +30,45 @@ function main(splash, args)
             return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         }
     ]])
-    splash:wait(18)
+    splash:wait(28)
+    --[[
     tamanho_pagina = getElementByXPath("//tbody/tr/td/div[@class='GPFMNGWKN' and text()='20']")
     tamanho_pagina:mouse_click()
     splash:wait(3)
     tamanho_1 = getElementByXPath("//div[1]/div/span[@class='GPFMNGWKGC']")
     tamanho_1:mouse_click()
     splash:wait(6)
-    page_box = getElementByXPath("//div[@class='GPFMNGWNP']/input[@class='gwt-TextBox GPFMNGWOJ GPFMNGWIK']")
-    page_box:send_keys("<Delete>")
-    page_box:send_text(args.page_number)
-    page_box:send_keys("<Return>")
-    splash:wait(6)
-    return {html = splash.html(), cookies = splash:get_cookies()}
+    --]]
+    page_input_box = getElementByXPath("//div[@class='GPFMNGWNP']/input[@class='gwt-TextBox GPFMNGWOJ GPFMNGWIK']")
+    page_input_box:send_keys("<Delete>")
+    page_input_box:send_text(args.page_number)
+    page_input_box:send_keys("<Return>")
+    splash:wait(24)
+    return {html = splash.html(), cookies = splash:get_cookies(), png = splash.png()}
 end
 """
-        for page_number in range(1, 1300):
+        for page_number in range(1, 60):
             yield SplashRequest(
                 url=response.url, callback=self.parse, endpoint='execute',
                 args={'lua_source': lua_script, 'timeout': 90,
                       'page_number': str(page_number)}
                 )
 
-        date = response.xpath(("//body/div/div/div/div/div/div/div/div/div"
-                               "/div/div/div/div/div/"
-                               "table/tbody/tr[1]/td[2]/div/text()"))
-        date = date.extract_first()
-        date = dateparser.parse(date)
-        if date.year < 2015:
-            print('ano de 2015, parando. data: {}'.format(date))
-            raise StopIteration
-
-        url = response.xpath(("//tbody/tr[4]/td[1]/a"
-                              "[@class='campoResultadoDownload']"
-                              "/@href")).extract_first()
-        url = response.url[:39] + url
-        yield Gazette(
-            date=date,
-            file_urls=[url],
-            is_extra_edition=False,
-            municipality_id=self.MUNICIPALITY_ID,
-            scraped_at=datetime.utcnow(),
-            power='executive'
-        )
+            for gazzete_table in response.xpath(
+                    "//table[contains(@class, 'tabelaResultado')]"):
+                date = gazzete_table.xpath(
+                        ".//tbody/tr[1]/td[2]/div/text()").extract_first()
+                date = dateparser.parse(date)
+                if date.year < 2015:
+                    print('ano de 2015, parando. data: {}'.format(date))
+                    raise StopIteration
+                url = gazzete_table.xpath((".//tbody/tr[4]/td[1]/a"
+                                        "[@class='campoResultadoDownload']"
+                                        "/@href")).extract_first()
+                url = response.url[:39] + url
+                yield Gazette(
+                    date=date, file_urls=[url],
+                    is_extra_edition=False,
+                    municipality_id=self.MUNICIPALITY_ID,
+                    scraped_at=datetime.utcnow(), power='executive'
+                )
