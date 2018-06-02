6ceac5fbbdd36b8e761637b76dbf01133b2b1f54
Alan Fachini <alfakini@gmail.com>
2018-06-02 16:20:21 -0300

    black .

--- processing/data_collection/gazette/spiders/df_brasilia.py
+++ processing/data_collection/gazette/spiders/df_brasilia.py
@@ -10,48 +10,49 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class DfBrasiliaSpider(BaseGazetteSpider):
-    TERRITORY_ID = '5300108'
-    GAZETTE_URL = 'http://dodf.df.gov.br/listar'
+    TERRITORY_ID = "5300108"
+    GAZETTE_URL = "http://dodf.df.gov.br/listar"
 
     MONTHS = {
-        '12': '12_Dezembro',
-        '11': '11_Novembro',
-        '10': '10_Outubro',
-        '09': '09_Setembro',
-        '08': '08_Agosto',
-        '07': '07_Julho',
-        '06': '06_Junho',
-        '05': '05_Maio',
-        '04': '04_Abril',
-        '03': '03_Março',
-        '02': '02_Fevereiro',
-        '01': '01_Janeiro'
+        "12": "12_Dezembro",
+        "11": "11_Novembro",
+        "10": "10_Outubro",
+        "09": "09_Setembro",
+        "08": "08_Agosto",
+        "07": "07_Julho",
+        "06": "06_Junho",
+        "05": "05_Maio",
+        "04": "04_Abril",
+        "03": "03_Março",
+        "02": "02_Fevereiro",
+        "01": "01_Janeiro",
     }
-    YEARS = ['2018', '2017', '2016', '2015']
+    YEARS = ["2018", "2017", "2016", "2015"]
     MONTHS_YEARS = itertools.product(MONTHS.values(), YEARS)
 
-    DATE_REGEX = r'DODF [0-9]+ ([0-9]{2}-[0-9]{2}-[0-9]{4})(.*)$'
+    DATE_REGEX = r"DODF [0-9]+ ([0-9]{2}-[0-9]{2}-[0-9]{4})(.*)$"
     EXTRA_EDITION_TEXT = "EDICAO EXTR"
-    PDF_URL = 'http://dodf.df.gov.br/index/visualizar-arquivo/?pasta={}&arquivo={}'
+    PDF_URL = "http://dodf.df.gov.br/index/visualizar-arquivo/?pasta={}&arquivo={}"
 
-    allowed_domains = ['dodf.df.gov.br']
-    name = 'df_brasilia'
+    allowed_domains = ["dodf.df.gov.br"]
+    name = "df_brasilia"
 
     def start_requests(self):
         for month, year in self.MONTHS_YEARS:
-            yield Request(f'{self.GAZETTE_URL}?dir={year}/{month}', self.parse_month)
+            yield Request(f"{self.GAZETTE_URL}?dir={year}/{month}", self.parse_month)
 
     def parse_month(self, response):
         json_response = json.loads(response.body_as_unicode())
-        dates = json_response['data']
+        dates = json_response["data"]
 
         if not dates:
             return
 
         for gazette_name in dates.values():
             date = re.search(self.DATE_REGEX, gazette_name).group(1)
-            day, month, year = date.split('-')
-            yield Request(f'{self.GAZETTE_URL}?dir={year}/{self.MONTHS[month]}/{gazette_name}')
+            day, month, year = date.split("-")
+            url = f"{self.GAZETTE_URL}?dir={year}/{self.MONTHS[month]}/{gazette_name}"
+            yield Request(url)
 
     def parse(self, response):
         """
@@ -65,14 +66,14 @@ class DfBrasiliaSpider(BaseGazetteSpider):
         if not json_response:
             return
 
-        json_dir = json_response['dir']
-        json_data = json_response['data']
+        json_dir = json_response["dir"]
+        json_data = json_response["data"]
 
         date = dateparser.parse(json_dir)
         is_extra_edition = self.EXTRA_EDITION_TEXT in json_dir
 
-        path = json_dir.replace('/', '|')
-        file_urls = [self.PDF_URL.format(path, url.split('/')[-1]) for url in json_data]
+        path = json_dir.replace("/", "|")
+        file_urls = [self.PDF_URL.format(path, url.split("/")[-1]) for url in json_data]
 
         yield Gazette(
             date=date,
@@ -80,5 +81,5 @@ class DfBrasiliaSpider(BaseGazetteSpider):
             is_extra_edition=is_extra_edition,
             territory_id=self.TERRITORY_ID,
             scraped_at=datetime.utcnow(),
-            power='executive_legislative'
+            power="executive_legislative",
         )
