8e712c34c1067576ad99b895c6a3be59fdcb160f
Alan Fachini <alfakini@gmail.com>
2018-06-02 11:29:30 -0300

    Refactoring

--- processing/data_collection/gazette/spiders/sc_joinville.py
+++ processing/data_collection/gazette/spiders/sc_joinville.py
@@ -1,44 +1,22 @@
-import re
 import dateparser
 
 from datetime import datetime
 from scrapy import Request, Spider
 from gazette.items import Gazette
 
-class GazetteElement:
-    DATE_CSS = 'span.article-date::text'
-    MUNICIPALITY_ID = '4209102'
-    PDF_URL = 'https://www.joinville.sc.gov.br{}'
-
-    def __init__(self, element):
-        self.element = element
-
-    def date(self):
-        date_str = self.element.css(self.DATE_CSS)[-1].extract()
-        return dateparser.parse(date_str, languages=['pt']).date()
-
-    def url(self):
-        path = self.element.css('a::attr(href)').extract_first()
-        return self.PDF_URL.format(path)
-
-    def to_gazette(self):
-        return Gazette(
-            date=self.date(),
-            file_urls=[self.url()],
-            is_extra_edition=False,
-            municipality_id=self.MUNICIPALITY_ID,
-            power='executive_legislature',
-            scraped_at=datetime.utcnow(),
-        )
-
 
 class ScJoinvilleSpider(Spider):
-    GAZETTE_ELEMENT_CSS = 'ul.jornal li'
-    NEXT_PAGE_CSS = 'ul.pagination li.next a::attr(href)'
+    MUNICIPALITY_ID = "4209102"
+    PDF_URL = "https://www.joinville.sc.gov.br{}"
 
-    allowed_domains = ['joinville.sc.gov.br']
-    name = 'sc_joinville'
-    start_urls = ['https://www.joinville.sc.gov.br/jornal/index/page/1']
+    GAZETTE_ELEMENT_CSS = "ul.jornal li"
+    NEXT_PAGE_CSS = "ul.pagination li.next a::attr(href)"
+    DATE_CSS = "span.article-date::text"
+    DATE_REGEX = "([\d]+)[ |]+([\w]+)[ |]+([\d]+)"
+
+    allowed_domains = ["joinville.sc.gov.br"]
+    name = "sc_joinville"
+    start_urls = ["https://www.joinville.sc.gov.br/jornal/index/page/1"]
 
     def parse(self, response):
         """
@@ -48,9 +26,26 @@ class ScJoinvilleSpider(Spider):
         """
 
         for element in response.css(self.GAZETTE_ELEMENT_CSS):
-            yield GazetteElement(element).to_gazette()
+            date = self.extract_date(element)
+            url = self.extract_url(element)
+
+            yield Gazette(
+                date=date,
+                file_urls=[url],
+                is_extra_edition=False,
+                municipality_id=self.MUNICIPALITY_ID,
+                power="executive_legislature",
+                scraped_at=datetime.utcnow(),
+            )
 
         for url in response.css(self.NEXT_PAGE_CSS).extract():
             yield Request(url)
 
+    def extract_date(self, element):
+        date = element.css(self.DATE_CSS).re(self.DATE_REGEX)
+        date = '/'.join(date)
+        return dateparser.parse(date, languages=["pt"]).date()
 
+    def extract_url(self, element):
+        path = element.css("a::attr(href)").extract_first()
+        return self.PDF_URL.format(path)
