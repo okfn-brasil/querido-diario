cfcb9f52f952efb8e80d8762b002d1ba121efb8e
Danilo Jeremias da Silva <dannnylo@gmail.com>
2018-06-01 21:09:02 -0300

    Improvement to older days

--- processing/data_collection/gazette/spiders/rj_campos_goytacazes.py
+++ processing/data_collection/gazette/spiders/rj_campos_goytacazes.py
@@ -12,11 +12,11 @@ class RjCampoGoytacazesSpider(BaseGazetteSpider):
 
     allowed_domains = ['www.campos.rj.gov.br']
     name = 'rj_campos_goytacazes'
-    start_urls = ['https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1']
+    start_urls = ['https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1&PGporPagina=15']
 
     def parse(self, response):
         """
-        @url https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1
+        @url https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1&PGporPagina=15
         @returns requests 1
         @scrapes date file_urls is_extra_edition municipality_id power scraped_at
         """
@@ -24,21 +24,22 @@ class RjCampoGoytacazesSpider(BaseGazetteSpider):
         for element in response.css(self.GAZETTE_ELEMENT_CSS):
             url = element.css('a::attr(href)').extract_first()
             gazette_text = element.css("h4::text").extract_first()
-            date_text = gazette_text.split(' - ')[0].split('Eletrônico de ')[1]
-            date = dateparser.parse(date_text, languages=['pt']).date()
-
             extra_edition = gazette_text.startswith('Suplemento')
 
-            yield Gazette(
-              date=date,
-              file_urls=[url],
-              is_extra_edition=extra_edition,
-              municipality_id=self.MUNICIPALITY_ID,
-              power='executive',
-              scraped_at=datetime.utcnow(),
-            )
+            date_re = re.search(r'(\d{2} de (.*) de \d{4})', gazette_text)
+            if date_re:
+                date = dateparser.parse(date_re.group(0), languages=['pt']).date()
+
+                yield Gazette(
+                  date=date,
+                  file_urls=[url],
+                  is_extra_edition=extra_edition,
+                  municipality_id=self.MUNICIPALITY_ID,
+                  power='executive',
+                  scraped_at=datetime.utcnow(),
+                )
 
         next_url = response.css('.pagination').xpath("//a[contains(text(), 'Proxima')]/@href").extract_first()
-
         if next_url:
+            print(next_url)
             yield  Request(response.urljoin(next_url))
