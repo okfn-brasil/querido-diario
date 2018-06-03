3bcb84311e63317d6e0714f1d2632799ebf5c40e
Vinicius Gasparini <viniciuszeiko@gmail.com>
2018-06-03 16:33:06 -0300

    Update sp_bauru.py
    he7d3r and alfakini suggestions
    reformatted using black

--- processing/data_collection/gazette/spiders/sp_bauru.py
+++ processing/data_collection/gazette/spiders/sp_bauru.py
@@ -7,7 +7,6 @@ from scrapy import Request
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
-
 class SpBauruSpider(BaseGazetteSpider):
     MUNICIPALITY_ID = "3506003"
 
@@ -29,13 +28,11 @@ class SpBauruSpider(BaseGazetteSpider):
             yield Request(self.PAGE_URL.format(year, month))
 
     def parse(self, response):
-
         """
         @url http://www.bauru.sp.gov.br/juridico/diariooficial.aspx?a=2015m=01
         @returns requests 1
         @scrapes date file_urls is_extra_edition municipality_id power scraped_at
         """
-
         for element in response.css(self.GAZETTE_CSS):
             url = self.extract_url(element)
             date = self.extract_date(element)
