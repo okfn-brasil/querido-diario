838aeeee6a97789c4044e7572c277fc458e08f32
antoniovendramin <drjedison@gmail.com>
2018-06-04 19:35:58 -0300

    Changing bas class to use start date Inlining scrape year Converting rows loop from tree lists to a single list loop

--- processing/data_collection/gazette/spiders/pr_curitiba.py
+++ processing/data_collection/gazette/spiders/pr_curitiba.py
@@ -2,18 +2,20 @@ from dateparser import parse
 import datetime as dt
 import re
 
+from gazette.spiders.base import BaseGazetteSpider
+
 import scrapy
 
 from gazette.items import Gazette
 
 
-class PrCuritibaSpider(scrapy.Spider):
+class PrCuritibaSpider(BaseGazetteSpider):
     MUNICIPALITY_ID = '4106902'
     name = 'pr_curitiba'
     allowed_domains = ['legisladocexterno.curitiba.pr.gov.br']
     start_urls = ['http://legisladocexterno.curitiba.pr.gov.br/DiarioConsultaExterna_Pesquisa.aspx']
 
-    def parse(self, response):
+    def start_requests(self):
         """
         The Curitiba website is a statefull page, so we can't just build the
         request from zero, we have to resend the viewstate with every request.
@@ -22,23 +24,23 @@ class PrCuritibaSpider(scrapy.Spider):
         """
         todays_date = dt.date.today()
         current_year = todays_date.year
-        for year in range(current_year, 2014, -1):
-            yield self.scrap_year(response, year)
-
-    def scrap_year(self, response, year):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={
-                'ctl00$cphMasterPrincipal$ddlGrAno': str(year)
-            },
-            callback=self.parse_year
-        )
+        stop_on_year = 2015
+        if hasattr(self, 'start_date'):
+            stop_on_year = self.start_date.year
+        for year in range(current_year, stop_on_year - 1, -1):
+            yield scrapy.FormRequest(
+                'http://legisladocexterno.curitiba.pr.gov.br/DiarioConsultaExterna_Pesquisa.aspx',
+                formdata={
+                    'ctl00$cphMasterPrincipal$ddlGrAno': str(year)
+                },
+                callback=self.parse_year
+            )
 
     def parse_year(self, response):
         for i in range(12):
-            yield self.scrap_month(response, i)
+            yield self.scrape_month(response, i)
 
-    def scrap_month(self, response, month):
+    def scrape_month(self, response, month):
         return scrapy.FormRequest.from_response(
             response,
             formdata={
@@ -75,8 +77,8 @@ class PrCuritibaSpider(scrapy.Spider):
 
     def parse_page(self, response):
         for idx, row in enumerate(response.css(".grid_Row")):
-            pdf_date = row.css("td:nth-child(2) span ::text").extract()
-            gazette_id = row.css("td:nth-child(3) a ::attr(data-teste)").extract()
+            pdf_date = row.css("td:nth-child(2) span ::text").extract_first()
+            gazette_id = row.css("td:nth-child(3) a ::attr(data-teste)").extract_first()
             parsed_date = parse(f'{pdf_date}', languages=['pt']).date()
             if gazette_id == '0':
                 starting_offset = 3
@@ -104,10 +106,10 @@ class PrCuritibaSpider(scrapy.Spider):
 
     def scrap_not_extra_edition(self, response):
         parsed_date = response.meta['parsed_date']
-        id = re.findall(r'Id=(\d+)', response.text)[0]
+        gazette_id = re.findall(r'Id=(\d+)', response.text)[0]
         return Gazette(
             date=parsed_date,
-            file_urls=["http://legisladocexterno.curitiba.pr.gov.br/DiarioConsultaExterna_Download.aspx?id={}".format(id)],
+            file_urls=["http://legisladocexterno.curitiba.pr.gov.br/DiarioConsultaExterna_Download.aspx?id={}".format(gazette_id)],
             is_extra_edition=False,
             municipality_id=self.MUNICIPALITY_ID,
             power='executive_legislature',
