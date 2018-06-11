a216b2334ec9ec0fd29b98028bc4b1f0f8f39fd9
Giovani <sousa_giovani@hotmail.com>
2018-06-11 13:14:35 -0300

    Black spider pr_londrina

--- processing/data_collection/gazette/spiders/pr_londrina.py
+++ processing/data_collection/gazette/spiders/pr_londrina.py
@@ -8,10 +8,10 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class PrLondrina(BaseGazetteSpider):
-    TERRITORY_ID = '4113700'
-    name = 'pr_londrina'
-    allowed_domains = ['londrina.pr.gov.br']
-    start_urls = ['http://www2.londrina.pr.gov.br/jornaloficial/']
+    TERRITORY_ID = "4113700"
+    name = "pr_londrina"
+    allowed_domains = ["londrina.pr.gov.br"]
+    start_urls = ["http://www2.londrina.pr.gov.br/jornaloficial/"]
 
     def parse(self, response):
         """
@@ -20,12 +20,17 @@ class PrLondrina(BaseGazetteSpider):
         """
         lines = response.xpath('//table[contains(@class, "adminlist")]/tr')
 
-        urls = [response.urljoin(relative_url)
-                for relative_url in lines.xpath('td[1]/a/@href').extract()]
-        is_extra_edition = ['Extra' in text
-                            for text in lines.xpath('td[1]/a/text()').extract()]
-        dates = [parse(date, languages=['pt']).date()
-                 for date in lines.xpath('td[2]/text()').extract()]
+        urls = [
+            response.urljoin(relative_url)
+            for relative_url in lines.xpath("td[1]/a/@href").extract()
+        ]
+        is_extra_edition = [
+            "Extra" in text for text in lines.xpath("td[1]/a/text()").extract()
+        ]
+        dates = [
+            parse(date, languages=["pt"]).date()
+            for date in lines.xpath("td[2]/text()").extract()
+        ]
 
         for url, is_extra, date in zip(urls, is_extra_edition, dates):
             yield Gazette(
@@ -33,9 +38,11 @@ class PrLondrina(BaseGazetteSpider):
                 file_urls=[url],
                 is_extra_edition=is_extra,
                 municipality_id=self.MUNICIPALITY_ID,
-                power='executive_legislature',
+                power="executive_legislature",
                 scraped_at=dt.datetime.utcnow(),
             )
 
-        for page in range(2, len(response.css('.button.othersOptPage')) + 1):
-            yield FormRequest(response.url, formdata={'hpage': str(page)}, callback=self.parse)
+        for page in range(2, len(response.css(".button.othersOptPage")) + 1):
+            yield FormRequest(
+                response.url, formdata={"hpage": str(page)}, callback=self.parse
+            )
