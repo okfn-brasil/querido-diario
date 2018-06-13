3a7ce9ddcba950c2cad73b58af1704de07cc59f8
antoniovendramin <drjedison@gmail.com>
2018-06-13 18:43:22 -0300

    Adding languages back to the parse of date

--- processing/data_collection/gazette/spiders/pr_maringa.py
+++ processing/data_collection/gazette/spiders/pr_maringa.py
@@ -39,7 +39,7 @@ class PrMaringaSpider(BaseGazetteSpider):
             gazette_id = row.css('td:nth-child(1) a::attr(href)').re_first('.*/[oO]{2}[mM] (.*)\.pdf')
             gazette_date = row.css('td:nth-child(2) font > font::text').extract_first()
             yield Gazette(
-                date=parse(gazette_date).date(),
+                date=parse(gazette_date, languages=['pt']).date(),
                 file_urls=[f'http://venus.maringa.pr.gov.br/arquivos/orgao_oficial/arquivos/oom%20{gazette_id}.pdf'],
                 is_extra_edition=any(caracter.isalpha() for caracter in gazette_id),
                 territory_id=self.TERRITORY_ID,
