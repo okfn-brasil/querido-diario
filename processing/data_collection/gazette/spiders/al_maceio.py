321d94b323072da9c4db726e96c8d71135ff2af5
Eduardo Cuducos <cuducos@gmail.com>
2018-07-10 15:32:21 -0300

    Format Python code with Black

--- processing/data_collection/gazette/spiders/al_maceio.py
+++ processing/data_collection/gazette/spiders/al_maceio.py
@@ -8,10 +8,10 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class AlMaceioSpider(BaseGazetteSpider):
-    MUNICIPALITY_ID = '2704302'
-    name = 'al_maceio'
-    allowed_domains = ['maceio.al.gov.br']
-    start_urls = ['http://www.maceio.al.gov.br/noticias/diario-oficial/']
+    MUNICIPALITY_ID = "2704302"
+    name = "al_maceio"
+    allowed_domains = ["maceio.al.gov.br"]
+    start_urls = ["http://www.maceio.al.gov.br/noticias/diario-oficial/"]
     page_number = 1
 
     def parse(self, response):
@@ -21,25 +21,25 @@ class AlMaceioSpider(BaseGazetteSpider):
         @returns requests 1 10
         @scrapes date file_urls is_extra_edition municipality_id power scraped_at
         """
-        gazettes = list(response.xpath('//article'))
+        gazettes = list(response.xpath("//article"))
         for gazette in gazettes:
-            url = gazette.xpath('a/@href').extract_first()
+            url = gazette.xpath("a/@href").extract_first()
 
-            if not url: # In some cases the href attr is empty, e.g. 24-11-2015
+            if not url:  # In some cases the href attr is empty, e.g. 24-11-2015
                 continue
 
-            date_str = gazette.xpath('time/text()').extract_first()
-            date = parse(date_str, languages=['pt'])
-            title = gazette.xpath('a/@title').extract_first()
-            is_extra_edition = 'suplemento' in (title.lower())
+            date_str = gazette.xpath("time/text()").extract_first()
+            date = parse(date_str, languages=["pt"])
+            title = gazette.xpath("a/@title").extract_first()
+            is_extra_edition = "suplemento" in (title.lower())
 
-            if 'wp-content/uploads' in url:
+            if "wp-content/uploads" in url:
                 gazette = self.create_gazette(date, url, is_extra_edition)
                 yield gazette
             else:
                 request = scrapy.Request(url, self.parse_additional_page)
-                request.meta['date'] = date
-                request.meta['is_extra_edition'] = is_extra_edition
+                request.meta["date"] = date
+                request.meta["is_extra_edition"] = is_extra_edition
                 yield request
 
         """
@@ -48,8 +48,7 @@ class AlMaceioSpider(BaseGazetteSpider):
         if gazettes:
             self.page_number += 1
             yield scrapy.Request(
-                '{0}/page/{1}'.format(self.start_urls[0],
-                str(self.page_number))
+                "{0}/page/{1}".format(self.start_urls[0], str(self.page_number))
             )
 
     def parse_additional_page(self, response):
@@ -60,9 +59,9 @@ class AlMaceioSpider(BaseGazetteSpider):
         @scrapes date file_urls is_extra_edition municipality_id power scraped_at
         """
         url = response.xpath('//p[@class="attachment"]/a/@href').extract_first()
-        gazette = self.create_gazette(response.meta['date'],
-                                      url,
-                                      response.meta['is_extra_edition'])
+        gazette = self.create_gazette(
+            response.meta["date"], url, response.meta["is_extra_edition"]
+        )
         yield gazette
 
     def create_gazette(self, date, url, is_extra_edition):
@@ -71,6 +70,6 @@ class AlMaceioSpider(BaseGazetteSpider):
             file_urls=[url],
             is_extra_edition=is_extra_edition,
             municipality_id=self.MUNICIPALITY_ID,
-            power='executive_legislature',
+            power="executive_legislature",
             scraped_at=datetime.utcnow(),
         )
