a40ee60eeb11b2607273ca1ddb373adf968a3ad2
Eduardo Cuducos <cuducos@gmail.com>
2018-07-10 15:49:48 -0300

    Update BH spider
    Esse commit atualiza o spider de Belo Horizonte de várias formas:
    
    * Deixando o controle de data p/ a arquitetura do projeto (e não para o Spider)
    * Atualizando os nomes (mg_belohorizonte para mg_belo_horizonte, por exemplo)
    * Invertendo a ordem de coleta (mais novos primeiro)
    * Melhorando a arquitetura do Spider (usando `start_requests`)
    * Utilizando o `urljoin` do Scrapy
    * Utilizando `yield` ao invés de alimentar uma lista
    * Formatando o código com Black

---
+++ processing/data_collection/gazette/spiders/mg_belo_horizonte.py
@@ -0,0 +1,50 @@
+from datetime import date, datetime, timedelta
+
+from scrapy import Request, Spider
+
+from gazette.items import Gazette
+
+
+class MgBeloHorizonteSpider(Spider):
+    MUNICIPALITY_ID = 3106200
+    name = "mg_belo_horizonte"
+    allowed_domains = ["portal6.pbh.gov.br"]
+    documents_url = (
+        "http://portal6.pbh.gov.br/dom/iniciaEdicao.do"
+        "?method=DomDia&dia={}&comboAno={}"
+    )
+    first_edition = date(2002, 3, 21)
+
+    def start_requests(self):
+        current_date = date.today()
+        while current_date >= self.first_edition:
+            formatted_date = current_date.strftime("%d/%m/%Y")
+            yield Request(
+                self.documents_url.format(formatted_date, current_date.year),
+                callback=self.parse_page_link,
+                meta={"current_date": current_date},
+            )
+            current_date -= timedelta(days=1)
+
+    def parse_page_link(self, response):
+        xpath = '//p[contains(@class,"dom-chamadas")]/a/@href'
+        url_path = response.xpath(xpath).extract_first()
+        if url_path:
+            yield Request(
+                response.urljoin(url_path),
+                callback=self.parse_details,
+                meta={"current_date": response.meta["current_date"]},
+            )
+
+    def parse_details(self, response):
+        xpath = '//a[contains(@alt, "link_anexo")]/@href'
+        file_url = response.xpath(xpath).extract_first()
+        if file_url:
+            yield Gazette(
+                date=response.meta["current_date"],
+                file_urls=[response.urljoin(file_url)],
+                is_extra_edition=False,
+                municipality_id=self.MUNICIPALITY_ID,
+                power="executive_legislature",
+                scraped_at=datetime.utcnow(),
+            )
