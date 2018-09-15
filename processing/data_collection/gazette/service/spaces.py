5623f9e94a579456dbd411f9d4abcb3fa792fc00
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-09-15 17:40:20 -0300

    Use decouple to get env vars in spaces.py

--- processing/data_collection/gazette/service/spaces.py
+++ processing/data_collection/gazette/service/spaces.py
@@ -1,5 +1,7 @@
 import os
 
+from decouple import config
+
 import boto3
 
 
@@ -9,11 +11,15 @@ class Spaces(object):
         session = boto3.session.Session()
         self.client = session.client(
             's3',
-            region_name=os.getenv('SPACES_REGION'),
-            endpoint_url=os.getenv('SPACES_ENDPOINT_URL'),
-            aws_access_key_id=os.getenv('SPACES_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('SPACES_SECRET_ACCESS_KEY')
+            region_name=config('SPACES_REGION', ''),
+            endpoint_url=config('SPACES_ENDPOINT_URL', ''),
+            aws_access_key_id=config('SPACES_ACCESS_KEY_ID', ''),
+            aws_secret_access_key=config('SPACES_SECRET_ACCESS_KEY', '')
         )
 
     def upload_file(self, file_path):
-        self.client.upload_file(file_path, os.getenv('SPACES_NAME'), file_path)
+        self.client.upload_file(
+            file_path,
+            config('SPACES_NAME', default=''),
+            file_path
+        )
