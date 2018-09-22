e665f3fa74755867ef9862c04a3f6ae42e93a59d
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-09-22 15:39:55 -0300

    Reformat test_bidding_exemption_parsing.py

--- processing/tests/gazette/data/test_bidding_exemption_parsing.py
+++ processing/tests/gazette/data/test_bidding_exemption_parsing.py
@@ -63,19 +63,35 @@ class TestBiddingExemptionParsing:
         assert object == exemption.object
 
     def test_update_object_using_label_with_introduction_1(self):
-        object = "Trata o Presente de Ato de Dispensa de Licitação prevista no artigo 24 inciso II da Lei 8.666 de 1993 tendo por objeto a aquisição de serviços de dedetização, desratização e limpeza da caixa d’água das áreas I e II da AGCMG;"
+        object = (
+            "Trata o Presente de Ato de Dispensa de Licitação prevista no "
+            + "artigo 24 inciso II da Lei 8.666 de 1993 tendo por objeto a "
+            + "aquisição de serviços de dedetização, desratização e limpeza "
+            + "da caixa d’água das áreas I e II da AGCMG;"
+        )
         exemption = MagicMock(data={"OBJETO": object})
         self.subject.update_object(exemption)
 
-        expected_object = "serviços de dedetização, desratização e limpeza da caixa d’água das áreas I e II da AGCMG;"
+        expected_object = (
+            "serviços de dedetização, desratização e limpeza "
+            + "da caixa d’água das áreas I e II da AGCMG;"
+        )
         assert expected_object == exemption.object
 
     def test_update_object_using_label_with_introduction_2(self):
-        object = "Trata o presente de Ato de Dispensa de Licitação prevista no artigo 24 inciso II da Lei nº 8.666 de 1993, tendo por objeto aquisição de Certificado Digital PJ1, para atender o Fundo Municipal da Guarda Civil Metropolitana - FMGCM."
+        object = (
+            "Trata o presente de Ato de Dispensa de Licitação prevista no "
+            + "artigo 24 inciso II da Lei nº 8.666 de 1993, tendo por objeto "
+            + "aquisição de Certificado Digital PJ1, para atender o Fundo "
+            + "Municipal da Guarda Civil Metropolitana - FMGCM."
+        )
         exemption = MagicMock(data={"OBJETO": object})
         self.subject.update_object(exemption)
 
-        expected_object = "Certificado Digital PJ1, para atender o Fundo Municipal da Guarda Civil Metropolitana - FMGCM."
+        expected_object = (
+            "Certificado Digital PJ1, para atender o Fundo Municipal "
+            + "da Guarda Civil Metropolitana - FMGCM."
+        )
         assert expected_object == exemption.object
 
     def test_update_value_label_1(self):
@@ -137,7 +153,8 @@ class TestBiddingExemptionParsing:
     def test_update_contracted_code_from_source_text(self):
         exemption = MagicMock(
             contracted=None,
-            source_text="- EMPRESAS CONTRATADAS: BR GÁS LTDA-ME, CNPJ nº 08.926.037/0001-57.\n- VALOR TOTAL: R$ 3.240,00 (Três mil, duzentos e quarenta reais).",
+            source_text="- EMPRESAS CONTRATADAS: BR GÁS LTDA-ME, CNPJ nº 08.926.037/"
+            + "0001-57.\n- VALOR TOTAL: R$ 3.240,00 (Três mil, duzentos e quarenta reais).",
         )
         self.subject.update_contracted_code(exemption)
         assert "08926037000157" == exemption.contracted_code
@@ -146,7 +163,8 @@ class TestBiddingExemptionParsing:
         exemption = MagicMock(
             contracted=None,
             contracted_code=None,
-            source_text="- EMPRESAS CONTRATADAS: BR GÁS LTDA-ME.\n- VALOR TOTAL: R$ 3.240,00 (Três mil, duzentos e quarenta reais).",
+            source_text="- EMPRESAS CONTRATADAS: BR GÁS LTDA-ME.\n- VALOR TOTAL: R$ 3.240,00 "
+            + "(Três mil, duzentos e quarenta reais).",
         )
         self.subject.update_contracted_code(exemption)
         assert exemption.contracted_code is None
@@ -155,7 +173,10 @@ class TestBiddingExemptionParsing:
         exemption = MagicMock(
             contracted=None,
             contracted_code=None,
-            source_text="- ÓRGÃO CONTRATANTE: Agência da Guarda Civil Metropolitana de Goiânia – AGCMG,\n         CNPJ nº 57.494.031/0010-54.\n         - EMPRESAS CONTRATADAS: REDE EPI EQUIPAMENTOS DE SEGURANÇA EIRELI,\n         CNPJ nº 18.428.558/0001-38.",
+            source_text="- ÓRGÃO CONTRATANTE: Agência da Guarda Civil Metropolitana"
+            + " de Goiânia – AGCMG,\n         CNPJ nº 57.494.031/0010-54.\n         "
+            + "- EMPRESAS CONTRATADAS: REDE EPI EQUIPAMENTOS DE SEGURANÇA EIRELI,\n         "
+            + "CNPJ nº 18.428.558/0001-38.",
         )
         self.subject.update_contracted_code(exemption)
         assert exemption.contracted_code is None
