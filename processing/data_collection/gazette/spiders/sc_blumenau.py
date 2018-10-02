464e16e28ff0e68cb76bfee2c8f73965f55d9077
marasquin <daiana.marasquin@gmail.com>
2018-10-02 19:51:24 -0300

    Fix import, empty lines and date extraction

--- processing/data_collection/gazette/spiders/sc_blumenau.py
+++ processing/data_collection/gazette/spiders/sc_blumenau.py
@@ -1,19 +1,22 @@
+from datetime import datetime
 
 from dateparser import parse
-from datetime import datetime
 from scrapy import Request
+
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class ScBlumenauSpider(BaseGazetteSpider):
-    TERRITORY_ID = '4202404'
-    POSSIBLE_GAZETTE_CSS = '.quiet'
-    NEXT_PAGE_CSS = '.pagination li.next:not(.disabled) a::attr(href)'
+    TERRITORY_ID = "4202404"
+    POSSIBLE_GAZETTE_CSS = ".quiet"
+    NEXT_PAGE_CSS = ".pagination li.next:not(.disabled) a::attr(href)"
 
-    allowed_domains = ['diariomunicipal.sc.gov.br']
-    name = 'sc_blumenau'
-    start_urls = ['https://www.diariomunicipal.sc.gov.br/site/?r=site/index&q=cod_municipio%3A164']
+    allowed_domains = ["diariomunicipal.sc.gov.br"]
+    name = "sc_blumenau"
+    start_urls = [
+        "https://www.diariomunicipal.sc.gov.br/site/?r=site/index&q=cod_municipio%3A164"
+    ]
 
     def parse(self, response):
         """
@@ -24,25 +27,22 @@ class ScBlumenauSpider(BaseGazetteSpider):
 
         possible_gazettes = response.css(self.POSSIBLE_GAZETTE_CSS)
         for element in possible_gazettes:
-            url = element.css('a::attr(href)').extract_first()
+            url = element.css("a::attr(href)").extract_first()
             if url:
-                date = self.extract_date(element)
+                date = parse(
+                    element.css("::text").re_first("([\d]{2}\/[\d]{2}\/[\d]{4})"),
+                    languages=["pt"],
+                ).date()
 
                 yield Gazette(
                     date=date,
                     file_urls=[url],
                     is_extra_edition=False,
                     territory_id=self.TERRITORY_ID,
-                    power='executive_legislature',
+                    power="executive_legislature",
                     scraped_at=datetime.utcnow(),
                 )
 
         next_page_path = response.css(self.NEXT_PAGE_CSS).extract_first()
         if next_page_path:
             yield Request(response.urljoin(next_page_path))
-
-    def extract_date(self, element):
-        text = element.css('::text').extract_first()
-        date = text[:10]
-        return parse(date, languages=['pt']).date()
-
