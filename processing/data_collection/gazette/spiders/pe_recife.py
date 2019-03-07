f27d53866cb8ca9c19562bdfdd3cee32d40b11bf
Anderson Berg <andersonberg@gmail.com>
2019-03-07 12:00:10 -0300

    fixing date parser

--- processing/data_collection/gazette/spiders/pe_recife.py
+++ processing/data_collection/gazette/spiders/pe_recife.py
@@ -1,4 +1,4 @@
-# from dateparser import parse
+from dateparser import parse
 import datetime as dt
 
 import scrapy
@@ -7,11 +7,11 @@ from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
-# Lista de todas as datas contendo diário (2017-2019):
+# All available dates list (2017-2019):
 # https://www.cepe.com.br/prefeituradiario/diarios.txt
 
-# URL pra acessar um diário:
-# http://200.238.101.22/docreader/docreader.aspx?bib=R20160324&pasta=Marco\Dia%2024
+# Example of URL to get a document:
+# http://200.238.101.22/docreader/docreader.aspx?bib=R20180227&pasta=Fevereiro%5CDia%2027
 
 
 class PeRecifeSpider(BaseGazetteSpider):
@@ -45,39 +45,43 @@ class PeRecifeSpider(BaseGazetteSpider):
     )
 
     def start_requests(self):
-        current_date = dt.date.today()
-        day_comp = int(dt.datetime.strftime(current_date, "%Y%m%d"))
-        return self.make_document_request(current_date, day_comp)
+        return self.make_dates_request()
 
-    def make_document_request(self, current_date, day_comp):
-        # TODO: fix for loop
-        for day_int in range(day_comp, 20_150_430, -1):
-            current_month = current_date.month
-            month_str = self.month_dict[f"{current_month:0>2}"]
-            day_str = f"{current_date.day:0>2}"
-            bib_code = f"R{day_int}"
-            pasta_code = f"{month_str}\\Dia%20{day_str}"
-            url = self.BASE_URL.format(bib_code=bib_code, pasta_code=pasta_code)
+    def make_dates_request(self):
+        url = "https://www.cepe.com.br/prefeituradiario/diarios.txt"
+        yield scrapy.Request(url, self.parse_dates)
 
-            doc_date = current_date
-            current_date = dt.datetime.strptime(str(day_int), "%Y%m%d")
-            # next date
-            # current_date = current_date - dt.timedelta(days=1)
-            # day_comp = int(dt.datetime.strftime(current_date, "%Y%m%d"))
+    def parse_dates(self, response):
+        dates_txt = response.text
+        available_dates = dates_txt.split("&")
+        for diario_date in available_dates:
+            if diario_date:
+                yield self.make_document_request(diario_date)
 
-            yield scrapy.Request(url, self.parse_document, meta={"date": doc_date})
+    def make_document_request(self, diario_date):
+        current_date = parse(diario_date, settings={"DATE_ORDER": "DMY"})
+        current_month = current_date.month
+
+        month_str = self.month_dict[f"{current_month:0>2}"]
+        day_str = f"{current_date.day:0>2}"
+        day_int = int(dt.datetime.strftime(current_date, "%Y%m%d"))
+        bib_code = f"R{day_int}"
+        pasta_code = f"{month_str}\\Dia%20{day_str}"
+        url = self.BASE_URL.format(bib_code=bib_code, pasta_code=pasta_code)
+
+        return scrapy.Request(url, self.parse_document, meta={"date": current_date})
 
     def parse_document(self, response):
         hidden_id = response.css("input[id=HiddenID]::attr(value)").extract_first()
         items = []
-        if hidden_id:
-            url = self.DOWNLOAD_URL.format(doc_id=hidden_id)
-            items.append(
-                Gazette(
-                    date=response.meta["date"],
-                    file_urls=[url],
-                    territory_id=self.MUNICIPALITY_ID,
-                    scraped_at=dt.datetime.utcnow(),
-                )
-            )
+        # if hidden_id:
+        #     url = self.DOWNLOAD_URL.format(doc_id=hidden_id)
+        #     items.append(
+        #         Gazette(
+        #             date=response.meta["date"],
+        #             file_urls=[url],
+        #             # territory_id=self.MUNICIPALITY_ID,
+        #             scraped_at=dt.datetime.utcnow(),
+        #         )
+        #     )
         return items
