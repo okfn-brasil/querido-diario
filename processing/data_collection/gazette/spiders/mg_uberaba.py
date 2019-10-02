bdff2725d0421d3dc036ad9ec8520cccb36dc867
Giovani <sousa_giovani@hotmail.com>
2019-10-02 00:36:57 -0300

    Add mg_uberaba spider

---
+++ processing/data_collection/gazette/spiders/mg_uberaba.py
@@ -0,0 +1,62 @@
+import datetime as dt
+
+from dateparser import parse
+from scrapy.http import FormRequest
+
+from gazette.items import Gazette
+from gazette.spiders.base import BaseGazetteSpider
+
+
+class MgUberaba(BaseGazetteSpider):
+    TERRITORY_ID = "3170107"
+    name = "mg_uberaba"
+    allowed_domains = ["http://www.uberaba.mg.gov.br"]
+
+    LIST_GAZETTES_URL = "http://www.uberaba.mg.gov.br/portal/listImagesHtml"
+    DOWNLOAD_URL_TEMPLATE = (
+        "http://www.uberaba.mg.gov.br:8080/portal/acervo/portavoz/arquivos/{}/{}"
+    )
+
+    def start_requests(self):
+        next_year = dt.datetime.today().year + 1
+
+        for year in range(2015, next_year):
+            yield FormRequest(
+                url=self.LIST_GAZETTES_URL,
+                method="POST",
+                formdata={
+                    "desc": "1",
+                    "type": "1",
+                    "folder": f"portavoz/arquivos/{year}",
+                    "limit": "5000",
+                    "page": "1",
+                    "types": "pdf",
+                    "listAll": "1",
+                },
+                meta={"year": year},
+            )
+
+    def parse(self, response):
+        filenames = [
+            filename.strip()
+            for filename in response.xpath(
+                '//div[@class="claGaleriaBoxFileTable"]/text()'
+            ).extract()
+        ]
+        for filename in filenames:
+            date = self.extract_date(filename)
+            yield Gazette(
+                date=date,
+                file_urls=[self.mount_url(filename, date.year)],
+                is_extra_edition=False,
+                territory_id=self.TERRITORY_ID,
+                power="executive_legislature",
+                scraped_at=dt.datetime.utcnow(),
+            )
+
+    def extract_date(self, filename):
+        date_str = filename.split()[-1].replace(".pdf", "").replace("-", " ").strip()
+        return parse(date_str, languages=["pt"]).date()
+
+    def mount_url(self, filename, year):
+        return self.DOWNLOAD_URL_TEMPLATE.format(year, filename)
