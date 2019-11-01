8e5c1c6769b1559c90ebeca2fc101ad2a954a66d
Irio Musskopf <irio@protonmail.com>
2019-11-01 07:36:26 +0100

    Format Python files using Black

--- processing/data_collection/gazette/spiders/pr_maringa.py
+++ processing/data_collection/gazette/spiders/pr_maringa.py
@@ -9,9 +9,9 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class PrMaringaSpider(BaseGazetteSpider):
-    TERRITORY_ID = '4115200'
-    name = 'pr_maringa'
-    allowed_domains = ['maringa.pr.gov.br']
+    TERRITORY_ID = "4115200"
+    name = "pr_maringa"
+    allowed_domains = ["maringa.pr.gov.br"]
 
     def start_requests(self):
         """
@@ -22,27 +22,26 @@ class PrMaringaSpider(BaseGazetteSpider):
         current_year = todays_date.year
         for year in range(current_year, 2006, -1):
             yield scrapy.FormRequest(
-                'http://venus.maringa.pr.gov.br/arquivos/orgao_oficial/paginar_orgao_oficial_ano.php',
-                headers={
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                formdata={
-                    'ano': str(year),
-                    'entrar': 'Buscar'
-                },
-                callback=self.parse_year
+                "http://venus.maringa.pr.gov.br/arquivos/orgao_oficial/paginar_orgao_oficial_ano.php",
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                formdata={"ano": str(year), "entrar": "Buscar"},
+                callback=self.parse_year,
             )
 
     def parse_year(self, response):
-        rows = response.css('table tr')[1:]
+        rows = response.css("table tr")[1:]
         for row in rows:
-            gazette_id = row.css('td:nth-child(1) a::attr(href)').re_first('.*/[oO]{2}[mM] (.*)\.pdf')
-            gazette_date = row.css('td:nth-child(2) font > font::text').extract_first()
+            gazette_id = row.css("td:nth-child(1) a::attr(href)").re_first(
+                ".*/[oO]{2}[mM] (.*)\.pdf"
+            )
+            gazette_date = row.css("td:nth-child(2) font > font::text").extract_first()
             yield Gazette(
-                date=parse(gazette_date, languages=['pt']).date(),
-                file_urls=[f'http://venus.maringa.pr.gov.br/arquivos/orgao_oficial/arquivos/oom%20{gazette_id}.pdf'],
+                date=parse(gazette_date, languages=["pt"]).date(),
+                file_urls=[
+                    f"http://venus.maringa.pr.gov.br/arquivos/orgao_oficial/arquivos/oom%20{gazette_id}.pdf"
+                ],
                 is_extra_edition=any(caracter.isalpha() for caracter in gazette_id),
                 territory_id=self.TERRITORY_ID,
-                power='executive_legislature',
-                scraped_at=datetime.utcnow()
+                power="executive_legislature",
+                scraped_at=datetime.utcnow(),
             )
