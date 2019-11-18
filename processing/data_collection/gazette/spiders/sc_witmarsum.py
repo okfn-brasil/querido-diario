c83b6838b0e82887bd2bdec7c4a12943bba72a1f
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-17 23:19:10 -0300

    sc_witmarsum.py: adds spider for Witmarsum
    Adds a new spider for Witmarsum - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_witmarsum.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScWitmarsumSpider(FecamGazetteSpider):
+    name = "sc_witmarsum"
+    FECAM_QUERY = 'cod_entidade:292'
+    TERRITORY_ID = "4219408"
