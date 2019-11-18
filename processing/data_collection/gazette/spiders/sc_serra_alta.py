7622d53173fdb6781cbf3f70c2bc451832d2c17e
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-17 23:53:23 -0300

    sc_serra_alta.py: adds spider for Serra Alta
    Adds a new spider for Serra Alta - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_serra_alta.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSerraAltaSpider(FecamGazetteSpider):
+    name = "sc_serra_alta"
+    FECAM_QUERY = 'cod_entidade:263'
+    TERRITORY_ID = "4217550"
