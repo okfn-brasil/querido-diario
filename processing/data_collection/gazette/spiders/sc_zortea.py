1d0bc3150c56a2d78cf895ac7ea784b983d4f249
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-17 22:58:16 -0300

    sc_zortea.py: adds spider for Zortéa
    Adds a new spider for Zortéa - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_zortea.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScZorteaSpider(FecamGazetteSpider):
+    name = "sc_zortea"
+    FECAM_QUERY = 'cod_entidade:296'
+    TERRITORY_ID = "4219853"
