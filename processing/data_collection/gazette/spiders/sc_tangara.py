118d4cf2e9a8a6ff1a80015d8417a3a053a8e817
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-17 23:48:39 -0300

    sc_tangara.py: adds spider for Tangará
    Adds a new spider for Tangará - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_tangara.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScTangaraSpider(FecamGazetteSpider):
+    name = "sc_tangara"
+    FECAM_QUERY = 'cod_entidade:268'
+    TERRITORY_ID = "4217907"
