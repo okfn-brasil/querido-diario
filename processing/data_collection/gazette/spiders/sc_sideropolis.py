c8c2fd344bb9b910c178e5cfe87e25ba882fa1f9
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-17 23:52:27 -0300

    sc_sideropolis.py: adds spider for Siderópolis
    Adds a new spider for Siderópolis - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_sideropolis.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSideropolisSpider(FecamGazetteSpider):
+    name = "sc_sideropolis"
+    FECAM_QUERY = 'cod_entidade:264'
+    TERRITORY_ID = "4217600"
