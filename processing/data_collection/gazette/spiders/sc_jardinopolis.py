f54df257fb47b9e7ab06da1703a49418b43a9283
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:07 -0300

    sc_jardinopolis.py: adds spider for Jardinópolis
    Adds a new spider for Jardinópolis - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_jardinopolis.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScJardinopolisSpider(FecamGazetteSpider):
+    name = "sc_jardinopolis"
+    FECAM_QUERY = 'cod_entidade:139'
+    TERRITORY_ID = "4208955"
