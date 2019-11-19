89e4a0b27a7c963d8ba35e9a9b3d6f9373e71364
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:51 -0300

    sc_apiuna.py: adds spider for Apiúna
    Adds a new spider for Apiúna - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_apiuna.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScApiunaSpider(FecamGazetteSpider):
+    name = "sc_apiuna"
+    FECAM_QUERY = 'cod_entidade:19'
+    TERRITORY_ID = "4201257"
