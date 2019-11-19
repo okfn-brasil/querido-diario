c110fa27871e7ea30d8dc26e699caa44251a8acd
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:57 -0300

    sc_cerro_negro.py: adds spider for Cerro Negro
    Adds a new spider for Cerro Negro - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cerro_negro.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCerroNegroSpider(FecamGazetteSpider):
+    name = "sc_cerro_negro"
+    FECAM_QUERY = 'cod_entidade:69'
+    TERRITORY_ID = "4204178"
