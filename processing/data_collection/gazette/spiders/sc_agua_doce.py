dda1515083893d0fc6ee5186a4b1a2216e6c03c9
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:31:28 -0300

    sc_agua_doce.py: adds spider for Água Doce
    Adds a new spider for Água Doce - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_agua_doce.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScAguaDoceSpider(FecamGazetteSpider):
+    name = "sc_agua_doce"
+    FECAM_QUERY = 'cod_entidade:8'
+    TERRITORY_ID = "4200408"
