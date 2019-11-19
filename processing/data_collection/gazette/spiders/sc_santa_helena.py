15d4141b572e3133145bb40057a329d42600f92a
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:08:58 -0300

    sc_santa_helena.py: adds spider for Santa Helena
    Adds a new spider for Santa Helena - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_santa_helena.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSantaHelenaSpider(FecamGazetteSpider):
+    name = "sc_santa_helena"
+    FECAM_QUERY = 'cod_entidade:232'
+    TERRITORY_ID = "4215554"
