87954005a4a583c704933141478927f02291573f
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:01:09 -0300

    sc_sao_francisco_do_sul.py: adds spider for São Francisco do Sul
    Adds a new spider for São Francisco do Sul - SC.
    It uses the as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_sao_francisco_do_sul.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSaoFranciscoDoSulSpider(FecamGazetteSpider):
+    name = "sc_sao_francisco_do_sul"
+    FECAM_QUERY = 'cod_entidade:245'
+    TERRITORY_ID = "4216206"
