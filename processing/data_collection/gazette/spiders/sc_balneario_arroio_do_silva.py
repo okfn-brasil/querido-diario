c6946956853e056455faf03d9e2a12c4393fb370
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:52 -0300

    sc_balneario_arroio_do_silva.py: adds spider for Balneário Arroio do Silva
    Adds a new spider for Balneário Arroio do Silva - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_balneario_arroio_do_silva.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScBalnearioArroioDoSilvaSpider(FecamGazetteSpider):
+    name = "sc_balneario_arroio_do_silva"
+    FECAM_QUERY = 'cod_entidade:29'
+    TERRITORY_ID = "4201950"
