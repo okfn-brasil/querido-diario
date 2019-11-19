43869e371c30146e89a0ede507634e2883dd120e
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:57 -0300

    sc_celso_ramos.py: adds spider for Celso Ramos
    Adds a new spider for Celso Ramos - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_celso_ramos.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCelsoRamosSpider(FecamGazetteSpider):
+    name = "sc_celso_ramos"
+    FECAM_QUERY = 'cod_entidade:68'
+    TERRITORY_ID = "4204152"
