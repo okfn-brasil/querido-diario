567e652d0dc723cec6c9a52d41a367c281bb9227
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 21:45:42 -0300

    sc_saudades.py: adds spider for Saudades
    Adds a new spider for Saudades - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_saudades.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSaudadesSpider(FecamGazetteSpider):
+    name = "sc_saudades"
+    FECAM_QUERY = 'cod_entidade:260'
+    TERRITORY_ID = "4217303"
