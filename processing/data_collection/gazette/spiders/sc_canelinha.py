af21eeca74ffbde657f3ae134c2dc186290b1025
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:56 -0300

    sc_canelinha.py: adds spider for Canelinha
    Adds a new spider for Canelinha - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_canelinha.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCanelinhaSpider(FecamGazetteSpider):
+    name = "sc_canelinha"
+    FECAM_QUERY = 'cod_entidade:61'
+    TERRITORY_ID = "4203709"
