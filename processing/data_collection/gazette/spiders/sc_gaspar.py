3dfa1e3663cd5ba8c53686c74919bc3248b442da
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:36:37 -0300

    sc_gaspar.py: remove imports
    Removes imports statements which is not needed.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- processing/data_collection/gazette/spiders/sc_gaspar.py
+++ processing/data_collection/gazette/spiders/sc_gaspar.py
@@ -3,5 +3,5 @@ from gazette.spiders.base import FecamGazetteSpider
 
 class ScGasparSpider(FecamGazetteSpider):
     name = "sc_gaspar"
-    FECAM_QUERY = 'entidade:"Prefeitura municipal de Gaspar"'
+    FECAM_QUERY = 'cod_entidade:100'
     TERRITORY_ID = "4205902"
