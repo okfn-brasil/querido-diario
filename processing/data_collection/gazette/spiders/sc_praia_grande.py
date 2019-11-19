679cd2ac29a13b0c30300045af0b017c0601d9c3
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:25:59 -0300

    sc_praia_grande.py: adds spider for Praia Grande
    Adds a new spider for Praia Grande - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_praia_grande.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScPraiaGrandeSpider(FecamGazetteSpider):
+    name = "sc_praia_grande"
+    FECAM_QUERY = 'cod_entidade:209'
+    TERRITORY_ID = "4213807"
