b23dfa99d02738ba473c2c8cb6c85dd0981dacb7
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:55 -0300

    sc_cacador.py: adds spider for Caçador
    Adds a new spider for Caçador - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cacador.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCacadorSpider(FecamGazetteSpider):
+    name = "sc_cacador"
+    FECAM_QUERY = 'cod_entidade:53'
+    TERRITORY_ID = "4203006"
