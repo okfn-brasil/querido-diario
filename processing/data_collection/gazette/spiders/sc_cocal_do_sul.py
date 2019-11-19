13b748ba2d97a431b55e0f1fd92ca2b1df006dea
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:58 -0300

    sc_cocal_do_sul.py: adds spider for Cocal do Sul
    Adds a new spider for Cocal do Sul - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cocal_do_sul.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCocalDoSulSpider(FecamGazetteSpider):
+    name = "sc_cocal_do_sul"
+    FECAM_QUERY = 'cod_entidade:72'
+    TERRITORY_ID = "4204251"
