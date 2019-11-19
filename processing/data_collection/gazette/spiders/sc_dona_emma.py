3933fd347a61244ee3617fdb1b29518e4a6a4684
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:00 -0300

    sc_dona_emma.py: adds spider for Dona Emma
    Adds a new spider for Dona Emma - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_dona_emma.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScDonaEmmaSpider(FecamGazetteSpider):
+    name = "sc_dona_emma"
+    FECAM_QUERY = 'cod_entidade:85'
+    TERRITORY_ID = "4205100"
