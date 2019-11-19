4b81a773b040b8b48fa3cfad1bf818f36e92600b
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:54 -0300

    sc_blumenau.py: adds spider for Blumenau
    Adds a new spider for Blumenau - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_blumenau.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScBlumenauSpider(FecamGazetteSpider):
+    name = "sc_blumenau"
+    FECAM_QUERY = 'cod_entidade:41'
+    TERRITORY_ID = "4202404"
