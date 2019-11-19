0c887ab98d257a4df53f0b4cee216ab92492dc0a
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:25:23 -0300

    sc_presidente_castello_branco.py: adds spider for Presidente Castello Branco
    Adds a new spider for Presidente Castello Branco - SC.
    It uses the as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_presidente_castello_branco.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScPresidenteCastelloBrancoSpider(FecamGazetteSpider):
+    name = "sc_presidente_castello_branco"
+    FECAM_QUERY = 'cod_entidade:210'
+    TERRITORY_ID = "4213906"
