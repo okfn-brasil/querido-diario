43e79fc65d5e058f011e0fba7aad1b8a3b1c9c62
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:00 -0300

    sc_entre_rios.py: adds spider for Entre Rios
    Adds a new spider for Entre Rios - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_entre_rios.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScEntreRiosSpider(FecamGazetteSpider):
+    name = "sc_entre_rios"
+    FECAM_QUERY = 'cod_entidade:87'
+    TERRITORY_ID = "4205175"
