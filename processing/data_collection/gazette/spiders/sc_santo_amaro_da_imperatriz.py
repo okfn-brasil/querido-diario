9f9bfc0a744d0120f7bb97e6c2d053015b798725
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:05:41 -0300

    sc_santo_amaro_da_imperatriz.py: adds spider for Santo Amaro da Imperatriz
    Adds a new spider for Santo Amaro da Imperatriz - SC.
    It uses the as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_santo_amaro_da_imperatriz.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSantoAmaroDaImperatrizSpider(FecamGazetteSpider):
+    name = "sc_santo_amaro_da_imperatriz"
+    FECAM_QUERY = 'cod_entidade:238'
+    TERRITORY_ID = "4215703"
