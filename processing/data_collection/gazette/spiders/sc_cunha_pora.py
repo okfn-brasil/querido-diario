ac4dd1251710a6ab5a253c45960b36c4bde8fd03
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:59 -0300

    sc_cunha_pora.py: adds spider for Cunha Porã
    Adds a new spider for Cunha Porã - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cunha_pora.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCunhaPoraSpider(FecamGazetteSpider):
+    name = "sc_cunha_pora"
+    FECAM_QUERY = 'cod_entidade:80'
+    TERRITORY_ID = "4204707"
