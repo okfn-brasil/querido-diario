91a30084bdd07f362f0cf0e1fa5ebaab2782732b
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:57 -0300

    sc_capinzal.py: adds spider for Capinzal
    Adds a new spider for Capinzal - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_capinzal.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCapinzalSpider(FecamGazetteSpider):
+    name = "sc_capinzal"
+    FECAM_QUERY = 'cod_entidade:64'
+    TERRITORY_ID = "4203907"
