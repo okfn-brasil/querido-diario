f4eddd549babde0ab64a6afdfa17ca506b67b695
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:26:28 -0300

    sc_pouso_redondo.py: adds spider for Pouso Redondo
    Adds a new spider for Pouse Redondo - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_pouso_redondo.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScPousoRedondoSpider(FecamGazetteSpider):
+    name = "sc_pouso_redondo"
+    FECAM_QUERY = 'cod_entidade:208'
+    TERRITORY_ID = "4213708"
