d6ad9727052d053cffe5a918d4cc1b308cf32cb1
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:00 -0300

    sc_doutor_pedrinho.py: adds spider for Doutor Pedrinho
    Adds a new spider for Doutor Pedrinho - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_doutor_pedrinho.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScDoutorPedrinhoSpider(FecamGazetteSpider):
+    name = "sc_doutor_pedrinho"
+    FECAM_QUERY = 'cod_entidade:86'
+    TERRITORY_ID = "4205159"
