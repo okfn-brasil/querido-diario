0dd149e43bf28c5d5b9eaab01f76e4bd40e7c8b0
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:14 -0300

    sc_nova_veneza.py: adds spider for Nova Veneza
    Adds a new spider for Nova Veneza - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_nova_veneza.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScNovaVenezaSpider(FecamGazetteSpider):
+    name = "sc_nova_veneza"
+    FECAM_QUERY = 'cod_entidade:177'
+    TERRITORY_ID = "4211603"
