56623cce04d2aa78d3de3c1fa3dad52c7d9032df
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:08 -0300

    sc_lacerdopolis.py: adds spider for Lacerdópolis
    Adds a new spider for Lacerdópolis - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_lacerdopolis.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScLacerdopolisSpider(FecamGazetteSpider):
+    name = "sc_lacerdopolis"
+    FECAM_QUERY = 'cod_entidade:144'
+    TERRITORY_ID = "4209201"
