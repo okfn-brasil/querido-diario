fd20b3fbc0f42a519d0cb1fc19c6631e43690cd8
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:58 -0300

    sc_coronel_martins.py: adds spider for Coronel Martins
    Adds a new spider for Coronel Martins - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_coronel_martins.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCoronelMartinsSpider(FecamGazetteSpider):
+    name = "sc_coronel_martins"
+    FECAM_QUERY = 'cod_entidade:76'
+    TERRITORY_ID = "4204459"
