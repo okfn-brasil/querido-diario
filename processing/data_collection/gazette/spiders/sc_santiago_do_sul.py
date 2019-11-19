e3f73582626073a617e2669dd8078669b9e1de4c
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:06:22 -0300

    sc_santiago_do_sul.py: adds spider for Santiago do Sul
    Adds a new spider for Santiago do Sul  - SC. It uses the
    as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_santiago_do_sul.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSantiagoDoSulSpider(FecamGazetteSpider):
+    name = "sc_santiago_do_sul"
+    FECAM_QUERY = 'cod_entidade:237'
+    TERRITORY_ID = "4215695"
