285a95fbb52b1b5b01468756ff25d23ac432bb1c
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:14 -0300

    sc_ouro_verde.py: adds spider for Ouro Verde
    Adds a new spider for Ouro Verde - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_ouro_verde.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScOuroVerdeSpider(FecamGazetteSpider):
+    name = "sc_ouro_verde"
+    FECAM_QUERY = 'cod_entidade:182'
+    TERRITORY_ID = "4211850"
