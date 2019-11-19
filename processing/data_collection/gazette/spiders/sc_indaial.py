20bbe97463bb6a696306f22b57c716111d23e817
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:04 -0300

    sc_indaial.py: adds spider for Indaial
    Adds a new spider for Indaial - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_indaial.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScIndaialSpider(FecamGazetteSpider):
+    name = "sc_indaial"
+    FECAM_QUERY = 'cod_entidade:118'
+    TERRITORY_ID = "4207502"
