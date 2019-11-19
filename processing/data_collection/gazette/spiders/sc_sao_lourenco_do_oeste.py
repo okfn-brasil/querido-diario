c2f78bbf62f51a958f012ca7613ad5835018784f
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 21:48:47 -0300

    sc_sao_lourenco_do_oeste.py: adds spider for São Lourenço do Oeste
    Adds a new spider for São Lourenço do Oeste - SC.
    It uses the as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_sao_lourenco_do_oeste.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSaoLourencoDoOesteSpider(FecamGazetteSpider):
+    name = "sc_sao_lourenco_do_oeste"
+    FECAM_QUERY = 'cod_entidade:254'
+    TERRITORY_ID = "4216909"
