71802ba9e5b828dc53b4366f0125704631f78f1e
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:59 -0300

    sc_cunhatai.py: adds spider for Cunhataí
    Adds a new spider for Cunhataí - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cunhatai.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCunhataiSpider(FecamGazetteSpider):
+    name = "sc_cunhatai"
+    FECAM_QUERY = 'cod_entidade:81'
+    TERRITORY_ID = "4204756"
