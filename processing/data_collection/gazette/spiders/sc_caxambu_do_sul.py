7455720306e892087e366c633567b6fb7435308f
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:57 -0300

    sc_caxambu_do_sul.py: adds spider for Caxambu do Sul
    Adds a new spider for Caxambu do Sul - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_caxambu_do_sul.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCaxambuDoSulSpider(FecamGazetteSpider):
+    name = "sc_caxambu_do_sul"
+    FECAM_QUERY = 'cod_entidade:67'
+    TERRITORY_ID = "4204103"
