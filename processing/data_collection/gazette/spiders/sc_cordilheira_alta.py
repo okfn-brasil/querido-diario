2afcff21b8d20d80ed34e3bc081d911e1b832f54
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:58 -0300

    sc_cordilheira_alta.py: adds spider for Cordilheira Alta
    Adds a new spider for Cordilheira Alta - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_cordilheira_alta.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScCordilheiraAltaSpider(FecamGazetteSpider):
+    name = "sc_cordilheira_alta"
+    FECAM_QUERY = 'cod_entidade:74'
+    TERRITORY_ID = "4204350"
