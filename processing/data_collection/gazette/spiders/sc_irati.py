5e9e676b986070f3d7f5bd4c1fd3cd60f32a5e4a
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:06 -0300

    sc_irati.py: adds spider for Irati
    Adds a new spider for Irati - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_irati.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScIratiSpider(FecamGazetteSpider):
+    name = "sc_irati"
+    FECAM_QUERY = 'cod_entidade:126'
+    TERRITORY_ID = "4207858"
