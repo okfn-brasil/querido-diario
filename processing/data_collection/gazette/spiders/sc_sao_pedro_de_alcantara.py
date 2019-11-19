bb47062afc7c28ee98e1ea43dc80e104c1e77e04
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 21:46:09 -0300

    sc_sao_pedro_de_alcantara.py: adds spider for São Pedro de Alcântara
    Adds a new spider for São Pedro de Alcântara - SC. It uses the as
    base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_sao_pedro_de_alcantara.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSaoPedroDeAlcantaraSpider(FecamGazetteSpider):
+    name = "sc_sao_pedro_de_alcantara"
+    FECAM_QUERY = 'cod_entidade:259'
+    TERRITORY_ID = "4217253"
