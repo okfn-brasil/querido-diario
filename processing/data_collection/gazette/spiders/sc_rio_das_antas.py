93923ab6965df1f1b87b5c194a5ba6ba24fbd72b
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:19:22 -0300

    sc_rio_das_antas.py: adds spider for Rio das Antas
    Adds a new spider for Rio das Antas - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_rio_das_antas.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScRioDasAntasSpider(FecamGazetteSpider):
+    name = "sc_rio_das_antas"
+    FECAM_QUERY = 'cod_entidade:216'
+    TERRITORY_ID = "4214409"
