f642e2fb65ff0cb9bfc1e9a0f5ebf1580ac1233e
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:32:53 -0300

    sc_barra_bonita.py: adds spider for Barra Bonita
    Adds a new spider for Barra Bonita - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_barra_bonita.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScBarraBonitaSpider(FecamGazetteSpider):
+    name = "sc_barra_bonita"
+    FECAM_QUERY = 'cod_entidade:35'
+    TERRITORY_ID = "4202099"
