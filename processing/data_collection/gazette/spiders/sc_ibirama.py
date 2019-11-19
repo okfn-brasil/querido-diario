da1570ca7adeb62592e1ed1a0da2060cecce65e7
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:04 -0300

    sc_ibirama.py: adds spider for Ibirama
    Adds a new spider for Ibirama - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_ibirama.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScIbiramaSpider(FecamGazetteSpider):
+    name = "sc_ibirama"
+    FECAM_QUERY = 'cod_entidade:112'
+    TERRITORY_ID = "4206900"
