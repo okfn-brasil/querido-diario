b6aa14fb63c0b7fb668ff615b9c38db66134e02b
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:00:38 -0300

    sc_sao_joao_batista.py: adds spider for São João Batista
    Adds a new spider for São João Batista - SC. It uses
    the as base spider the FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_sao_joao_batista.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScSaoJoaoBatistaSpider(FecamGazetteSpider):
+    name = "sc_sao_joao_batista"
+    FECAM_QUERY = 'cod_entidade:246'
+    TERRITORY_ID = "4216305"
