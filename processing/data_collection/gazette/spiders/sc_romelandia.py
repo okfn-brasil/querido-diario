690b1c25a62c97acffe491b007435aea17dc26ce
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 22:13:41 -0300

    sc_romelandia.py: adds spider for Romelândia
    Adds a new spider for Romelândia - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_romelandia.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScRomelandiaSpider(FecamGazetteSpider):
+    name = "sc_romelandia"
+    FECAM_QUERY = 'cod_entidade:226'
+    TERRITORY_ID = "4215208"
