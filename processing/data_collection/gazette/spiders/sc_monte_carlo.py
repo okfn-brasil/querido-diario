79e877a26d2da0f3370e613541f04fd5ca1ff330
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:11 -0300

    sc_monte_carlo.py: adds spider for Monte Carlo
    Adds a new spider for Monte Carlo - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_monte_carlo.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScMonteCarloSpider(FecamGazetteSpider):
+    name = "sc_monte_carlo"
+    FECAM_QUERY = 'cod_entidade:169'
+    TERRITORY_ID = "4211058"
