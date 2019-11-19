c8fbbb586a91a20eac3444a936f2c21cabd041b5
José Guilherme Vanz <jvanz@jvanz.com>
2019-11-18 23:33:16 -0300

    sc_paraiso.py: adds spider for Paraíso
    Adds a new spider for Paraíso - SC. It uses the as base spider the
    FecamGazetteSpider class.
    
    Issue https://github.com/okfn-brasil/diario-oficial/issues/134
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ processing/data_collection/gazette/spiders/sc_paraiso.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import FecamGazetteSpider
+
+
+class ScParaisoSpider(FecamGazetteSpider):
+    name = "sc_paraiso"
+    FECAM_QUERY = 'cod_entidade:190'
+    TERRITORY_ID = "4212239"
