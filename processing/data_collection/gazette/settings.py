5d86099b1a9e24cd6d693110ab17972b9f88fcf0
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-02-08 14:44:00 -0300

    Remove temporary pipeline due to scrapy update

--- processing/data_collection/gazette/settings.py
+++ processing/data_collection/gazette/settings.py
@@ -4,7 +4,6 @@ NEWSPIDER_MODULE = "gazette.spiders"
 ROBOTSTXT_OBEY = False
 ITEM_PIPELINES = {
     "gazette.pipelines.GazetteDateFilteringPipeline": 50,
-    "gazette.parser.GazetteFilesPipeline": 60,
     "scrapy.pipelines.files.FilesPipeline": 100,
     "gazette.pipelines.ExtractTextPipeline": 200,
     "gazette.pipelines.PostgreSQLPipeline": 300,
