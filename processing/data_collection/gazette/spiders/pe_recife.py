8263de8116294bde27489897b546602ee7efd5bc
Giulio Carvalho <gcc@cin.ufpe.br>
2020-06-23 01:27:45 -0300

    Add support for more dates and extra editions
    The first available date on
    "https://www.cepe.com.br/prefeituradiario/diarios.txt" is in 2017.
    But this system has gazettes available since 2015. Manual date
    generation had to be done to achieve that.

--- processing/data_collection/gazette/spiders/pe_recife.py
+++ processing/data_collection/gazette/spiders/pe_recife.py
@@ -1,21 +1,21 @@
 """Recife's gazettes spider
 
 This spider is implemented to crawl Recife's `newest gazette system
-<https://www.cepe.com.br/>`_. This system covers gazettes from 2017 to
+<https://www.cepe.com.br/>`_. This system covers gazettes from 30/04/2015 to
 current days. There is also a `deprecated system
-<http://www.recife.pe.gov.br/diariooficial-acervo/>`_ which covers gazettes from 2001 to
-2016 which is still not implemented.
+<http://www.recife.pe.gov.br/diariooficial-acervo/>`_ which covers gazettes from 2001
+to 2016 which is still not implemented.
 
 Implementation details:
-    The spider get all dates with gazettes published fetching the diarios.txt
-    file. Using this data, it generates the url for the files hosted in the
-    CEPE website.
-
+    The spider generates all dates from the first available date (30/04/2015) until
+    today. These dates are used to fetch the name of all available gazettes on that
+    date, including Recife's. Then, the respective documents for Recife's gazettes
+    are requested and the rest if filtered out.
 
 Gazettes examples:
+    - http://200.238.105.211/cadernos/2020/20200326/8-PrefeituradoRecifeEdicaoExtra/PrefeituradoRecifeEdicaoExtra(20200326).pdf
     - http://200.238.105.211/cadernos/2020/20200613/8-PrefeituradoRecife/PrefeituradoRecife(20200613).pdf
     - http://200.238.105.211/cadernos/2020/20200616/8-PrefeituradoRecife/PrefeituradoRecife(20200616).pdf
-    - http://200.238.105.211/cadernos/2020/20200618/8-PrefeituradoRecife/PrefeituradoRecife(20200618).pdf
 
 """
 
@@ -23,6 +23,7 @@ import datetime as dt
 import re
 
 import dateparser
+from dateutil.rrule import DAILY, rrule
 import scrapy
 
 from gazette.items import Gazette
@@ -33,44 +34,66 @@ class PeRecifeSpider(BaseGazetteSpider):
     name = "pe_recife"
     TERRITORY_ID = "2611606"
 
-    AVAILABLE_DATES_URL = "https://www.cepe.com.br/prefeituradiario/diarios.txt"
-    BASE_URL = "http://200.238.105.211/cadernos/{full_year}/{full_date}/8-PrefeituradoRecife/PrefeituradoRecife({full_date}).pdf"
+    FIRST_AVAILABLE_DATE = dt.date(2015, 4, 30)
+
+    AVAILABLE_DATES_URL = "https://ws.cepe.com.br/publicar/dows.php"
+    EDITIONS_IN_DATE_URL = "https://ws.cepe.com.br/publicar/dows.php?&dia={full_date}"
+    GAZETTE_URL = "http://200.238.105.211/cadernos/{full_year}/{full_date}/{edition_type}/{edition_type_name_only}({full_date}).pdf"
 
     def start_requests(self):
-        # first request just get the txt file with all available dates
-        yield scrapy.Request(self.AVAILABLE_DATES_URL, self.parse_diarios_file)
-
-    def parse_diarios_file(self, response):
         """
-        Callback function called to parse the diarios.txt file
+        Requests documents which specifies edition types available for dates.
         """
-        # Parse the dates available and return a request for get the page for
-        # each one
-        dates = self._parse_available_dates(raw_dates=response.text)
+        dates = rrule(
+            freq=DAILY, dtstart=self.FIRST_AVAILABLE_DATE, until=dt.date.today()
+        )
 
-        # generate the gazette item for each date found
         for date in dates:
-            url = self.BASE_URL.format(
-                full_date=date.strftime("%Y%m%d"), full_year=date.strftime("%Y"),
+            yield scrapy.Request(
+                url=self.EDITIONS_IN_DATE_URL.format(full_date=date.strftime("%Y%m%d")),
+                meta={"date": date},
+                callback=self.parse_editions_in_date,
             )
-            self.log(url)
+
+    def parse_editions_in_date(self, response):
+        """
+        Parses available editions to request only Recife's gazette documents.
+        """
+        recife_editions = self._find_recife_editions(response.text)
+        date = response.meta["date"]
+
+        for edition in recife_editions:
+            url = self.GAZETTE_URL.format(
+                full_year=date.strftime("%Y"),
+                full_date=date.strftime("%Y%m%d"),
+                edition_type=edition,
+                edition_type_name_only=re.search("\w+$", edition).group(),
+            )
+
             yield Gazette(
                 date=date,
                 file_urls=[url],
                 territory_id=self.TERRITORY_ID,
+                is_extra_edition=self._is_extra(edition),
                 scraped_at=dt.datetime.utcnow(),
                 power="executive_legislative",
             )
 
-    def _parse_available_dates(self, raw_dates):
+    def _find_recife_editions(self, text):
         """
-        Parses the diarios.txt file.
+        Finds editions related to Recife's gazette.
 
-        This generator method gets the string of the diarios.txt file get
-        from the gazette site and parses it into date objects.
-
-        Returns dates found in the diarios.txt file
+        Filters out entries which are not exclusively related to Recife
+        (e.g. "1-PoderExecutivo").
         """
-        available_dates = raw_dates.split("&")
-        for date in filter(str.isdigit, available_dates):
-            yield dateparser.parse(date, settings={"DATE_ORDER": "DMY"})
+        return (
+            edition
+            for edition in text.split("&")
+            if re.search("prefeituradorecife", edition, re.IGNORECASE)
+        )
+
+    def _is_extra(self, edition):
+        """
+        Checks if edition is extra or not.
+        """
+        return re.search("extra", edition, re.IGNORECASE) is not None
