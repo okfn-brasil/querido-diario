967d42c3c8b3ea816fa47e45a8a70506b48e449b
Victor Torres <vpaivatorres@gmail.com>
2020-06-26 11:37:59 -0300

    Black

--- processing/data_collection/gazette/spiders/rn_natal.py
+++ processing/data_collection/gazette/spiders/rn_natal.py
@@ -9,28 +9,28 @@ from gazette.spiders.base import BaseGazetteSpider
 
 class RnNatalSpider(BaseGazetteSpider):
 
-    name = 'rn_natal'
-    allowed_domains = ['www.natal.rn.gov.br']
+    name = "rn_natal"
+    allowed_domains = ["www.natal.rn.gov.br"]
 
     def start_requests(self):
-        base_url = 'http://www.natal.rn.gov.br/dom/'
+        base_url = "http://www.natal.rn.gov.br/dom/"
         for year in range(2003, datetime.now().year + 1):
             for month in range(1, 13):
-                data = dict(ano=str(year), mes=str(month).zfill(2), list='Listar')
+                data = dict(ano=str(year), mes=str(month).zfill(2), list="Listar")
                 yield FormRequest(url=base_url, formdata=data)
 
     def parse(self, response):
-        for entry in response.css('#texto a'):
-            file_url = response.urljoin(entry.css('::attr(href)').get())
-            title = entry.css('::text').get()
-            date = dateparser.parse(title.split('-')[-1], languages=['pt']).date()
-            extra_edition = 'Extra' in title
+        for entry in response.css("#texto a"):
+            file_url = response.urljoin(entry.css("::attr(href)").get())
+            title = entry.css("::text").get()
+            date = dateparser.parse(title.split("-")[-1], languages=["pt"]).date()
+            extra_edition = "Extra" in title
 
             yield Gazette(
                 date=date,
                 file_urls=[file_url],
                 is_extra_edition=extra_edition,
-                territory_id='2408102',
-                power='executive',
-                scraped_at=datetime.utcnow()
+                territory_id="2408102",
+                power="executive",
+                scraped_at=datetime.utcnow(),
             )
