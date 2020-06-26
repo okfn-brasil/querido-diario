a3a7b3c7be53fe83dbd827dcb17c13999cabd2b8
Victor Torres <vpaivatorres@gmail.com>
2020-06-26 10:56:59 -0300

    Refactor

--- processing/data_collection/gazette/spiders/rn_natal.py
+++ processing/data_collection/gazette/spiders/rn_natal.py
@@ -11,8 +11,6 @@ from gazette.spiders.base import BaseGazetteSpider
 
 class RnNatalSpider(BaseGazetteSpider):
 
-    TERRITORY_ID = '2408102'
-
     name = 'rn_natal'
     allowed_domains = ['www.natal.rn.gov.br']
 
@@ -28,13 +26,13 @@ class RnNatalSpider(BaseGazetteSpider):
             url = response.urljoin(element.css('::attr(href)').get())
             link_text = element.css('::text').get()
             date = dateparser.parse(link_text.split(' - ')[-1], languages=['pt']).date()
-            extra_edition = "Extra" in link_text
+            extra_edition = 'Extra' in link_text
 
             yield Gazette(
                 date=date,
                 file_urls=[url],
                 is_extra_edition=extra_edition,
-                territory_id=self.TERRITORY_ID,
+                territory_id='2408102',
                 power='executive',
                 scraped_at=datetime.utcnow()
             )
