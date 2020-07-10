6076e35e278f30a7972b9e55070ab402aac21008
≈ Laerte Pereira <laertefbk@gmail.com>
2020-07-10 10:57:43 -0300

    Bauru Gazette Spider

---
+++ processing/data_collection/gazette/spiders/sp_bauru.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from scrapy import Request, Spider
+from gazette.items import Gazette
+from datetime import datetime
+import dateparser
+
+
+class SpBauruSpider(Spider):
+    TERRITORY_ID = '3506003'
+    BASE_XPATH = "//div[@class='col-md-12']"
+    name = 'sp_bauru'
+    allowed_domains = ['bauru.sp.gov.br']
+    start_urls = ['https://www2.bauru.sp.gov.br/juridico/diariooficial.aspx']
+
+    def parse(self, response):
+        avaliable_years = response.xpath(f"{self.BASE_XPATH}"
+                                         "/ul/li/a/text()").extract()
+
+        for year in [avaliable_years[0]]:
+            yield Request(url=f"{response.url}?a={year}",
+                          callback=self.parse_year)
+
+    def parse_year(self, response):
+
+        months_links = response.xpath(f"{self.BASE_XPATH}"
+                                      "/ul/li/ul/li/a/@href").extract()
+
+        for month_link in months_links:
+            yield Request(url=f"https://www2.bauru.sp.gov.br/juridico/{month_link}",
+                          callback=self.parse_month)
+
+    def parse_month(self, response):
+        gazettes_seletor = response.xpath(f"{self.BASE_XPATH}"
+                                          "/ul/li/ul/li/ul/li/a")
+
+        for gazette_seletor in gazettes_seletor:
+            date = dateparser.parse(gazette_seletor.xpath("//b/text()")
+                                    .get()
+                                    .split(":")[0]
+                                    .strip(),
+                                    date_formats=["%d/%m/%Y"]).date()
+            url = response.urljoin(gazette_seletor.xpath("@href").get())
+
+            yield Gazette(
+                date=date,
+                file_urls=[url],
+                is_extra_edition='especial' in url.lower(),
+                territory_id=self.TERRITORY_ID,
+                scraped_at=datetime.utcnow(),
+                power="executive"
+            )
