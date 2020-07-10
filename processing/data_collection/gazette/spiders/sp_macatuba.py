1da5349099d043b9ac8136bf0e1b9e0310601dc8
Giulio Carvalho <gcc@cin.ufpe.br>
2020-07-10 04:03:02 -0300

    Add Sigpub base spider and all subspiders
    SigpubGazetteSpider is a base spider for many systems. Most of them are
    associations of cities, but some are not.
    
    Spiders added:
    - al_associacao_municipios
    - am_associacao_municipios
    - ba_associacao_municipios
    - ce_associacao_municipios
    - go_associacao_municipios_agm
    - go_associacao_municipios_fgm
    - mg_associacao_municipios
    - ms_associacao_municipios
    - mt_associacao_municipios
    - pa_associacao_municipios
    - pb_associacao_municipios
    - pe_associacao_municipios
    - pi_associacao_municipios
    - pr_associacao_municipios
    - rj_associacao_municipios
    - rn_associacao_municipios
    - ro_associacao_municipios
    - rr_associacao_municipios
    - rs_associacao_municipios
    - se_associacao_municipios
    - sp_associacao_municipios
    - sp_macatuba
    - sp_monte_alto_sigpub

---
+++ processing/data_collection/gazette/spiders/sp_macatuba.py
@@ -0,0 +1,7 @@
+from gazette.spiders.base import SigpubGazetteSpider
+
+
+class SpMacatubaSpider(SigpubGazetteSpider):
+    name = "sp_macatuba"
+    TERRITORY_ID = "3528007"
+    CALENDAR_URL = "http://www.diariomunicipal.com.br/macatuba"
