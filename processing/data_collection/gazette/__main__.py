5fae54469bb5f5f481400aa016c90df7b8e9d603
Felipe Rodrigues <felipe@felipevr.com>
2020-08-15 16:14:33 -0300

    Refactor main

--- processing/data_collection/gazette/__main__.py
+++ processing/data_collection/gazette/__main__.py
@@ -4,7 +4,14 @@ import re
 from pathlib import Path
 
 
-def main():
+def list_spiders(path="spiders"):
+    """
+    Lists all the spiders available and breaks their names
+    in state and city.
+
+    Returns a dictionary containing the state's initials
+    and a list of its cities.
+    """
     root = Path(__file__).parent
     spiders = root / "spiders"
     pattern = r"^([a-z]{2})_([a-z_]+)\.py$"
@@ -21,11 +28,29 @@ def main():
             city = city.replace("_", " ").title()
             results[state].append(city)
 
-    for state in sorted(results.keys()):
+    return results
+
+
+def print_spiders_list(spider_list):
+    """
+    Given a dictionary as returned by `lists_spiders`,
+    prints them in a user-friendly format.
+
+    Currently, it will order the keys alphabetically
+    and print each city on a separate line, also
+    alphabetically. At the end of each key, it
+    prints a separator.
+    """
+    for state in sorted(spider_list.keys()):
         print(f"Cidades de {state}")
-        for city in sorted(results[state]):
+        for city in sorted(spider_list[state]):
             print(f"\t{city}")
         print("*-" * 20, "*", sep="")
+
+
+def main():
+    results = list_spiders()
+    print_spiders_list(results)
 
 
 if __name__ == "__main__":
