6dbb66c8e7e2e8e66bdbaabd4bee5472a58fbfba
Felipe Rodrigues <felipe@felipevr.com>
2020-08-15 16:08:14 -0300

    Enhance output

--- processing/data_collection/gazette/__main__.py
+++ processing/data_collection/gazette/__main__.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from os import stat
 import re
 from pathlib import Path
 
@@ -6,7 +7,7 @@ from pathlib import Path
 def main():
     root = Path(__file__).parent
     spiders = root / "spiders"
-    pattern = r"([a-z]{2})_([a-z_]+)\.py"
+    pattern = r"^([a-z]{2})_([a-z_]+)\.py$"
 
     results = defaultdict(list)
 
@@ -16,10 +17,15 @@ def main():
 
         if matches:
             state, city = matches[0]
+            state = state.upper()
             city = city.replace("_", " ").title()
             results[state].append(city)
 
-    print(results)
+    for state in sorted(results.keys()):
+        print(f"Cidades de {state}")
+        for city in sorted(results[state]):
+            print(f"\t{city}")
+        print("*-" * 20, "*", sep="")
 
 
 if __name__ == "__main__":
