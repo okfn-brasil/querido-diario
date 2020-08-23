293dbd1f967ccc9f3f943cfd6ef6a614beab663d
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-08-23 16:43:01 -0300

    Retrieve gazette attachments

--- processing/data_collection/gazette/spiders/pi_teresina.py
+++ processing/data_collection/gazette/spiders/pi_teresina.py
@@ -2,6 +2,7 @@ import scrapy
 import dateparser
 
 from datetime import datetime
+from urllib.parse import urlencode
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -9,30 +10,31 @@ from gazette.spiders.base import BaseGazetteSpider
 
 class PiTeresina(BaseGazetteSpider):
     TERRITORY_ID = "2211001"
+    BASE_URL = "http://dom.pmt.pi.gov.br/lista_diario.php"
     name = "pi_teresina"
-    start_urls = ["http://dom.pmt.pi.gov.br/lista_diario.php"]
+    allowed_domains = ["dom.pmt.pi.gov.br"]
+    start_urls = [BASE_URL]
 
     def parse(self, response):
-        total_items = int(response.css("#meio > div.texto > span > b::text").get())
+        total_items = int(response.css(".texto span b::text").get())
         total_pages = self.calculate_number_of_pages(total_items, 10)
 
-        pages_urls = [
-            "http://dom.pmt.pi.gov.br/lista_diario.php?pagina={}".format(i)
-            for i in range(1, total_pages + 1)
-        ]
-        for url in pages_urls:
+        for i in range(1, total_pages + 1):
+            params = {"pagina": i}
+            url = "{}?{}".format(self.BASE_URL, urlencode(params))
             yield scrapy.Request(url=url, callback=self.parse_page)
 
     def parse_page(self, response):
-        for entry in response.css("tbody > tr"):
-            file_url = entry.css("a::attr(href)").get()
+        for entry in response.css("tbody tr"):
+            # retrieves gazette files and attachments
+            file_urls = entry.css("td a::attr(href)").getall()
 
             date_text = entry.css("td:nth-child(2)::text").get()
             date = dateparser.parse(date_text, languages=["pt"]).date()
 
             yield Gazette(
                 date=date,
-                file_urls=[file_url],
+                file_urls=file_urls,
                 territory_id=self.TERRITORY_ID,
                 power="executive_legislative",
                 scraped_at=datetime.utcnow(),
