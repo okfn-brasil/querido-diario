a1f34beadb3a10de09d3f8c1c5199e212fc88d56
Andre Angeluci <andreb.angelucci@gmail.com>
2020-08-25 00:03:58 -0300

    Add spider for Fernandopolis, Sao Paulo [reformatted]

--- processing/data_collection/gazette/spiders/sp_fernandopolis.py
+++ processing/data_collection/gazette/spiders/sp_fernandopolis.py
@@ -8,19 +8,23 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class SpFernandopolis(BaseGazetteSpider):
-    MUNICIPALITY_ID = '3515509'
-    name = 'sp_fernandopolis'
-    start_urls = ['https://diariotransparente.com.br/publicacoes/frame/spfernandopolispm']
+    MUNICIPALITY_ID = "3515509"
+    name = "sp_fernandopolis"
+    start_urls = [
+        "https://diariotransparente.com.br/publicacoes/frame/spfernandopolispm"
+    ]
 
     def parse(self, response):
         IDX_EXTRA_EDITION_LI = 3
-        list_pubs = response.css('div.row')
+        list_pubs = response.css("div.row")
         for pub in list_pubs:
-            link_date = pub.css('h3>a')
-            date = link_date.css('::text').get()
-            date = parse_date(date, languages=['pt']).date()
-            link = response.urljoin(link_date.attrib['href'])
-            is_extra_edition = pub.css('ul>li::text')[IDX_EXTRA_EDITION_LI].re_first('EXTRA') is None
+            link_date = pub.css("h3>a")
+            date = link_date.css("::text").get()
+            date = parse_date(date, languages=["pt"]).date()
+            link = response.urljoin(link_date.attrib["href"])
+            is_extra_edition = (
+                pub.css("ul>li::text")[IDX_EXTRA_EDITION_LI].re_first("EXTRA") is None
+            )
             yield Gazette(
                 date=date,
                 file_urls=[link],
