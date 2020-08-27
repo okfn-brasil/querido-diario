c1e3d4e706ad3dd6a5523075f0736a22a2d07ad8
Anderson Berg <andersonberg@gmail.com>
2020-08-26 23:36:02 -0300

    refactoring urls

--- processing/data_collection/gazette/spiders/mt_cuiaba.py
+++ processing/data_collection/gazette/spiders/mt_cuiaba.py
@@ -9,31 +9,28 @@ from gazette.spiders.base import BaseGazetteSpider
 
 # based on the website
 FIRST_YEAR = 1967
+BASE_URL = "https://diariooficial.cuiaba.mt.gov.br/api/api/editions"
 
 
 class MtCuiabaSpider(BaseGazetteSpider):
     TERRITORY_ID = 5103403
     name = "mt_cuiaba"
     allowed_domains = ["diariooficial.cuiaba.mt.gov.br"]
-    start_urls = ["https://diariooficial.cuiaba.mt.gov.br/edicoes"]
+    start_urls = ["https://diariooficial.cuiaba.mt.gov.br/"]
 
     def start_requests(self):
-        date_url = "https://diariooficial.cuiaba.mt.gov.br/api/api/editions/published/{year}/{month}"
         today = datetime.today()
         final_year = today.year
         for year in range(FIRST_YEAR, final_year + 1):
             for month in range(1, 13):
-                url = date_url.format(year=year, month=month)
-                yield scrapy.Request(url)
+                date_url = f"{BASE_URL}/published/{year}/{month}"
+                yield scrapy.Request(date_url)
 
     def parse(self, response):
-        base_url = (
-            "https://diariooficial.cuiaba.mt.gov.br/api/api/editions/downloadPdf/{}"
-        )
         editions = json.loads(response.text)["editions"]
         for edition in editions:
             edition_id = edition["id"]
-            edition_url = base_url.format(edition_id)
+            edition_url = f"{BASE_URL}/downloadPdf/{edition_id}"
             edition_date = edition["publication_date"]
 
             yield Gazette(
