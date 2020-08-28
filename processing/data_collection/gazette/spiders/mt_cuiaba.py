ec88898203df111569ea6ae48ade7fa721eba0a8
Anderson Berg <andersonberg@gmail.com>
2020-08-28 00:06:58 -0300

    fix failed responses

--- processing/data_collection/gazette/spiders/mt_cuiaba.py
+++ processing/data_collection/gazette/spiders/mt_cuiaba.py
@@ -1,7 +1,9 @@
 import json
+import time
 from dateparser import parse
 from datetime import datetime
 from scrapy import Request
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -12,11 +14,42 @@ FIRST_YEAR = 1967
 BASE_URL = "https://diariooficial.cuiaba.mt.gov.br/api/api/editions"
 
 
+# class TooManyRequestsRetryMiddleware(RetryMiddleware):
+#     def __init__(self, crawler):
+#         super(TooManyRequestsRetryMiddleware, self).__init__(crawler.settings)
+#         self.crawler = crawler
+
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         return cls(crawler)
+
+#     def process_response(self, request, response, spider):
+#         if response.status == 429:
+#             delay = int(response.headers.get("Retry-After", 25))
+#             self.crawler.engine.pause()
+#             time.sleep(delay)
+#             self.crawler.engine.unpause()
+#             return self._retry(request, "429 response", spider) or response
+#         return response
+
+
 class MtCuiabaSpider(BaseGazetteSpider):
     TERRITORY_ID = 5103403
     name = "mt_cuiaba"
     allowed_domains = ["diariooficial.cuiaba.mt.gov.br"]
     start_urls = ["https://diariooficial.cuiaba.mt.gov.br/"]
+
+    # handle_httpstatus_list = [429]
+
+    custom_settings = {
+        # "RETRY_TIMES": 5,
+        "CONCURRENT_REQUESTS": 3,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 3,
+        "DOWNLOAD_DELAY": 1,
+        # "DOWNLOADER_MIDDLEWARES":{
+        #     "gazette.spiders.mt_cuiaba.TooManyRequestsRetryMiddleware": 98
+        # },
+    }
 
     def start_requests(self):
         today = datetime.today()
@@ -24,9 +57,23 @@ class MtCuiabaSpider(BaseGazetteSpider):
         for year in range(FIRST_YEAR, final_year + 1):
             for month in range(1, 13):
                 date_url = f"{BASE_URL}/published/{year}/{month}"
-                yield Request(date_url)
+                yield Request(
+                    url=date_url,
+                    headers={
+                        "referer": "https://diariooficial.cuiaba.mt.gov.br/edicoes",
+                    }
+                )
 
     def parse(self, response):
+        # if response.status == 429:
+        #     delay = int(response.headers.get("Retry-After", 25))
+        #     self.crawler.stats.inc_value("response_429")
+        #     print(f"Got response 429, start waiting {delay} seconds...")
+        #     time.sleep(delay)
+        #     yield Request(
+        #         response.url,
+        #         dont_filter=True,
+        #     )
         editions = json.loads(response.text)["editions"]
         for edition in editions:
             edition_id = edition["id"]
@@ -38,6 +85,6 @@ class MtCuiabaSpider(BaseGazetteSpider):
                 file_urls=[edition_url],
                 is_extra_edition=edition["suplement"],
                 territory_id=self.TERRITORY_ID,
-                power="executive_legislative",
+                power="executive",
                 scraped_at=datetime.utcnow(),
             )
