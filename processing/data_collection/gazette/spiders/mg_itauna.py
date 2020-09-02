883bc5a081f214a673be4c3ca8cf34037f4bcd09
a3data <pedro.garcias@gmail.com>
2020-09-02 10:54:18 -0300

    add changes in review

--- processing/data_collection/gazette/spiders/mg_itauna.py
+++ processing/data_collection/gazette/spiders/mg_itauna.py
@@ -1,5 +1,4 @@
 import datetime as dt
-
 from dateparser import parse
 from scrapy import Request
 
@@ -14,23 +13,22 @@ class MgItaunaSpider(BaseGazetteSpider):
     start_urls = ["https://www.itauna.mg.gov.br/portal/diario-oficial/"]
 
     def parse(self, response):
+        url = self.start_urls[0] + "{i}/0/0/0/0/"
+        last_page = int(response.xpath("//select/option/text()")[-1].extract())
+        for i in range(1, 1 + last_page):
+            yield Request(url.format(i=i), callback=self.parse_editions_page)
+
+    def parse_editions_page(self, response):
         diarios = response.xpath("//div[@class='d_e_modelo_diario']")
         for diario in diarios:
-            url = (
-                "https://www.itauna.mg.gov.br"
-                + diario.css("a")[1].extract().split('"')[1]
-            )
-            date = diario.xpath("div/span/text()").extract()[0].split()[0]
-            is_extra_edition = True if len(diario.css("i")) == 4 else False
+            href = diario.xpath('.//a[contains(@href, "downloads")]/@href').get()
+            date = diario.xpath("div/span/text()").re_first("\d{2}/\d{2}/\d{4}")
+            is_extra_edition = diario.xpath(".//span[contains(./text(), 'Extra')]").get() is not None
             yield Gazette(
                 date=parse(date, languages=["pt"]).date(),
-                file_urls=[url],
+                file_urls=[response.urljoin(href)],
                 is_extra_edition=is_extra_edition,
                 territory_id=self.TERRITORY_ID,
                 power="executive_legislature",
                 scraped_at=dt.datetime.utcnow(),
             )
-        url = "https://www.itauna.mg.gov.br/portal/diario-oficial/{i}/0/0/0/0/"
-        last_page = int(response.xpath("//select/option/text()")[-1].extract())
-        for i in range(1, 1 + last_page):
-            yield Request(url.format(i=i), callback=self.parse)
