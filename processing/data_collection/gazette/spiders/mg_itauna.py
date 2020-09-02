bb41d3089034d34f489ed212491b7ee1b89c5a3c
a3data <pedro.garcias@gmail.com>
2020-09-02 11:02:38 -0300

    add changes in review

--- processing/data_collection/gazette/spiders/mg_itauna.py
+++ processing/data_collection/gazette/spiders/mg_itauna.py
@@ -23,7 +23,9 @@ class MgItaunaSpider(BaseGazetteSpider):
         for diario in diarios:
             href = diario.xpath('.//a[contains(@href, "downloads")]/@href').get()
             date = diario.xpath("div/span/text()").re_first("\d{2}/\d{2}/\d{4}")
-            is_extra_edition = diario.xpath(".//span[contains(./text(), 'Extra')]").get() is not None
+            is_extra_edition = (
+                diario.xpath(".//span[contains(./text(), 'Extra')]").get() is not None
+            )
             yield Gazette(
                 date=parse(date, languages=["pt"]).date(),
                 file_urls=[response.urljoin(href)],
