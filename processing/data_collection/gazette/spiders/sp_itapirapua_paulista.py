443f25b0d4d9771c8e7cdc4fd2da06be03e424c5
a3data <pedro.garcias@gmail.com>
2020-09-15 23:09:20 -0300

    add cities with instar developer

--- processing/data_collection/gazette/spiders/sp_itapirapua_paulista.py
+++ processing/data_collection/gazette/spiders/sp_itapirapua_paulista.py
@@ -3,6 +3,6 @@ from gazette.spiders.instar_base import BaseInstarSpider
 
 class SpItapirapuaPaulistaSpider(BaseInstarSpider):
     TERRITORY_ID = "3522653"
-    name = "sp_itapira_puapaulista"
+    name = "sp_itapirapua_paulista"
     allowed_domains = ["itapirapuapaulista.sp.gov.br"]
     start_urls = ["https://www.itapirapuapaulista.sp.gov.br/portal/diario-oficial"]
