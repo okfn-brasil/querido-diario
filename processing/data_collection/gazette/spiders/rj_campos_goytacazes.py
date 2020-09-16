e00f5ce774acda75ebb638cece1293b7f078897c
Rodrigo Chamun <rochamun@gmail.com>
2020-09-16 17:46:22 -0300

    Reformat file
    These changes were automatically done by black.

--- processing/data_collection/gazette/spiders/rj_campos_goytacazes.py
+++ processing/data_collection/gazette/spiders/rj_campos_goytacazes.py
@@ -6,13 +6,16 @@ from scrapy import Request
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
-class RjCampoGoytacazesSpider(BaseGazetteSpider):
-    GAZETTE_ELEMENT_CSS = 'ul.ul-licitacoes li'
-    MUNICIPALITY_ID = '3301009'
 
-    allowed_domains = ['www.campos.rj.gov.br']
-    name = 'rj_campos_goytacazes'
-    start_urls = ['https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1&PGporPagina=15']
+class RjCampoGoytacazesSpider(BaseGazetteSpider):
+    GAZETTE_ELEMENT_CSS = "ul.ul-licitacoes li"
+    MUNICIPALITY_ID = "3301009"
+
+    allowed_domains = ["www.campos.rj.gov.br"]
+    name = "rj_campos_goytacazes"
+    start_urls = [
+        "https://www.campos.rj.gov.br/diario-oficial.php?PGpagina=1&PGporPagina=15"
+    ]
 
     def parse(self, response):
         """
@@ -22,24 +25,28 @@ class RjCampoGoytacazesSpider(BaseGazetteSpider):
         """
 
         for element in response.css(self.GAZETTE_ELEMENT_CSS):
-            url = element.css('a::attr(href)').extract_first()
+            url = element.css("a::attr(href)").extract_first()
             gazette_text = element.css("h4::text").extract_first()
-            extra_edition = gazette_text.startswith('Suplemento')
+            extra_edition = gazette_text.startswith("Suplemento")
 
-            date_re = re.search(r'(\d{2} de (.*) de \d{4})', gazette_text)
+            date_re = re.search(r"(\d{2} de (.*) de \d{4})", gazette_text)
             if date_re:
-                date = dateparser.parse(date_re.group(0), languages=['pt']).date()
+                date = dateparser.parse(date_re.group(0), languages=["pt"]).date()
 
                 yield Gazette(
-                  date=date,
-                  file_urls=[url],
-                  is_extra_edition=extra_edition,
-                  municipality_id=self.MUNICIPALITY_ID,
-                  power='executive',
-                  scraped_at=datetime.utcnow(),
+                    date=date,
+                    file_urls=[url],
+                    is_extra_edition=extra_edition,
+                    municipality_id=self.MUNICIPALITY_ID,
+                    power="executive",
+                    scraped_at=datetime.utcnow(),
                 )
 
-        next_url = response.css('.pagination').xpath("//a[contains(text(), 'Proxima')]/@href").extract_first()
+        next_url = (
+            response.css(".pagination")
+            .xpath("//a[contains(text(), 'Proxima')]/@href")
+            .extract_first()
+        )
         if next_url:
             print(next_url)
-            yield  Request(response.urljoin(next_url))
+            yield Request(response.urljoin(next_url))
