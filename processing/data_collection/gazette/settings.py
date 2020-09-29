a8077f99490a1097f66c01de76e7670fdb9cfd53
Renne Rocha <renne@rennerocha.com>
2020-09-28 23:09:58 -0300

    Remove scrapy-deltafetch middleware
    This middleware was introduced to avoid spiders to request data that was
    already requested before, reducing the amount of requests made. However,
    it is not certain that it will reach this goal considering that some
    requests may be the same but the content returned different. A better
    solution to reduce the number of requests may be necessary, but we will
    only know for sure when we start running all the spiders daily.

--- processing/data_collection/gazette/settings.py
+++ processing/data_collection/gazette/settings.py
@@ -7,8 +7,4 @@ ITEM_PIPELINES = {
     "scrapy.pipelines.files.FilesPipeline": 100,
     "gazette.pipelines.ExtractTextPipeline": 200,
 }
-SPIDER_MIDDLEWARES = {"scrapy_deltafetch.DeltaFetch": 100}
 FILES_STORE = "/mnt/data/"
-# skip already crawled item
-DELTAFETCH_ENABLED = True
-DELTAFETCH_DIR = "/mnt/data/deltafetch"
