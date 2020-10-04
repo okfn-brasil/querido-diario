f60e02fc43ba5d0801a14d4246374ab4d912e29a
Giulio Carvalho <gcc@cin.ufpe.br>
2020-10-04 15:50:24 -0300

    Update website URL

--- processing/data_collection/gazette/spiders/pb_joao_pessoa.py
+++ processing/data_collection/gazette/spiders/pb_joao_pessoa.py
@@ -15,13 +15,12 @@ class PbJoaoPessoaSpider(BaseGazetteSpider):
     DATE_CSS = "td:nth-last-child(2)::text"
     DATE_REGEX = r"[0-9]{2}/[0-9]{2}/[0-9]{4}"
 
-    allowed_domains = ["www.joaopessoa.pb.gov.br"]
     name = "pb_joao_pessoa"
-    start_urls = ["http://www.joaopessoa.pb.gov.br/semanariooficial/"]
+    start_urls = ["http://antigo.joaopessoa.pb.gov.br/semanariooficial/"]
 
     def parse(self, response):
         """
-        @url http://www.joaopessoa.pb.gov.br/semanariooficial/
+        @url http://antigo.joaopessoa.pb.gov.br/semanariooficial/
         @returns items 10
         @returns requests 1
         @scrapes date file_urls is_extra_edition territory_id power scraped_at
