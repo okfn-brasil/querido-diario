05bc328e7faecd6a1fd67cc16557b5d9952d6e01
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-10-08 09:24:01 -0300

    Refactor Petropolis spider

--- processing/data_collection/gazette/spiders/rj_petropolis.py
+++ processing/data_collection/gazette/spiders/rj_petropolis.py
@@ -19,43 +19,40 @@ class RjPetropolis(BaseGazetteSpider):
     start_urls = [
         "http://www.petropolis.rj.gov.br/pmp/index.php/servicos-na-web/informacoes/diario-oficial/viewcategory/3-diario-oficial.html"
     ]
-    set_to_discard = {
-        "/pmp/index.php/servicos-na-web/informacoes/diario-oficial.html",
-        None,
-    }
 
     def parse(self, response):
-        for entry in response.css("#col1 > div > table"):
-            entry_url = entry.css("a::attr(href)").get()
-            if entry_url not in self.set_to_discard:
-                url = urljoin(self.BASE_URL, entry_url)
-                if re.search(r"\d+-\d+", url):
-                    yield scrapy.Request(url=url, callback=self.parse)
-                else:
-                    yield scrapy.Request(url=url, callback=self.parse_items_page)
+        for entry in response.css("#col1 div table"):
+            entry_url = entry.css("b a::attr(href)").get()
+            url = urljoin(self.BASE_URL, entry_url)
+            yield scrapy.Request(url=url, callback=self.parse_month_page)
+
+    def parse_month_page(self, response):
+        for entry in response.css("#col1 div table"):
+            entry_url = entry.css("b a::attr(href)").get()
+            url = urljoin(self.BASE_URL, entry_url)
+            yield scrapy.Request(url=url, callback=self.parse_items_page)
 
     def parse_items_page(self, response):
-        for entry in response.css("#col1 > div > table"):
+        for entry in response.css(".jd_download_url"):
             entry_url = entry.css("a::attr(href)").get()
-            if entry_url not in self.set_to_discard:
-                url = urljoin(self.BASE_URL, entry_url)
-                file_url = url.replace(".html", ".pdf")
+            url = urljoin(self.BASE_URL, entry_url)
+            file_url = url.replace(".html", ".pdf")
 
-                date_match = re.search(r"(\d+-de-\w+-de-\d+)|(\d+-\d+-\d+)", url)
-                is_extra_edition = "suplemento" in url
-                edition_number = re.search(r"\d+-(\d+)", url).group(1)
+            title = entry.css("::text").get().strip()
+            date_match = re.search(
+                r"(\d+ de \w+ de \d+)|(\d+\/\d+\/\d+)", title, re.IGNORECASE
+            )
+            is_extra_edition = bool(re.search(r"Suplemento", title, re.IGNORECASE))
+            edition_number = re.search(r"^\d+", title).group(0)
 
-                if date_match is not None:
-                    date = dateparser.parse(
-                        date_match.group(0), languages=["pt"]
-                    ).date()
-
-                    yield Gazette(
-                        date=date,
-                        file_urls=[file_url],
-                        is_extra_edition=is_extra_edition,
-                        territory_id=self.TERRITORY_ID,
-                        power="executive_legislative",
-                        scraped_at=datetime.utcnow(),
-                        edition_number=edition_number,
-                    )
+            if date_match is not None:
+                date = dateparser.parse(date_match.group(0), languages=["pt"]).date()
+                yield Gazette(
+                    date=date,
+                    file_urls=[file_url],
+                    is_extra_edition=is_extra_edition,
+                    territory_id=self.TERRITORY_ID,
+                    power="executive_legislative",
+                    scraped_at=datetime.utcnow(),
+                    edition_number=edition_number,
+                )
