fc9e648dc2af1a95e0af0b2e929e4afddab421d1
Thiago Curvelo <tcurvelo@gmail.com>
2020-10-09 14:31:01 -0300

    [pb_campina_grande] Update spider after a long rebase

--- processing/data_collection/gazette/spiders/pb_campina_grande.py
+++ processing/data_collection/gazette/spiders/pb_campina_grande.py
@@ -1,98 +1,41 @@
-import dateparser
-from datetime import datetime
+from datetime import datetime as dt
 
-from scrapy import Request
-
+from dateutil.parser import parse
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class PbCampinaGrandeSpiderExecutive(BaseGazetteSpider):
-
     TERRITORY_ID = "2504009"
     allowed_domains = ["campinagrande.pb.gov.br"]
-    name = "pb_campina_grande_executive"
-    start_urls = ["http://campinagrande.pb.gov.br/semanario-oficial/"]
+    name = "pb_campina_grande"
+    start_urls = ["https://campinagrande.pb.gov.br/semanario-oficial/"]
 
     def parse(self, response):
-        """
-        @url http://campinagrande.pb.gov.br/semanario-oficial/
-        @scrapes date file_urls territory_id power scraped_at
-        """
-        for href in response.xpath("//div[@class='secretaria-text']/a/@href").extract():
-            yield Request(href, self.find_gazettes_by_month)
+        years_urls = response.css(".secretaria-text a::attr(href)").getall()
+        yield from response.follow_all(years_urls, self.parse_year)
 
-    def find_gazettes_by_month(self, response):
-        """
-            Get gazetes by month of the year
-        :param response: url to gazzetes of the year
-        """
-        for href in response.xpath("//div[@class='secretaria-text']/a/@href").extract():
-            yield Request(href, self.get_gazettes)
+    def parse_year(self, response):
+        monthes_urls = response.css(".secretaria-text a::attr(href)").getall()
+        yield from response.follow_all(monthes_urls, self.parse_month)
 
-    def get_gazettes(self, response):
-        """
-            List gazettes of month
-        :param response: url
-        """
-        editions = response.xpath(
-            "//div[@class='td_module_1 td_module_wrap td-animation-stack td_module_no_thumb']"
-        )
-        # If all gazettes were released in a single file.
+    def parse_month(self, response):
+        editions = response.css(".td_module_1")
         if not editions:
-            yield Request(
-                response.url, self.get_pdfurl, meta={"author": None, "posttime": None}
-            )
+            self.logger.warning(f"No editions found at {response.url}")
         else:
             for edition in editions:
-                pdfurl = response.xpath(
-                    "//h3[@class='entry-title td-module-title']/a/@href"
-                ).extract_first()
-                post_author = response.xpath(
-                    "//span[@class='td-post-author-name']/a/text()"
-                ).extract_first()
-                post_time = response.xpath(
-                    "//time[@class='entry-date updated td-module-date']/text()"
-                ).extract_first()
+                url = edition.css("a::attr(href)").get()
+                date = parse(edition.css("time::attr(datetime)").get())
+                yield response.follow(url, self.parse_issue, meta={"date": date})
 
-                yield Request(
-                    pdfurl,
-                    self.get_pdfurl,
-                    meta={"author": post_author, "posttime": post_time},
-                )
+    def parse_issue(self, response):
+        urls = response.css(".td-post-content a::attr(href)").getall()
 
-    def get_pdfurl(self, response):
-        """
-            Return the gazette with url,date (gazette and scraped) and file_urls
-        """
-        post_author = response.meta.get("author")
-        pt = response.meta.get("posttime")
-        if pt == None:
-            post_time = None
-        else:
-            post_time = dateparser.parse(pt).date()
-
-        from scrapy.selector import Selector
-
-        hxs = Selector(response)
-
-        file_url = hxs.xpath(
-            "//div[@class='td-post-content']/p/a/@href"
-        ).extract_first()
-
-        # For unknow resont the mayor office realese all gazetes in one edition (shouul be 4 editions by month)
-        if file_url == None:
-            file_url = hxs.xpath("//p[@class='embed_download']/a/@href").extract_first()
-
-        g = Gazette(
-            date=post_time,
-            file_urls=[file_url],
+        yield Gazette(
+            date=response.meta.get("date"),
+            file_urls=urls,
             territory_id=self.TERRITORY_ID,
             power="executive",
-            scraped_at=datetime.utcnow(),
+            scraped_at=dt.utcnow(),
         )
-
-        if file_url == None:
-            print(response)
-            print(g)
-        return g
