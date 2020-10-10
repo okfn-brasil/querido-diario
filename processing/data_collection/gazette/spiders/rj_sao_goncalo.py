1036b7e7f02edfc732db655322ca73d387c2cd98
Vitor Baptista <vitor@vitorbaptista.com>
2020-10-10 15:20:56 -0300

    Fix Sao Goncalo - RJ spider

--- processing/data_collection/gazette/spiders/rj_sao_goncalo.py
+++ processing/data_collection/gazette/spiders/rj_sao_goncalo.py
@@ -5,25 +5,31 @@ from gazette.items import Gazette
 
 
 class RjSaoGoncaloSpider(BaseGazetteSpider):
-    TERRITORY_ID = '3304904'
-    name = 'rj_sao_goncalo'
-    allowed_domains = ['www.pmsg.rj.gov.br']
-    download_gazette_url = 'http://www.pmsg.rj.gov.br/diario/{}.pdf'
-    start_urls = ['http://www.pmsg.rj.gov.br/diario_oficial.php']
+    TERRITORY_ID = "3304904"
+    DOWNLOAD_GAZETTE_URL = "https://servicos.pmsg.rj.gov.br/diario/{}.pdf"
+    start_date = datetime.date(1998, 2, 3)
+
+    name = "rj_sao_goncalo"
+    allowed_domains = ["servicos.pmsg.rj.gov.br"]
+    start_urls = ["https://servicos.pmsg.rj.gov.br/diario_oficial.php"]
 
     def parse(self, response):
         date = datetime.date.today()
-        end_date = datetime.date(1998, 2, 3)
-        while date >= end_date:
-            self.logger.info("got gazette for day %s", date)
-            url = self.download_gazette_url.format(date.strftime('%Y_%m_%d'))
-            is_extra_edition = False
+        start_date = self.start_date
+        while date >= start_date:
+            is_weekend = (date.weekday() >= 5)
+            if is_weekend:
+                date = date - datetime.timedelta(days=1)
+                continue
+
+            url = self.DOWNLOAD_GAZETTE_URL.format(date.strftime("%Y_%m_%d"))
+            # TODO: How to figure out is_extra_edition? Doesn't seem possible
+            # without extracting the text.
             yield Gazette(
                 date=date,
                 file_urls=[url],
-                is_extra_edition=is_extra_edition,
                 territory_id=self.TERRITORY_ID,
-                power='executive',
+                power="executive",
                 scraped_at=datetime.datetime.utcnow(),
             )
             date = date - datetime.timedelta(days=1)
