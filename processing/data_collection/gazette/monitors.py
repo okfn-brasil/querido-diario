33cc5ae34d1a5dd6f92d13ccb17f4fdfc4710928
Anderson Berg <andersonberg@gmail.com>
2020-10-12 13:22:05 -0300

    black reformat

--- processing/data_collection/gazette/monitors.py
+++ processing/data_collection/gazette/monitors.py
@@ -7,9 +7,11 @@ class RequestsItemsRatioMonitor(Monitor):
     def test_requests_items_ratio(self):
         n_scraped_items = self.data.stats.get("item_scraped_count", 0)
         n_requests_count = self.data.stats.get("downloader/request_count", 0)
-        max_ratio = self.data.crawler.settings.get("GAZETTE_MAX_REQUESTS_ITEMS_RATIO", 0)
+        max_ratio = self.data.crawler.settings.get(
+            "GAZETTE_MAX_REQUESTS_ITEMS_RATIO", 0
+        )
 
-        if n_scraped_items  > 0:
+        if n_scraped_items > 0:
             ratio = n_requests_count / n_scraped_items
             percent = round(ratio * 100, 2)
             allowed_percent = round(max_ratio * 100, 2)
@@ -18,7 +20,7 @@ class RequestsItemsRatioMonitor(Monitor):
                 max_ratio,
                 msg=f"""{percent}% is greater than the allowed {allowed_percent}%
                 ratio of requests over items scraped.
-                """
+                """,
             )
 
 
