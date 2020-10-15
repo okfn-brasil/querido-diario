f793bc8df4ca7bf24c3ef50f1a9d84283ccd6b0b
Renne Rocha <renne@rennerocha.com>
2020-10-14 23:45:51 -0300

    Remove text extraction from pipeline
    Processing of content of downloaded files will be done in a separated
    pipeline, so we don't need it in this part of the process.

--- processing/data_collection/gazette/pipelines.py
+++ processing/data_collection/gazette/pipelines.py
@@ -1,9 +1,6 @@
 import datetime as dt
-import os
-import subprocess
 from pathlib import Path
 
-import magic
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 from scrapy.http import Request
@@ -12,7 +9,6 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
 from gazette.database.models import initialize_database, Gazette
-from gazette.settings import FILES_STORE
 
 
 class GazetteDateFilteringPipeline:
@@ -36,103 +32,6 @@ class DefaultValuesPipeline:
             if field not in item:
                 item[field] = self.default_field_values.get(field)(spider)
         return item
-
-
-class ExtractTextPipeline:
-    """
-    Identify file format and call the right tool to extract the text from it
-    """
-
-    def process_item(self, item, spider):
-        extract_text_from_file = spider.settings.getbool(
-            "QUERIDODIARIO_EXTRACT_TEXT_FROM_FILE", True
-        )
-        if not extract_text_from_file:
-            return item
-
-        if self.is_doc(item["files"][0]["path"]):
-            item["source_text"] = self.doc_source_text(item)
-        elif self.is_pdf(item["files"][0]["path"]):
-            item["source_text"] = self.pdf_source_text(item)
-        elif self.is_txt(item["files"][0]["path"]):
-            item["source_text"] = self.txt_source_text(item)
-        else:
-            raise Exception(
-                "Unsupported file type: " + self.get_file_type(item["files"][0]["path"])
-            )
-
-        return item
-
-    def pdf_source_text(self, item):
-        """
-        Gets the text from pdf files
-        """
-        pdf_path = os.path.join(FILES_STORE, item["files"][0]["path"])
-        text_path = pdf_path + ".txt"
-        command = f"pdftotext -layout {pdf_path} {text_path}"
-        subprocess.run(command, shell=True, check=True)
-        with open(text_path) as file:
-            return file.read()
-
-    def doc_source_text(self, item):
-        """
-        Gets the text from docish files
-        """
-        doc_path = os.path.join(FILES_STORE, item["files"][0]["path"])
-        text_path = doc_path + ".txt"
-        command = f"java -jar /tika-app.jar --text {doc_path}"
-        with open(text_path, "w") as f:
-            subprocess.run(command, shell=True, check=True, stdout=f)
-        with open(text_path, "r") as f:
-            return f.read()
-
-    def txt_source_text(self, item):
-        """
-        Gets the text from txt files
-        """
-        with open(
-            os.path.join(FILES_STORE, item["files"][0]["path"]), encoding="ISO-8859-1"
-        ) as f:
-            return f.read()
-
-    def is_pdf(self, filepath):
-        """
-        If the file type is pdf returns True. Otherwise,
-        returns False
-        """
-        return self._is_file_type(filepath, file_types=["application/pdf"])
-
-    def is_doc(self, filepath):
-        """
-        If the file type is doc or similar returns True. Otherwise,
-        returns False
-        """
-        file_types = [
-            "application/msword",
-            "application/vnd.oasis.opendocument.text",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        ]
-        return self._is_file_type(filepath, file_types)
-
-    def is_txt(self, filepath):
-        """
-        If the file type is txt returns True. Otherwise,
-        returns False
-        """
-        return self._is_file_type(filepath, file_types=["text/plain"])
-
-    def get_file_type(self, filename):
-        """
-        Returns the file's type
-        """
-        file_path = os.path.join(FILES_STORE, filename)
-        return magic.from_file(file_path, mime=True)
-
-    def _is_file_type(self, filepath, file_types):
-        """
-        Generic method to check if a identified file type matches a given list of types
-        """
-        return self.get_file_type(filepath) in file_types
 
 
 class SQLDatabasePipeline:
