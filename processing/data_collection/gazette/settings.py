6a5bd93c1d3ceca4db0386e3924a0ab9967b9575
Renne Rocha <renne@rennerocha.com>
2020-10-16 14:43:01 -0300

    Remove text extraction from pipeline (#310)
    Processing of content of downloaded files will be done in a separated
    pipeline, so we don't need it in this part of the process.

--- processing/data_collection/gazette/settings.py
+++ processing/data_collection/gazette/settings.py
@@ -6,10 +6,8 @@ ITEM_PIPELINES = {
     "gazette.pipelines.GazetteDateFilteringPipeline": 50,
     "gazette.pipelines.DefaultValuesPipeline": 60,
     "scrapy.pipelines.files.FilesPipeline": 100,
-    "gazette.pipelines.ExtractTextPipeline": 200,
     "gazette.pipelines.SQLDatabasePipeline": 300,
 }
 FILES_STORE = "/mnt/data/"
 
-QUERIDODIARIO_EXTRACT_TEXT_FROM_FILE = True
 QUERIDODIARIO_DATABASE_URL = "sqlite:///querido-diario.db"
