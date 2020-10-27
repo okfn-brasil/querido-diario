c6992cb8e94cd2750f8408ee73de8a24e12e4dd4
Andre Assumpcao <andre.assumpcao@gmail.com>
2020-10-27 10:22:42 -0400

    up

--- processing/data_collection/gazette/spiders/sp_vinhedo.py
+++ processing/data_collection/gazette/spiders/sp_vinhedo.py
@@ -9,48 +9,31 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class SpVinhedoSpider(BaseGazetteSpider):
-
-    # define class variables
-    allowed_domains = ['vinhedo.sp.gov.br']
-    name = 'sp_vinhedo'
-    start_urls = [
-        'http://vinhedo.sp.gov.br/arquivos/?wpdmc=boletim-municipal'
-    ]
+    allowed_domains = ["vinhedo.sp.gov.br"]
+    start_urls = ["http://vinhedo.sp.gov.br/arquivos/?wpdmc=boletim-municipal"]
+    name = "sp_vinhedo"
     start_date = date(2010, 12, 9)
-    TERRITORY_ID = '3556701'
+    TERRITORY_ID = "3556701"
 
-    # parse gazette pages dynamically
     def parse(self, response):
-        pages = response.xpath('//*[@class="page-numbers"]//@href').getall()
-        last_page = re.search(r'(paged=)(\d{,2})', pages[-1]).group(2)
-        for i in range(1, int(last_page)+1):
-             url = f'{self.start_urls[0]}&paged={i}'
-             yield scrapy.Request(url, self.parse_page)
-
-    # parse gazette links per gazette page
-    def parse_page(self, response):
-        dates = response.xpath('//td[@class="hidden-xs"]/text()').getall()
-        dates = [parse(date, languages=['pt']).date() for date in dates]
-        editions = response.xpath('//a[@class="package-title"]/text()')
-        editionnum = [
-            re.search(r'(ção|xtra|úmero)(\D*)(\d{1,4})', edition).group(3)
-            for edition in editions.getall()
-        ]
-        extra = [
-            True if re.search(r'[Ee]xtra', edition) else False
-            for edition in editions.getall()
-        ]
-        links = response.xpath('//a[@rel="nofollow"]/@onclick').getall()
-        links = [re.search(r'(htt(.)+)(\';)', link).group(1) for link in links]
-
-        for date, edition, extra, link in zip(dates, editionnum, extra, links):
-            if date >= self.start_date:
+        page = response.xpath('//*[@class="next page-numbers"]//@href')
+        last_page = re.search(r"(paged=)(\d{,2})", page.get()).group(2)
+        for i in range(1, int(last_page)):
+            response.urljoin(page.get())
+            gazettes = response.css('table tbody tr')
+            for gazette in gazettes:
+                data = gazette.css('td')
+                edition = data.css('.package-title::text').get()
+                date = data.css('.hidden-sm::text').get()
+                link = data.css('[onclick]').get()
                 yield Gazette(
-                    date=date,
-                    edition_number=edition,
-                    file_urls=[link],
-                    is_extra_edition=extra,
-                    power='executive_legislative',
+                    date=parse(date, languages=["pt"]).date(),
+                    edition_number=re.search(
+                        r"(ção|xtra|úmero)(\D*)(\d{1,4})", edition
+                    ).group(3),
+                    file_urls=[re.search(r"(htt(.)+)(\';)", link).group(1)],
+                    is_extra_edition=bool(re.search(r"[Ee]xtra", edition)),
+                    power="executive_legislative",
                     scraped_at=datetime.utcnow(),
                     territory_id=self.TERRITORY_ID,
                 )
