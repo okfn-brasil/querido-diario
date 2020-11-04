d7f20cb1689efddeb382069cfdf93995e0e91251
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-04 12:13:24 +0100

    Atualiza localização dos spiders

--- data_collection/gazette/spiders/ba_amelia_rodrigues.py
+++ data_collection/gazette/spiders/ba_amelia_rodrigues.py
@@ -6,6 +6,6 @@ class BaAmeliaRodriguesSpider(ImprensaOficialSpider):
 
     name = "ba_amelia_rodrigues"
     allowed_domains = ["pmameliarodriguesba.imprensaoficial.org"]
-    start_date = date(2020, 9, 1)  # FIXME
+    start_date = date(2020, 1, 1)
     url_base = "http://pmameliarodriguesba.imprensaoficial.org/{}/"
-    TERRITORY_ID = "2901106"
+    TERRITORY_ID = "2930501"
