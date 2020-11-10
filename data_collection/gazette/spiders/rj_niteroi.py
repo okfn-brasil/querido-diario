f35f94f95d0eaece81b3e99d738a36a0e8e6c472
Renne Rocha <renne@rennerocha.com>
2020-11-10 15:50:42 -0300

    Finish spider and refactor it with new project structure.

--- data_collection/gazette/spiders/rj_niteroi.py
+++ data_collection/gazette/spiders/rj_niteroi.py
@@ -1,15 +1,19 @@
 import datetime as dt
 
+import scrapy
+
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class RjNiteroiSpider(BaseGazetteSpider):
-    MUNICIPALITY_ID = "3303302"
+    TERRITORY_ID = "3303302"
     name = "rj_niteroi"
     allowed_domains = ["niteroi.rj.gov.br"]
     start_urls = ["http://www.niteroi.rj.gov.br"]
-    download_url = "http://www.niteroi.rj.gov.br/downloads/do/{}/{}/{:02d}"
+    download_url = "http://pgm.niteroi.rj.gov.br/downloads/do/{}/{}/{:02d}.pdf"
+    start_date = dt.date(2003, 7, 1)
+
     month_names = [
         "01_Jan",
         "02_Fev",
@@ -26,21 +30,22 @@ class RjNiteroiSpider(BaseGazetteSpider):
     ]
 
     def parse(self, response):
-        """
-        @url http://www.niteroi.rj.gov.br
-        @returns items 1
-        @scrapes date file_urls municipality_id power scraped_at
-        """
-        start_date = dt.date(2003, 7, 1)
         parsing_date = dt.date.today()
-        while parsing_date >= start_date:
+        while parsing_date >= self.start_date:
             month = self.month_names[parsing_date.month - 1]
             url = self.download_url.format(parsing_date.year, month, parsing_date.day)
-            yield Gazette(
-                date=parsing_date,
-                file_urls=[url],
-                municipality_id=self.MUNICIPALITY_ID,
-                power="executive_legislature",
-                scraped_at=dt.datetime.utcnow(),
+            yield scrapy.Request(
+                url,
+                method="HEAD",
+                callback=self.parse_valid_gazette_file,
+                cb_kwargs={"gazette_date": parsing_date},
             )
             parsing_date = parsing_date - dt.timedelta(days=1)
+
+    def parse_valid_gazette_file(self, response, gazette_date):
+        yield Gazette(
+            date=gazette_date,
+            file_urls=[response.url],
+            is_extra_edition=False,
+            power="executive",
+        )
