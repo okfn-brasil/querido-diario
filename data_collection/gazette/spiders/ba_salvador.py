4f1535c9f27c507a765d21c0228e7c191602e4a8
Renne Rocha <renne@rennerocha.com>
2020-11-11 12:11:48 -0300

    Refactor spider code
    - Replaced `extract_first` with `get` calls
    - Removed need of dateparser lib. Using built-in datetime to parse dates
    - Removed unnecessary variables
    - Fixed imports

--- data_collection/gazette/spiders/ba_salvador.py
+++ data_collection/gazette/spiders/ba_salvador.py
@@ -1,12 +1,9 @@
 import datetime
-import re
 import urllib.parse
 
-import dateparser
 import scrapy
 
 from gazette.items import Gazette
-from gazette.settings import ITEM_PIPELINES
 from gazette.spiders.base import BaseGazetteSpider
 
 
@@ -14,7 +11,6 @@ class BaSalvadorSpider(BaseGazetteSpider):
     TERRITORY_ID = "2927408"
     name = "ba_salvador"
     allowed_domains = ["salvador.ba.gov.br"]
-    power = "executive"
     start_date = datetime.date(2001, 1, 1)
 
     def start_requests(self):
@@ -37,29 +33,25 @@ class BaSalvadorSpider(BaseGazetteSpider):
 
     def parse(self, response):
         for gazette in response.css(".dmarticlesfilter_results_title"):
-            gazette_date = gazette.css(
-                "#dmarticlesfilter_results_date::text"
-            ).extract_first("")
-            gazette_url = gazette.css("a::attr(href)").extract_first()
+            gazette_date = gazette.css("#dmarticlesfilter_results_date::text").get()
+            gazette_url = gazette.css("a::attr(href)").get()
 
             yield scrapy.Request(
                 response.urljoin(gazette_url),
-                meta={"gazette_date": gazette_date},
+                cb_kwargs={"gazette_date": gazette_date},
                 callback=self.parse_gazette,
             )
 
         for next_page_url in response.css(".paginacao a::attr(href)"):
             yield response.follow(next_page_url)
 
-    def parse_gazette(self, response):
-        parsed_date = dateparser.parse(
-            response.meta.get("gazette_date"), settings={"DATE_ORDER": "YMD"}
-        )
-        pdf_url = response.css("#PDFId embed::attr(src)").extract_first()
+    def parse_gazette(self, response, gazette_date):
+        gazette_date = datetime.datetime.strptime(gazette_date, "%Y-%m-%d").date()
+        pdf_url = response.css("#PDFId embed::attr(src)").get()
 
         yield Gazette(
-            date=parsed_date.date(),
+            date=gazette_date,
             file_urls=[pdf_url],
-            power=self.power,
+            power="executive",
             is_extra_edition=False,
         )
