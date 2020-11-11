2c41bfc2fe618a0108977c5d7abaa324ac934c9e
Renne Rocha <renne@rennerocha.com>
2020-11-11 11:54:17 -0300

    Remove custom pipeline that looks inside PDF content
    As we removed content extraction from the spiders, we are
    not able to identify if a gazette is or isn't extra without
    accessing its content. This procedure needs to be placed in the
    data-processing project.

--- data_collection/gazette/spiders/ba_salvador.py
+++ data_collection/gazette/spiders/ba_salvador.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import datetime
 import re
 import urllib.parse
@@ -16,14 +15,6 @@ class BaSalvadorSpider(BaseGazetteSpider):
     name = "ba_salvador"
     allowed_domains = ["salvador.ba.gov.br"]
     power = "executive"
-
-    @classmethod
-    def update_settings(cls, settings):
-        pipelines = settings["ITEM_PIPELINES"]
-        pipelines[
-            "gazette.spiders.ba_salvador.BaSalvadorExtraEditionItemPipeline"
-        ] = 1000
-        super().update_settings(settings)
 
     def start_requests(self):
         # According to their website, they have gazettes available from 2001-01-01
@@ -68,20 +59,5 @@ class BaSalvadorSpider(BaseGazetteSpider):
             date=parsed_date.date(),
             file_urls=[pdf_url],
             power=self.power,
+            is_extra_edition=False,
         )
-
-
-class BaSalvadorExtraEditionItemPipeline:
-    def process_item(self, item, spider):
-        item["is_extra_edition"] = False
-
-        # The only indication if the gazette is an extra edition is a
-        # red strip over the first page of the PDF with the text 'EDIÇÃO EXTRA'
-        # Unfortunately pdftotext is unable to get the whole word and after some
-        # tests comparing some gazettes, this regex seems to be enough to identify an
-        # extra edition. But I can't guarantee this is 100% accurate. Maybe older
-        # editions has a different pattern.
-        if re.findall("T[\s]+RA", item.get("source_text", "")):
-            item["is_extra_edition"] = True
-
-        return item
