6afa760aa36c8a50571dfb0ff020644cb2c18931
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 10:39:09 +0100

    Add Gentio do Ouro spider

---
+++ data_collection/gazette/spiders/ba_gentio_do_ouro.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaGentioDoOuroSpider(ImprensaOficialSpider):
+
+    name = "ba_gentio_do_ouro"
+    allowed_domains = ["pmGENTIODOOUROBA.imprensaoficial.org"]
+    start_date = date(2017, 2, 1)
+    url_base = "http://pmGENTIODOOUROBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2911303"
