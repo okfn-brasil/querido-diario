1f87c0935b181ce0434350d8d431859d3721974c
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 12:00:00 +0100

    Add São Francisco do Conde spider

---
+++ data_collection/gazette/spiders/ba_sao_francisco_do_conde.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaSaoFranciscoDoCondeSpider(ImprensaOficialSpider):
+
+    name = "ba_sao_francisco_do_conde"
+    allowed_domains = ["pmSAOFRANCISCODOCONDEBA.imprensaoficial.org"]
+    start_date = date(2019, 3, 1)
+    url_base = "http://pmSAOFRANCISCODOCONDEBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2929206"
