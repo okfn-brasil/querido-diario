660d0c73d00f13bba3543d914d42045a8e02fd43
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:03:35 +0100

    Add Itaquara spider

---
+++ data_collection/gazette/spiders/ba_itaquara.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaItaquaraSpider(ImprensaOficialSpider):
+
+    name = "ba_itaquara"
+    allowed_domains = ["pmITAQUARABA.imprensaoficial.org", "itaquara.ba.gov.br"]
+    start_date = date(2019, 1, 1)
+    url_base = "http://itaquara.ba.gov.br/{}/"
+    TERRITORY_ID = "2916708"
