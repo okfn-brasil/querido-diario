090dc5ff2fb8ff0d23c49e215e94059a6ec21f7d
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:17:57 +0100

    Add Muniz Ferreira sider

---
+++ data_collection/gazette/spiders/ba_muniz_ferreira.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaMunizFerreiraSpider(ImprensaOficialSpider):
+
+    name = "ba_muniz_ferreira"
+    allowed_domains = ["pmMUNIZFERREIRABA.imprensaoficial.org", "munizferreira.ba.gov.br"]
+    start_date = date(2014, 12, 1)
+    url_base = "http://munizferreira.ba.gov.br/{}/"
+    TERRITORY_ID = "2922201"
