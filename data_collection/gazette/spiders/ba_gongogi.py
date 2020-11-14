e48d1ec9432b561d51c726b9c0b54edd69a51c63
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 10:45:39 +0100

    Add Gongogi spider

---
+++ data_collection/gazette/spiders/ba_gongogi.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaGongogiSpider(ImprensaOficialSpider):
+
+    name = "ba_gongogi"
+    allowed_domains = ["pmGONGOGIBA.imprensaoficial.org", "gongogi.ba.gov.br"]
+    start_date = date(2020, 2, 1)
+    url_base = "http://gongogi.ba.gov.br/{}/"
+    TERRITORY_ID = "2911501"
