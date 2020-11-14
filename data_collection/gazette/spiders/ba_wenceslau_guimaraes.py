92a876efc0d2bbde4a2709e313f2c9e81a005534
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 12:23:49 +0100

    Add Wenceslau Guimarães spider

---
+++ data_collection/gazette/spiders/ba_wenceslau_guimaraes.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaWenceslauGuimaraesSpider(ImprensaOficialSpider):
+
+    name = "ba_wenceslau_guimaraes"
+    allowed_domains = ["pmWENCESLAUGUIMARAESBA.imprensaoficial.org"]
+    start_date = date(2017, 1, 1)
+    url_base = "http://pmWENCESLAUGUIMARAESBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2933505"
