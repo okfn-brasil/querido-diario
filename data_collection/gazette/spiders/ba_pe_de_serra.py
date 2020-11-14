8f80cb28f8bf02d020a6d4b576657f797ec89843
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:24:51 +0100

    Add Pé de Serra spider

---
+++ data_collection/gazette/spiders/ba_pe_de_serra.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaPeDeSerraSpider(ImprensaOficialSpider):
+
+    name = "ba_pe_de_serra"
+    allowed_domains = ["pmPEDESERRABA.imprensaoficial.org"]
+    start_date = date(2017, 1, 1)
+    url_base = "http://pmPEDESERRABA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2924058"
