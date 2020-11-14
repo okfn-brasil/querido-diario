ae303c8488289e5faaa8b6169cd88901adba179a
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:07:15 +0100

    Add Jaguarari spider

---
+++ data_collection/gazette/spiders/ba_jaguarari.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaJaguarariSpider(ImprensaOficialSpider):
+
+    name = "ba_jaguarari"
+    allowed_domains = ["pmJAGUARARIBA.imprensaoficial.org"]
+    start_date = date(2019, 10, 1)
+    url_base = "http://pmJAGUARARIBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2917706"
