8c756a6b0e7f9e983de64cbe16c293619f44ace7
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:21:03 +0100

    Add Paratinga spider

---
+++ data_collection/gazette/spiders/ba_paratinga.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaParatingaSpider(ImprensaOficialSpider):
+
+    name = "ba_paratinga"
+    allowed_domains = ["pmPARATINGABA.imprensaoficial.org"]
+    start_date = date(2018, 4, 1)
+    url_base = "http://pmPARATINGABA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2923704"
