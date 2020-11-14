e842a702d13c9ea3a8bf2b2c57e409e78735e340
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 12:18:27 +0100

    Add Vera Cruz spider

---
+++ data_collection/gazette/spiders/ba_vera_cruz.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaVeraCruzSpider(ImprensaOficialSpider):
+
+    name = "ba_vera_cruz"
+    allowed_domains = ["pmVERACRUZBA.imprensaoficial.org"]
+    start_date = date(2017, 4, 1)
+    url_base = "http://pmVERACRUZBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2933208"
