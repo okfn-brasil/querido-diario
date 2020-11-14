8af97e8a016987ae26c3af413299024ab8ed8a7f
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 12:15:05 +0100

    Add Saude spider

---
+++ data_collection/gazette/spiders/ba_saude.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaSaudeSpider(ImprensaOficialSpider):
+
+    name = "ba_saude"
+    allowed_domains = ["pmSAUDEBA.imprensaoficial.org"]
+    start_date = date(2018, 2, 1)
+    url_base = "http://pmSAUDEBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2929800"
