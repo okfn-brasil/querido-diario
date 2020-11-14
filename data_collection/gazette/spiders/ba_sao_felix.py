ca91014643164ec113aa49a29d739cc471a964c8
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-14 11:52:01 +0100

    Add São Felix spider

---
+++ data_collection/gazette/spiders/ba_sao_felix.py
@@ -0,0 +1,11 @@
+from datetime import date
+from gazette.spiders.base import ImprensaOficialSpider
+
+
+class BaSaoFelixSpider(ImprensaOficialSpider):
+
+    name = "ba_sao_felix"
+    allowed_domains = ["pmSAOFELIXBA.imprensaoficial.org"]
+    start_date = date(2017, 1, 1)
+    url_base = "http://pmSAOFELIXBA.imprensaoficial.org/{}/"
+    TERRITORY_ID = "2929008"
