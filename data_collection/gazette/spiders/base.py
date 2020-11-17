a2b2acf5e01651b0c2ed8be092f3d202bf8114e8
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-11-17 01:24:38 -0300

    Add base spider for doem.org.br and spider for Acajutiba/BA (#347)

--- data_collection/gazette/spiders/base.py
+++ data_collection/gazette/spiders/base.py
@@ -188,3 +188,84 @@ class FecamGazetteSpider(BaseGazetteSpider):
             file_urls=(document[0],),
             power="executive",
         )
+
+
+class DoemGazetteSpider(BaseGazetteSpider):
+    """
+    Base spider for all cities listed on https://doem.org.br
+    """
+
+    allowed_domains = ["doem.org.br"]
+    start_date = date(2009, 1, 1)
+
+    def start_requests(self):
+        yield scrapy.Request(self.get_url(), callback=self.parse_pagination)
+
+    def parse_pagination(self, response):
+        """
+        This parse function is used to get all the pages available and
+        return request object for each one
+        """
+        return [
+            scrapy.Request(self.get_url(page), callback=self.parse)
+            for page in range(1, 1 + self.get_last_page(response))
+        ]
+
+    def parse(self, response):
+        """
+        Parse each page from the results page and yield the gazette issues available.
+        """
+        GAZETTE_CONTAINER_CSS_QUERY = "div.box-diario"
+        gazette_boxes = response.css(GAZETTE_CONTAINER_CSS_QUERY)
+
+        for gazette_box in gazette_boxes:
+            file_url = self.get_pdf_url(gazette_box)
+            date = self.get_gazette_date(gazette_box)
+            edition_number = self.get_edition_number(gazette_box)
+            yield Gazette(
+                date=date,
+                file_urls=[file_url],
+                edition_number=edition_number,
+                power="executive_legislative",
+            )
+
+    def get_url(self, page=None):
+        url = f"https://doem.org.br/{self.state_city_url_part}"
+        start_date = self.start_date.strftime("%Y-%m-%d")
+        page_param = f"&page={page}" if page is not None else ""
+        return f"{url}/pesquisar?data_inicial={start_date}{page_param}"
+
+    def get_last_page(self, response):
+        """
+        Gets the last page number available in the pages navigation menu
+        """
+        PAGINATION_CSS_QUERY = "ul.pagination li a::text"
+        pages = response.css(PAGINATION_CSS_QUERY).getall()
+        if len(pages) == 0:
+            return 1
+        return max([int(page) for page in pages if page.isnumeric()])
+
+    def get_pdf_url(self, response_item):
+        """
+        Gets the url for the gazette inside one of the 'box-diario' divs
+        """
+        LINK_CSS_QUERY = "a[title='Baixar Publicação']::attr(href)"
+        preview_link = response_item.css(LINK_CSS_QUERY).get()
+        return preview_link.replace("previsualizar", "baixar")
+
+    def get_gazette_date(self, response_item):
+        """
+        Get the date for the gazette inside one of the 'box-diario' divs
+        """
+        DATE_CSS_QUERY = "span.data-diario::text"
+        date = response_item.css(DATE_CSS_QUERY).get().strip()
+        return dateparser.parse(date, languages=["pt"]).date()
+
+    def get_edition_number(self, response_item):
+        """
+        Get the edition number inside one of the 'box-diario' divs
+        """
+        DIV_HEADER_CSS_QUERY = "h2::text"
+        edition_text = response_item.css(DIV_HEADER_CSS_QUERY).get()
+        edition_number = edition_text.strip().split(" ")[1]
+        return int(edition_number) if edition_number.isnumeric() else None
