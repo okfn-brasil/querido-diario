65ff166eb1c49bedd05a49338cf4b94d719eee5d
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-23 19:51:36 +0100

    Remove unnecessary comment

--- data_collection/gazette/spiders/ba_amelia_rodrigues.py
+++ data_collection/gazette/spiders/ba_amelia_rodrigues.py
@@ -8,5 +8,5 @@ class BaAmeliaRodriguesSpider(ImprensaOficialSpider):
     name = "ba_amelia_rodrigues"
     allowed_domains = ["pmameliarodriguesba.imprensaoficial.org"]
     start_date = date(2015, 1, 1)
-    url_base = "http://pmameliarodriguesba.imprensaoficial.org/{}"  # 2020/07
+    url_base = "http://pmameliarodriguesba.imprensaoficial.org/{}"
     TERRITORY_ID = "2930501"
