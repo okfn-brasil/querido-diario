9f9a244cf95a81a54fc94f719301b658e7f05bb7
Joao Meyer <1994meyer@gmail.com>
2020-12-13 17:02:52 -0300

    Applied requested fixes and suggestions

--- data_collection/gazette/spiders/rs_lajeado.py
+++ data_collection/gazette/spiders/rs_lajeado.py
@@ -1,6 +1,7 @@
 from datetime import date
 from urllib.parse import urlencode
 
+import dateparser
 from scrapy import Request
 
 from gazette.items import Gazette
@@ -9,7 +10,7 @@ from gazette.spiders.base import BaseGazetteSpider
 
 class RsLajeadoSpider(BaseGazetteSpider):
     TERRITORY_ID = "4311403"
-    BASE_URL = "https://lajeado.rs.gov.br"
+    BASE_URL = "https://lajeado.rs.gov.br/"
 
     name = "rs_lajeado"
     start_date = date(2016, 4, 5)
@@ -21,43 +22,39 @@ class RsLajeadoSpider(BaseGazetteSpider):
 
     def start_requests(self):
         params = urlencode(self.params)
-        yield Request(self.BASE_URL + "/?" + params)
+        yield Request(f"{self.BASE_URL}?{params}")
 
     def parse(self, response):
         pdfs = response.css("ul.anexos li a")
         for item in pdfs:
             filename = item.css("::text").get()
 
-            kwargs = {"filedate": self.extract_date(filename)}
+            kwargs = {
+                "gazette_date": self.extract_date(filename),
+                "edition_number": self.extract_edition(filename),
+            }
 
-            href = item.attrib["href"]
-            href = response.urljoin(href)
+            href = response.urljoin(item.attrib["href"])
+            yield Request(href, callback=self.parse_url, cb_kwargs=kwargs)
 
-            yield Request(href, method="GET", callback=self.parse_url, cb_kwargs=kwargs)
-
-    def parse_url(self, response, **kwargs: dict):
+    def parse_url(self, response, gazette_date=None, edition_number=None):
         url = response.css("div.topoAbreAnexoDownload a::attr(href)").get()
         yield Gazette(
-            date=kwargs["filedate"],
+            date=gazette_date,
+            edition_number=edition_number,
             file_urls=[url],
-            power="legislative",
+            power="executive",
             territory_id=self.TERRITORY_ID,
         )
 
-    @staticmethod
-    def extract_date(filename: str) -> date:
+    def extract_date(self, filename):
         filedate, *_ = filename.split("_")
 
         # There is a single case where the first day
         # of the month is represented like '1º'
         filedate = filedate.replace("º", "")
+        filedate = dateparser.parse(filedate, languages=["pt"])
+        return filedate.date()
 
-        filedate = filedate.split("/")
-
-        # There is a single case where the date is formatted
-        # as 'dd mm yy' instead of 'dd/mm/yy'
-        if len(filedate) == 1:
-            filedate = filedate[0].split()
-
-        day, month, year = (int(f) for f in filedate)
-        return date(year + 2000, month, day)
+    def extract_edition(self, filename):
+        return filename.lower().split("_edição_")[1].split()[0]
