18767f2284a442ad581a9df771ef0ed238df81c0
Renne Rocha <renne@rennerocha.com>
2021-02-26 17:23:09 -0300

    Refactor of Manaus-AM spider

--- data_collection/gazette/spiders/am_manaus.py
+++ data_collection/gazette/spiders/am_manaus.py
@@ -1,72 +1,64 @@
-import dateparser
-from scrapy import Request
+import datetime
+import re
+
+import scrapy
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class AmManausSpider(BaseGazetteSpider):
+
+    name = "am_manaus"
+    allowed_domains = ["dom.manaus.am.gov.br"]
+    start_date = datetime.date(2000, 4, 3)
+    start_urls = ["http://dom.manaus.am.gov.br/diario-oficial-de-manaus"]
+
     TERRITORY_ID = "1302603"
 
-    EXECUTIVE_URL = "http://dom.manaus.am.gov.br/diario-oficial-de-manaus"
-    EXECUTIVE_NEXT_PAGE_URL = EXECUTIVE_URL + "/atct_topic_view?b_start:int={}&-C="
-    LEGISLATIVE_URL = "http://www.cmm.am.gov.br/diario-oficial"
+    def parse(self, response):
+        follow_next_page = True
+        gazettes = response.css(".listing tbody tr")
+        for gazette in gazettes:
+            gazette_date_raw = gazette.xpath("./td[1]//text()").re_first(
+                r"\d{2}\/\d{2}\/\d{4}"
+            )
+            gazette_date = datetime.datetime.strptime(
+                gazette_date_raw, "%d/%m/%Y"
+            ).date()
 
-    EXECUTIVE_DATE_CSS = "td:first-child span::text"
-    EXECUTIVE_GAZETTE_ROW_CSS = "table.listing tbody tr"
-    EXECUTIVE_PDF_HREF_CSS = "td:nth-child(2) a::attr(href)"
-    EXECUTIVE_PDF_TEXT_CSS = "td:nth-child(2) a:last-child::text"
+            if gazette_date < self.start_date:
+                follow_next_page = False
+                break
 
-    LEGISLATIVE_DATE_CSS = "td:first-child"
-    LEGISLATIVE_GAZETTE_ROW_CSS = ".table-cmm tr"
-    LEGISLATIVE_NEXT_PAGE_CSS = ".paging a.next::attr(href)"
-    LEGISLATIVE_PDF_HREF_CSS = "td:last-child a::attr(href)"
-    LEGISLATIVE_DATE_REGEX = r"[0-9]{2}/[0-9]{2}/[0-9]{4}"
+            title = "".join(gazette.xpath("./td[2]//text()").getall()).strip()
+            edition_number = self._extract_edition_number(title, gazette_date)
+            is_extra_edition = re.search(r"eex|ext", title.lower()) is not None
+            gazette_url = gazette.css("a::attr(href)").get()
 
-    EXECUTIVE_LAST_PAGE = 1000
-    EXECUTIVE_STEP = 20
+            yield Gazette(
+                date=gazette_date,
+                edition_number=edition_number,
+                is_extra_edition=is_extra_edition,
+                file_urls=[gazette_url],
+                power="executive",
+            )
 
-    allowed_domains = ["manaus.am.gov.br", "cmm.am.gov.br"]
-    name = "am_manaus"
+        if follow_next_page:
+            next_page_url = response.css(".next a::attr(href)").get()
+            yield scrapy.Request(next_page_url)
 
-    def start_requests(self):
-        yield Request(self.EXECUTIVE_URL, self.parse_executive)
-        yield Request(self.LEGISLATIVE_URL, self.parse_legislative)
-
-    def parse_executive(self, response):
-        for element in response.css(self.EXECUTIVE_GAZETTE_ROW_CSS):
-            url = element.css(self.EXECUTIVE_PDF_HREF_CSS).extract_first()
-            date = element.css(self.EXECUTIVE_DATE_CSS).extract_first()
-            date = dateparser.parse(date, languages=["pt"]).date()
-            pdf_title = element.css(self.EXECUTIVE_PDF_TEXT_CSS).extract_first()
-            is_extra_edition = "Edição Extra" in pdf_title
-
-            yield self.build_gazzete(date, url, "executive", is_extra_edition)
-
-        steps = range(20, self.EXECUTIVE_LAST_PAGE, self.EXECUTIVE_STEP)
-        for step in steps:
-            url = self.EXECUTIVE_NEXT_PAGE_URL.format(step)
-            yield Request(url, self.parse_executive)
-
-    def parse_legislative(self, response):
-        for element in response.css(self.LEGISLATIVE_GAZETTE_ROW_CSS):
-            if not element.css("td"):
-                continue
-
-            url = element.css(self.LEGISLATIVE_PDF_HREF_CSS).extract_first()
-            date = element.css(self.LEGISLATIVE_DATE_CSS)
-            date = date.re(self.LEGISLATIVE_DATE_REGEX).pop()
-            date = dateparser.parse(date, languages=["pt"]).date()
-
-            yield self.build_gazzete(date, url, "legislative")
-
-        for url in response.css(self.LEGISLATIVE_NEXT_PAGE_CSS).extract():
-            yield Request(url, self.parse_legislative)
-
-    def build_gazzete(self, date, url, power, is_extra_edition=False):
-        return Gazette(
-            date=date,
-            file_urls=[url],
-            is_extra_edition=is_extra_edition,
-            power=power,
+    def _extract_edition_number(self, text, gazette_date):
+        year = gazette_date.year
+        pattern = r"|".join(
+            [
+                r"Edição (\d+)\s",
+                rf"dom{year}(\d{{4}})[A-Za-z.]",
+                r"dom(\d{4})[A-Za-z.]",
+                r"(\d+)\s",
+            ]
         )
+        matches = re.search(pattern, text)
+        if matches:
+            return matches.group(matches.lastindex)
+        return ""
