a08f65e5d4fcb6cb72261bf3151bc1867b6d43c4
Alexandre Kajihara <alexandre.ykz@gmail.com>
2021-04-22 00:28:16 -0300

    add parser

--- data_collection/gazette/spiders/rj_duque_de_caxias.py
+++ data_collection/gazette/spiders/rj_duque_de_caxias.py
@@ -1,19 +1,19 @@
-import scrapy
-
 from datetime import date
+
+import scrapy
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class RjDuqueDeCaxiasSpider(BaseGazetteSpider):
-    
     TERRITORY_ID = "3301702"
     name = "rj_duque_de_caxias"
     allowed_domains = ["duquedecaxias.rj.gov.br"]
     start_urls = ["https://duquedecaxias.rj.gov.br"]
-    base_url_before_2017 = "https://duquedecaxias.rj.gov.br/portal/arquivos/{}/{}/"
-    base_url_after_2017 = "https://duquedecaxias.rj.gov.br/portal/{}.html"
+    base_url = "https://duquedecaxias.rj.gov.br/portal"
+    base_url_before_2017 = base_url + "/boletim-oficial/{}/{}"
+    base_url_after_2017 = base_url + "/{}.html"
     start_date = date(2013, 1, 1)
 
     month_names = [
@@ -31,16 +31,16 @@ class RjDuqueDeCaxiasSpider(BaseGazetteSpider):
         "Dezembro",
     ]
 
-
     def start_requests(self):
         end_date = date.today()
         year = self.start_date.year
         while year <= end_date.year:
             month = self.start_date.month
-            while month <= len(self.month_names):
+            while month < len(self.month_names):
                 if year < 2017:
-                    folder_month_name = "{:02d}".format(month + 1) + "-" + self.month_names[month]
-                    url = self.base_url_before_2017.format(year, folder_month_name, " ")
+                    url = self.base_url_before_2017.format(
+                        year, "{:02d}".format(month + 1) + "-" + self.month_names[month]
+                    )
                 else:
                     url = self.base_url_after_2017.format(year)
                 yield scrapy.Request(url)
@@ -48,3 +48,18 @@ class RjDuqueDeCaxiasSpider(BaseGazetteSpider):
             year += 1
 
     def parse(self, response):
+        for element in response.xpath('//a[contains(@href,"pdf")]/@href'):
+            if ".html" in response.url:
+                url = self.base_url + element.get()
+            else:
+                url = response.url + element.get()
+            extra_edition = False
+            if element.get().lower() in "extraordionario":
+                extra_edition: True
+            gazette_date = date.today()
+            yield Gazette(
+                date=gazette_date,
+                file_urls=[url],
+                is_extra_edition=extra_edition,
+                power="executive_legislative",
+            )
