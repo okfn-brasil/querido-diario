91da94a654b311996a078d5fb8280bd8a745a98d
Daniel Farina <daniel.rodrigues.45@hotmail.com>
2021-08-13 10:52:35 -0300

    Remove allowed_domains typo

--- data_collection/gazette/spiders/sp_guaracai.py
+++ data_collection/gazette/spiders/sp_guaracai.py
@@ -6,7 +6,6 @@ from gazette.spiders.base.dosp import DospGazetteSpider
 class SpGuaracaiSpider(DospGazetteSpider):
     TERRITORY_ID = "3517802"
     name = "sp_guaracai"
-    allowed_domains = ["glicerio.sp.gov.br"]
 
     code = 4853
     start_date = datetime.date(2018, 9, 27)
