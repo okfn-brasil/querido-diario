a6e22c94d5e982306b462f30ebf01e62e9325efa
Renne Rocha <renne@rennerocha.com>
2021-08-28 09:17:35 -0300

    Test changin spiders directory

--- data_collection/gazette/spiders/al_associacao_municipios.py
+++ data_collection/gazette/spiders/al_associacao_municipios.py
@@ -3,5 +3,5 @@ from gazette.spiders.base.sigpub import SigpubGazetteSpider
 
 class AlAssociacaoMunicipiosSpider(SigpubGazetteSpider):
     name = "al_associacao_municipios"
-    TERRITORY_ID = "2700000"
+    TERRITORY_ID = "2700001"
     CALENDAR_URL = "http://www.diariomunicipal.com.br/ama"
