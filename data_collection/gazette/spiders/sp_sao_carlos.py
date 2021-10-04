7848b64238bb77bbda276bd5390237123d07a4d4
Tatiana <43054051+tacoelho@users.noreply.github.com>
2021-10-04 15:03:56 -0300

    Update sp_sao_carlos.py

--- data_collection/gazette/spiders/sp_sao_carlos.py
+++ data_collection/gazette/spiders/sp_sao_carlos.py
@@ -2,6 +2,6 @@ from gazette.spiders.base.doem import DoemGazetteSpider
 
 
 class SPSaoCarlosSpider(DoemGazetteSpider):
-    TERRITORY_ID = ""
+    TERRITORY_ID = "3548906"
     
     state_city_url_part = "sp/sao carlos"
