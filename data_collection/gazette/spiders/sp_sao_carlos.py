0fb53a4fe413b60c9f86956c12567fbc41fa965b
Tatiana <43054051+tacoelho@users.noreply.github.com>
2021-10-04 15:22:15 -0300

    Update sp_sao_carlos.py
    Add Spider for São Carlos

--- data_collection/gazette/spiders/sp_sao_carlos.py
+++ data_collection/gazette/spiders/sp_sao_carlos.py
@@ -3,5 +3,7 @@ from gazette.spiders.base.doem import DoemGazetteSpider
 
 class SPSaoCarlosSpider(DoemGazetteSpider):
     TERRITORY_ID = "3548906"
-    
     state_city_url_part = "sp/sao carlos"
+    allowed_domains = ["saocarlos.sp.gov.br"]
+    start_urls = ["http://www.saocarlos.sp.gov.br/index.php/diario-oficial.html"]
+    start_date = datetime.date(2009, 6, 11)
