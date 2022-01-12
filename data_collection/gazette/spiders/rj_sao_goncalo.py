23aa65b0d9edd35f0dafa1b6562761afa902dfe6
Giancarlo Susin <susin@gamut.tech>
2022-01-12 19:20:52 -0300

    Fixed small issues. Based on work by @vitorbaptista and @chicocvenancio.

--- data_collection/gazette/spiders/rj_sao_goncalo.py
+++ data_collection/gazette/spiders/rj_sao_goncalo.py
@@ -1,4 +1,7 @@
-import datetime
+from datetime import date, timedelta
+
+import scrapy
+from scrapy.spidermiddlewares.httperror import HttpError
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -7,29 +10,40 @@ from gazette.spiders.base import BaseGazetteSpider
 class RjSaoGoncaloSpider(BaseGazetteSpider):
     TERRITORY_ID = "3304904"
     DOWNLOAD_GAZETTE_URL = "https://servicos.pmsg.rj.gov.br/diario/{}.pdf"
-    start_date = datetime.date(1998, 2, 3)
-
+    start_date = date(1998, 2, 3)
+    end_date = date.today()
     name = "rj_sao_goncalo"
-    allowed_domains = ["servicos.pmsg.rj.gov.br"]
-    start_urls = ["https://servicos.pmsg.rj.gov.br/diario_oficial.php"]
+    allowed_domains = ["pmsg.rj.gov.br"]
+    start_urls = ["https://www.pmsg.rj.gov.br/diario-oficial"]
 
     def parse(self, response):
-        date = datetime.date.today()
-        start_date = self.start_date
-        while date >= start_date:
-            is_weekend = date.weekday() >= 5
-            if is_weekend:
-                date = date - datetime.timedelta(days=1)
-                continue
-
-            url = self.DOWNLOAD_GAZETTE_URL.format(date.strftime("%Y_%m_%d"))
-            # TODO: How to figure out is_extra_edition? Doesn't seem possible
-            # without extracting the text.
-            yield Gazette(
-                date=date,
-                file_urls=[url],
-                territory_id=self.TERRITORY_ID,
-                power="executive",
-                scraped_at=datetime.datetime.utcnow(),
+        empty_files = [date(1998, 10, 15), date(2006, 2, 3), date(2006, 4, 27)]
+        curr_date = self.end_date
+        while curr_date >= self.start_date:
+            url = self.DOWNLOAD_GAZETTE_URL.format(curr_date.strftime("%Y_%m_%d"))
+            yield scrapy.Request(
+                url,
+                cb_kwargs={
+                    "gazette_date": curr_date,
+                    "file_urls": [url],
+                },
+                callback=self.parse_gazette,
+                errback=self.errback_gazette,
             )
-            date = date - datetime.timedelta(days=1)
+            while True:
+                curr_date = curr_date - timedelta(days=1)
+                # Known empty files are ignored so as not to break validation
+                if curr_date not in empty_files:
+                    break
+
+    def parse_gazette(self, response, gazette_date, file_urls):
+        yield Gazette(
+            date=gazette_date,
+            file_urls=file_urls,
+            power="executive",
+        )
+
+    def errback_gazette(self, failure):
+        # Gazette doesn't exist for some dates so response http 404 is not an error
+        if not failure.check(HttpError):
+            self.logger.error(repr(failure))
