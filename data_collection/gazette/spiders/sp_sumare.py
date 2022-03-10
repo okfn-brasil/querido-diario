cb7e354e0732335039fcb473a46e41fdf09f2644
Giulio Carvalho <giulioccavalcante@gmail.com>
2022-03-10 17:04:59 -0300

    Remove uso de variável gazette_data

--- data_collection/gazette/spiders/sp_sumare.py
+++ data_collection/gazette/spiders/sp_sumare.py
@@ -7,7 +7,7 @@ from gazette.spiders.base import BaseGazetteSpider
 
 class SpSumareSpider(BaseGazetteSpider):
     TERRITORY_ID = "3552403"
-    allowed_domains = ["https://www.sumare.sp.gov.br"]
+    allowed_domains = ["sumare.sp.gov.br"]
     name = "sp_sumare"
     start_urls = ["https://www.sumare.sp.gov.br/Diario.Oficial.php?edicao=todas"]
     base_url = "https://www.sumare.sp.gov.br/"
@@ -25,15 +25,10 @@ class SpSumareSpider(BaseGazetteSpider):
             if hasattr(self, "start_date") and date < self.start_date:
                 continue
 
-            gazette_data = {
-                "edition_number": re.search(r"\d+", title).group(0),
-                "date": date,
-                "file_urls": [f"{self.base_url}{url}"],
-                "is_extra_edition": "extra" in title.lower(),
-                "power": "executive",
-            }
-
             yield Gazette(
-                **gazette_data,
-                scraped_at=datetime.utcnow(),
+                edition_number=re.search(r"\d+", title).group(0),
+                date=date,
+                file_urls=[f"{self.base_url}{url}"],
+                is_extra_edition="extra" in title.lower(),
+                power="executive",
             )
