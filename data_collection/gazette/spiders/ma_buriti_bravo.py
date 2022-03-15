4bd2de9ff19270ec009fabdb9f8565e1e96c2d18
Alen Vieira <github@alenvieira.com>
2022-03-14 21:33:31 -0300

    Change ma_buriti_bravo spider

--- data_collection/gazette/spiders/ma_buriti_bravo.py
+++ data_collection/gazette/spiders/ma_buriti_bravo.py
@@ -14,24 +14,21 @@ class MaBuritiBravoSpider(BaseGazetteSpider):
     url_base = "http://diario.buritibravo.ma.gov.br/publicacao/selecionar.php"
     TERRITORY_ID = "2102200"
 
-    def start_requests(self):
-        yield scrapy.Request(self.url_base, callback=self.parse_page)
-
-    def parse_page(self, response):
+    def parse(self, response):
         last_page = int(
             response.xpath("//div[@class='paginacao']/a[last()]/span/text()").get()
         )
-        for page in range(0, last_page):
+        for page in range(last_page):
             yield scrapy.FormRequest(
-                url=response.url, method="POST", formdata={"pag": str(page)}
+                url=response.url, formdata={"pag": str(page), callback=self.parse_page}
             )
 
-    def parse(self, response):
+    def parse_parse(self, response):
         lines = response.xpath("//table/tbody/tr")
 
         for line in lines:
             try:
-                date = datetime.strptime(line.xpath("td[3]/text()").get(), "%d/%m/%Y")
+                date_gazette = parse(line.xpath("td[3]/text()").get(), languages=["pt"]).date()
             except ValueError:
                 continue
             except TypeError:
@@ -40,7 +37,7 @@ class MaBuritiBravoSpider(BaseGazetteSpider):
             edition_number = line.xpath("td[4]/text()").re_first(r"\d+")
 
             yield Gazette(
-                date=date.date(),
+                date=date_gazette,
                 file_urls=[file_url],
                 edition_number=edition_number,
                 is_extra_edition=False,
