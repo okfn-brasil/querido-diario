ae96a6f7ac14eb5c1e0008cb351f2ec25f5cf78d
Eduardo P. Lima <eplima.cc@gmail.com>
2022-09-08 20:33:18 -0300

    Changes pagination and adds follow_next_page flag

--- data_collection/gazette/spiders/rn_macaiba.py
+++ data_collection/gazette/spiders/rn_macaiba.py
@@ -14,25 +14,23 @@ class RnMacaibaSpider(BaseGazetteSpider):
     allowed_domains = ["macaiba.rn.gov.br"]
     start_urls = ["https://macaiba.rn.gov.br/servicos/diario-oficial/"]
     start_date = dt.date(2013, 8, 9)
-    
-    def start_requests(self):
-        start_date_str = self.start_date.strftime("%Y-%m-%d")
-        end_date_str = self.end_date.strftime("%Y-%m-%d")
-        url = f"https://macaiba.rn.gov.br/?post_types=diario-oficial&post_date={start_date_str}+{end_date_str}"
-        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        follow_next_page = True
+
         for edition in response.css("li.link_item a"):
             url = edition.css("::attr(href)").get()
             raw_edition, raw_date = edition.css("::text").get().split(" - ")
             is_extra_edition = "xtra" in raw_edition
             edition_number = re.search(r"(\d+)", raw_edition).group(0).lstrip("0")
             date = dateparser.parse(raw_date).date()
+    
+            if date < self.start_date:
+                follow_next_page = False
+                break
 
             if date > self.end_date:
                 continue
-            elif date < self.start_date:
-                return
 
             yield Gazette(
                 file_urls=[url],
@@ -42,6 +40,7 @@ class RnMacaibaSpider(BaseGazetteSpider):
                 power="executive_legislative",
             )
 
-        next_page_url = response.css("a.nextpostslink::attr(href)").get()
-        if next_page_url:
-            yield scrapy.Request(next_page_url, dont_filter=True)
+        if follow_next_page:
+            next_page_url = response.css("a.nextpostslink::attr(href)").get()
+            if next_page_url:
+                yield scrapy.Request(next_page_url)
