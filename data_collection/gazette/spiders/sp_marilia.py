48eb502bacb41e1e1703fdd1892a13332ee61ced
Gustavo Bonesso <gustavo_bonesso@hotmail.com>
2022-09-28 15:17:10 -0300

    Implementação de Marilia/SP

--- data_collection/gazette/spiders/sp_marilia.py
+++ data_collection/gazette/spiders/sp_marilia.py
@@ -5,4 +5,4 @@ class SpMariliaSpider(BaseInstarSpider):
     TERRITORY_ID = "3529005"
     name = "sp_marilia"
     allowed_domains = ["marilia.sp.gov.br"]
-    start_urls = ["https://www.marilia.sp.gov.br/portal/diario-oficial"]
+    base_url = "https://www.marilia.sp.gov.br/portal/diario-oficial"
