c5692922e3111ae46a9d628a3240b13c30969b29
Gustavo Bonesso <gustavo_bonesso@hotmail.com>
2022-09-28 15:35:16 -0300

    Contagem-MG #625 - Implementação de spider com BaseInstarSpider

--- data_collection/gazette/spiders/mg_contagem.py
+++ data_collection/gazette/spiders/mg_contagem.py
@@ -1,48 +1,8 @@
-import dateparser
-
-from gazette.items import Gazette
-from gazette.spiders.base import BaseGazetteSpider
+from gazette.spiders.base.instar import BaseInstarSpider
 
 
-class MgContagemSpider(BaseGazetteSpider):
+class MgContagemSpider(BaseInstarSpider):
     TERRITORY_ID = "3118601"
     name = "mg_contagem"
     allowed_domains = ["contagem.mg.gov.br"]
-    start_urls = ["http://www.contagem.mg.gov.br/?se=doc"]
-
-    def parse(self, response):
-        """
-        @url http://www.contagem.mg.gov.br/?se=doc&pagina=2
-        @returns items 15 15
-        @scrapes date file_urls is_extra_edition power
-        """
-        anchor_elements = response.css(".texto11pt a")
-
-        urls = [
-            response.urljoin(url)
-            for url in anchor_elements.css("::attr(href)").re(".+pdf")
-        ]
-        extra_editions = ["complementar" in url for url in urls]
-
-        dates_in_sentence = anchor_elements.css("p span:last-child ::text").re(
-            r"(\d{1,2}\s+de\s+\w+\s+de\s+\d{4})"
-        )
-        dates = [
-            dateparser.parse(date, languages=["pt"]).date()
-            for date in dates_in_sentence
-        ]
-
-        for url, date, is_extra_edition in zip(urls, dates, extra_editions):
-            yield Gazette(
-                date=date,
-                file_urls=[url],
-                is_extra_edition=is_extra_edition,
-                power="executive_legislative",
-            )
-
-        number_of_pages = int(
-            response.css("table.subtitulo12pt tr:first-child td ::text").extract()[-1]
-        )
-        for next_page in range(2, number_of_pages + 1):
-            next_page_url = f"{self.start_urls[0]}&pagina={next_page}"
-            yield response.follow(next_page_url, callback=self.parse)
+    base_url = "https://www.portal.contagem.mg.gov.br/portal/diario-oficial"
