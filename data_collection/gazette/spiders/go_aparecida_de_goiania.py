020fa26f91377f899a57ec6679b9bbc681249703
Renne Rocha <renne@rocha.dev.br>
2022-10-03 21:02:03 -0300

    Update Aparecida de Goiania spider
    Update spider code to follow latest best practices:
    - Set a default `start_date` value
    - Avoid the usage of `dateparser` when built-in `datetime` functions accomplish the same
    - Remove not needed log

--- data_collection/gazette/spiders/go_aparecida_de_goiania.py
+++ data_collection/gazette/spiders/go_aparecida_de_goiania.py
@@ -1,6 +1,4 @@
-import json
-
-from dateparser import parse
+import datetime
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -11,26 +9,22 @@ class GoAparecidaDeGoianiaSpider(BaseGazetteSpider):
     name = "go_aparecida_de_goiania"
     allowed_domains = ["aparecida.go.gov.br"]
     start_urls = ["https://webio.aparecida.go.gov.br/api/diof/lista"]
+    start_date = datetime.date(2014, 8, 1)
 
     def parse(self, response):
         download_url = "https://webio.aparecida.go.gov.br/diariooficial/download/{}"
 
-        records = json.loads(response.text)["records"]
+        records = response.json()["records"]
         for record in records:
-            url = download_url.format(record["numero"])
-            power = "executive_legislative"
-            date = parse(record["publicado"], languages=["en"]).date()
+            gazette_url = download_url.format(record["numero"])
+            gazette_date = datetime.datetime.strptime(
+                record["publicado"], "%Y-%m-%d %H:%M:%S"
+            ).date()
 
-            self.logger.info(
-                "Start Date: %s End Date: %s Date: %s",
-                self.start_date.strftime("%d/%m/%Y"),
-                self.end_date.strftime("%d/%m/%Y"),
-                date.strftime("%d/%m/%Y"),
-            )
-            if date >= self.start_date and date <= self.end_date:
+            if gazette_date >= self.start_date and gazette_date <= self.end_date:
                 yield Gazette(
-                    date=date,
-                    file_urls=[url],
+                    date=gazette_date,
+                    file_urls=[gazette_url],
                     is_extra_edition=False,
-                    power=power,
+                    power="executive",
                 )
