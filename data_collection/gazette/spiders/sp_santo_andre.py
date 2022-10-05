6e9a1947b4ea521ef4746e47b2a74e5802034576
Gustavo Bonesso <gustavo_bonesso@hotmail.com>
2022-10-05 16:19:40 -0300

    Duque de Caxias-RJ #616 - Implementação do Spider

--- data_collection/gazette/spiders/sp_santo_andre.py
+++ data_collection/gazette/spiders/sp_santo_andre.py
@@ -1,7 +1,10 @@
+import datetime
+
 from gazette.spiders.base.instar import BaseInstarSpider
 
-class MgContagemSpider(BaseInstarSpider):
-    TERRITORY_ID = "3547809"
+class SpSantoAndre(BaseInstarSpider):
+    TERRITORY_ID = "3547809"  # 3547809,Santo André,SP,São Paulo
     name = "sp_santo_andre"
     allowed_domains = ["santoandre.sp.gov.br"]
     base_url = "https://web.santoandre.sp.gov.br/portal/diario-oficial"
+    start_date = datetime.date(2010, 1, 5)
