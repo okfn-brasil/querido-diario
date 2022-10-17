ce2d0836ad19f3aa1c8d4a2744fb7d10adc34d9f
lucas de oliveira carvalho <lucas.carvalho@sergipetec.corp.org>
2022-10-17 13:33:54 -0300

    added pagination

--- data_collection/gazette/spiders/mt_rondonopolis.py
+++ data_collection/gazette/spiders/mt_rondonopolis.py
@@ -1,5 +1,5 @@
 import datetime
-
+import scrapy
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
@@ -7,20 +7,23 @@ from gazette.spiders.base import BaseGazetteSpider
 class MtRondonopolisSpider(BaseGazetteSpider):
     TERRITORY_ID = "5107602"
     name = "mt_rondonopolis"
-    allowed_domains = ["rondonopolis.mt.gov.br/"]
+    allowed_domains = ["rondonopolis.mt.gov.br"]
     start_urls = ["http://www.rondonopolis.mt.gov.br/diario-oficial/"]
 
-    start_date = datetime.date(2022, 9, 1)
+    start_date = datetime.date(2004, 9, 1)
 
     # o diario vai ate a pagina 238
     # a data mais antiga é 01/09/2004 ; edicao 852
+    # data transiçao para pdf 29/06/11
 
     def parse(self, response):
-        for gazette in response.css("tbody tr"):
+        to_next = True
+        for gazette in response.css("tbody tr"):    
             date = gazette.xpath('td[contains(text(), "/")]//text()').get()
             parsed_date = datetime.datetime.strptime(date, "%d/%m/%y").date()
 
             if self.start_date > parsed_date:
+                to_next = False
                 break
 
             edition = gazette.css("td:first-child::text").get()
@@ -34,8 +37,8 @@ class MtRondonopolisSpider(BaseGazetteSpider):
                 edition_number=edition,
             )
 
-        next_page = response.xpath(
-            '//li[@class="page-item"]/a[contains(text(), "Próxima")]//@href'
-        ).get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        if to_next:
+            next_page = response.xpath(
+                '//li[@class="page-item"]/a[contains(text(), "Próxima")]//@href'
+            ).get()
+            yield scrapy.Request(f'http://www.rondonopolis.mt.gov.br/diario-oficial/{next_page}')
