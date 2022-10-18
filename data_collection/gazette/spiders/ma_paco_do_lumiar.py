d2b52571e541db2dad44a53f44e96b4f0e8303f7
Lucca Bortoloso <lucca.bortoloso@souunit.com.br>
2022-10-18 11:02:13 -0300

    Add MaPacodoLumiarSpider

--- data_collection/gazette/spiders/ma_paco_do_lumiar.py
+++ data_collection/gazette/spiders/ma_paco_do_lumiar.py
@@ -1,7 +1,5 @@
 import datetime as dt
 
-import scrapy
-from dateutil.rrule import DAILY, rrule
 from scrapy import Request
 
 from gazette.items import Gazette
@@ -9,49 +7,100 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class MaPacodoLumiarSpider(BaseGazetteSpider):
+
+    # O Diário Oficial n° 117/2021 Legislativo não possui o PDF no link
+
     TERRITORY_ID = "2107506"
     name = "ma_paco_do_lumiar"
     start_date = dt.date(2017, 8, 3)
+    end_date = dt.date.today()
     allowed_domains = ["pacodolumiar.ma.gov.br"]
 
     BASE_URL = "https://www.pacodolumiar.ma.gov.br/diariooficial.php"
 
     def start_requests(self):
-        dates = rrule(freq=DAILY, dtstart=self.start_date, until=dt.date.today())
-        for date in dates:
-            yield Request(
-                f"{self.BASE_URL}?descr=&esfera=0&Num=&dtini={date.day}%2F{date.month}%2F{date.year}&dtfim=&Exerc="
-            )
+        start_date = self.start_date.strftime("%d/%m/%Y")
+        end_date = self.end_date.strftime("%d/%m/%Y")
+
+        for i in range(1, 3):
+            start_url = f"{self.BASE_URL}?descr=&esfera={i}&Num=&dtini={start_date}&dtfim={end_date}"
+            yield Request(start_url)
 
     def parse(self, response):
-        gazettes = response.css(".tab-pane.active").css(".list-group-item")
-        root_url = "https://www.pacodolumiar.ma.gov.br/"
+        follow_next_page = True
+        gazettes = response.css(".tab-pane.active .list-group-item")
+
         for gazette in gazettes:
             gazette_date = dt.datetime.strptime(
-                gazettes.css("span.calendarioIcon::text").get().replace(" ", ""),
+                gazette.css("span.calendarioIcon::text").get().replace(" ", ""),
                 "%d/%m/%Y",
             ).date()
-            edition_number = (
-                gazettes.xpath("//span/strong/text()")
-                .get()
-                .split(": ")[1]
-                .split("/")[0]
+
+            if gazette_date < self.start_date:
+                follow_next_page = False
+                break
+
+            url = gazette.css(".pull-right a::attr(href)").get()
+            yield Request(response.urljoin(url), callback=self.extract_information)
+
+        if (
+            gazettes.xpath(
+                "//div[@class='tab-pane active']//nav//ul//li/a[re:test(@aria-label, 'Next')]/span/i"
+            ).get()
+            is None
+        ):
+            follow_next_page = False
+
+        if follow_next_page:
+            next_page_url = gazettes.xpath(
+                "//div[@class='tab-pane active']//nav//ul//li/a[re:test(@aria-label, 'Next')]/@href"
+            ).getall()[-1]
+            yield Request(f"{self.BASE_URL}{next_page_url}")
+
+    def extract_information(self, response):
+        is_extra_edition = False
+
+        edition_number = (
+            response.xpath("//div[@class='public_paginas']//h1/strong/text()")
+            .get()
+            .split(": ")[-1]
+            .split("/")[0]
+        )
+
+        if edition_number.endswith("-A"):
+            is_extra_edition = True
+
+        power = (
+            response.xpath(
+                "//div[@class='public_paginas']//div[@class='row']//span/text()"
             )
-            gazette_next_url = gazettes.css(".pull-right a::attr(href)").get()
+            .get()
+            .lower()
+        )
+        power = power.replace("o", "e")
 
-            yield scrapy.Request(f"{root_url}{gazette_next_url}")
+        gazette_url = response.urljoin(
+            response.xpath(
+                "//div[@class='row']//div/a[re:test(@href, 'diario/')]/@href"
+            ).get()
+        )
 
-            pdf_url = (
-                response.css(".public_paginas .row .btn.btn-primary")
-                .css("a::attr(href)")
-                .get()
+        gazette_date = (
+            response.xpath(
+                "//div[@class='public_paginas']//div[@class='row']/div/text()"
             )
-            gazette_url = f"{root_url}{pdf_url}"
+            .getall()[1]
+            .replace(" ", "")
+        )
+        gazette_date = dt.datetime.strptime(gazette_date, "%d/%m/%Y").date()
 
+        if gazette_url.endswith(".pdf"):
             yield Gazette(
                 date=gazette_date,
                 edition_number=edition_number,
-                file_urls=gazette_url,
-                is_extra_edition=False,
-                power="executive",
+                file_urls=[gazette_url],
+                is_extra_edition=is_extra_edition,
+                power=power,
             )
+        else:
+            pass
