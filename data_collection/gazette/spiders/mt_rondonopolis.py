766e931f9d88dbd4fd0ebb9a4dd91812b55e923c
Renne Rocha <renne@rocha.dev.br>
2022-10-19 18:38:46 -0300

    Update Rondonopolis-MT spider
    - Remover requests desnecessários
    - Simplificar o uso de seletores para obter lista de Diários Oficiais disponíveis
    - Adicionar configuração para fazer download de qualquer arquivo, sem se preocupar com o COntent-Length

--- data_collection/gazette/spiders/mt_rondonopolis.py
+++ data_collection/gazette/spiders/mt_rondonopolis.py
@@ -11,48 +11,30 @@ class MtRondonopolisSpider(BaseGazetteSpider):
     TERRITORY_ID = "5107602"
     name = "mt_rondonopolis"
     allowed_domains = ["rondonopolis.mt.gov.br"]
-
     BASE_URL = "http://www.rondonopolis.mt.gov.br/diario-oficial/"
-
     start_date = datetime.date(2004, 9, 1)
-    end_date = datetime.date.today()
+
+    custom_settings = {
+        "DOWNLOAD_FAIL_ON_DATALOSS": False,
+    }
 
     def start_requests(self):
         for date in rrule(DAILY, dtstart=self.start_date, until=self.end_date):
+            search_date = date.strftime("%Y-%m-%d")
             yield scrapy.Request(
-                f"{self.BASE_URL}?publish_date={date.date()}&number=&q=",
+                f"{self.BASE_URL}?publish_date={search_date}&number=&q=",
                 cb_kwargs={"gazette_date": date.date()},
             )
 
     def parse(self, response, gazette_date):
-        if response.css("tbody tr").get() is not None:
-            for gazette in response.css("tbody tr"):
-                edition = gazette.css("td:first-child::text").get()
-                pdf_url = response.urljoin(gazette.css("td a::attr(href)").get())
+        for gazette in response.css("tbody tr"):
+            edition_number = gazette.css("td:first-child::text").get()
+            gazette_url = response.urljoin(gazette.css("td a::attr(href)").get())
 
-                if pdf_url.endswith(".pdf") or pdf_url.endswith(".zip"):
-                    item = Gazette(
-                        date=gazette_date,
-                        file_urls=[pdf_url],
-                        is_extra_edition=False,
-                        edition_number=edition,
-                        power="executive",
-                    )
-
-                    yield scrapy.Request(
-                        pdf_url,
-                        callback=self.http_response,
-                        errback=self.http_error,
-                        cb_kwargs={"item": item},
-                    )
-
-                else:
-                    pass
-
-    def http_response(self, response, item):
-        yield Gazette(
-            **item,
-        )
-
-    def http_error(self, failure):
-        pass
+            yield Gazette(
+                date=gazette_date,
+                file_urls=[gazette_url],
+                is_extra_edition=False,
+                edition_number=edition_number,
+                power="executive",
+            )
