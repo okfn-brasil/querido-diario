1846ea7063b5ae011ae40b594736eb33100deed3
Lucca Bortoloso <lucca.bortoloso@souunit.com.br>
2022-10-24 16:20:15 -0300

    Add PiAcauaSpider

---
+++ data_collection/gazette/spiders/pi_acaua.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+
+from gazette.spiders.base.dioenet import BaseDioenetSpider
+
+
+class PiAcauaSpider(BaseDioenetSpider):
+    TERRITORY_ID = "2200053"
+    name = "pi_acaua"
+    start_date = dt.date(2021, 11, 5)
+    end_date = dt.date.today()
+    allowed_domains = ["plenussistemas.dioenet.com.br"]
+    BASE_URL = "https://plenussistemas.dioenet.com.br/list/acaua"
