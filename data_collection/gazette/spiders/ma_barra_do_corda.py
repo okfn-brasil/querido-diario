fff1e00d7a615bc28d0e9b36290d8472beaf22ca
Lucca Bortoloso <lucca.bortoloso@souunit.com.br>
2022-10-24 16:19:34 -0300

    Add MaBarradoCorda

---
+++ data_collection/gazette/spiders/ma_barra_do_corda.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+
+from gazette.spiders.base.dioenet import BaseDioenetSpider
+
+
+class MaBarradoCordaSpider(BaseDioenetSpider):
+    TERRITORY_ID = "2101608"
+    name = "ma_barra_do_corda"
+    start_date = dt.date(2021, 3, 5)
+    end_date = dt.date.today()
+    allowed_domains = ["plenussistemas.dioenet.com.br"]
+    BASE_URL = "https://plenussistemas.dioenet.com.br/list/barra-do-corda"
