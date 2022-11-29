848e911b3b63112d38616c43879dd4a95b1f1651
Renne Rocha <renne@rocha.dev.br>
2022-11-28 21:45:08 -0300

    Atualização do spider de Macapá-AP
    - Corrigindo obtenção de edition_number e date

--- data_collection/gazette/spiders/ap_macapa.py
+++ data_collection/gazette/spiders/ap_macapa.py
@@ -1,5 +1,4 @@
-import datetime as dt
-import re
+import datetime
 
 import dateparser
 import scrapy
@@ -9,17 +8,15 @@ from gazette.spiders.base import BaseGazetteSpider
 
 
 class ApMacapaSpider(BaseGazetteSpider):
-
     name = "ap_macapa"
     allowed_domains = ["macapa.ap.gov.br"]
-    start_date = dt.datetime(2018, 1, 1)
+    start_date = datetime.date(2018, 1, 1)
     TERRITORY_ID = "1600303"
 
     def start_requests(self):
-        base_url = "http://macapa.ap.gov.br/page/{page}/".format(**{"page": 1})
-        start_date = self.start_date.strftime("%d/%m/%Y") if self.start_date else ""
-        end_date = self.end_date.strftime("%d/%m/%Y") if self.end_date else ""
-        self.logger.debug(f"Start Date: {start_date} End Date: {end_date}")
+        base_url = "http://macapa.ap.gov.br/page/1/"
+        start_date = self.start_date.strftime("%d/%m/%Y")
+        end_date = self.end_date.strftime("%d/%m/%Y")
         params = {
             "s": "",
             "post_type": "official_diaries",
@@ -37,39 +34,44 @@ class ApMacapaSpider(BaseGazetteSpider):
 
     def _pagination_requests(self, response, params):
         pages = response.xpath("//a[@class='page-numbers']/text()").getall()
-        last_page = int(pages[-1]) if pages else None
-        if last_page:
-            for next_page in range(1, last_page + 1):
-                self.logger.debug(f"Page {next_page} of {last_page}")
-                next_page_url = "http://macapa.ap.gov.br/page/{page}/".format(
-                    **{"page": next_page}
-                )
+        if pages:
+            last_page = max([int(page) for page in pages])
+            for page in range(1, last_page + 1):
+                next_page_url = f"http://macapa.ap.gov.br/page/{page}/"
                 yield scrapy.FormRequest(
                     url=next_page_url,
                     method="GET",
                     formdata=params,
                     callback=self.parse,
                 )
+        yield from self.parse(response)
+
+    def get_gazette_date(self, gazette):
+        gazette_date = None
+        raw_date = gazette.css("a h4::text").re_first(r"\d{2}\/\d{2}\/\d{4}")
+        if raw_date:
+            gazette_date = datetime.datetime.strptime(raw_date, "%d/%m/%Y").date()
         else:
-            self.logger.debug("One page only")
-            yield from self.parse(response)
+            raw_date = gazette.css("a h4::text").re_first(r"(DE .*)")
+            gazette_date = dateparser.parse(raw_date)
+        return gazette_date
 
     def parse(self, response):
-        # Extract Items
-        extract_number_date = re.compile(
-            r".+?(?P<num>\d{4}).+?(?P<date>\d\d/\d\d/\d\d\d\d)"
-        )
-        divs = response.xpath("//div[@class='panel-body']")[1:]
-        for div in divs:
-            url = div.xpath("./a/@href").get()
-            text = div.xpath("./a/h4/text()").get()
-            num_date = re.match(extract_number_date, text)
-            edition_number = num_date.group("num")
-            date = num_date.group("date")
-            date = dateparser.parse(date, languages=["pt"]).date()
+        gazettes = response.css(".diary > .panel")
+        for gazette in gazettes:
+            gazette_url = gazette.css("a::attr(href)").get()
+            edition_number = gazette.css(".panel-heading ::text").re_first(r"\d+")
+            gazette_date = self.get_gazette_date(gazette)
+            if (
+                gazette_date
+                and gazette_date < self.start_date
+                or gazette_date > self.end_date
+            ):
+                continue
+
             yield Gazette(
-                date=date,
-                file_urls=[url],
+                date=gazette_date,
+                file_urls=[gazette_url],
                 edition_number=edition_number,
                 is_extra_edition=False,
                 power="executive",
