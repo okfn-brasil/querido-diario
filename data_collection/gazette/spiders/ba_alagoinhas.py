1fe3c0ef3c03979f5fdd6586aff2e22b1a5892df
Alexandre Harano <email@ayharano.dev>
2022-12-19 15:26:06 -0300

    Adjust Alagoinhas-BA spider
    resolve okfn-brasil/querido-diario#787

--- data_collection/gazette/spiders/ba_alagoinhas.py
+++ data_collection/gazette/spiders/ba_alagoinhas.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from gazette.spiders.base.doem import DoemGazetteSpider
 
 
 class BaAlagoinhasSpider(DoemGazetteSpider):
     TERRITORY_ID = "2900702"
     name = "ba_alagoinhas"
+    start_date = date(2018, 1, 2)  # edition_number 1.950
     state_city_url_part = "ba/alagoinhas"
