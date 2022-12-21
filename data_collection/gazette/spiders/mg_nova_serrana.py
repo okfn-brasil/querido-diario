b0858029c0f0bf67ced6e636c57da80cd3e12d03
Juliana Trevine <44185775+trevineju@users.noreply.github.com>
2022-12-21 18:29:23 -0300

    Update mg_nova_serrana.py

--- data_collection/gazette/spiders/mg_nova_serrana.py
+++ data_collection/gazette/spiders/mg_nova_serrana.py
@@ -7,5 +7,5 @@ class MgNovaSerranaSpider(BaseInstarSpider):
     TERRITORY_ID = "3145208"
     name = "mg_nova_serrana"
     allowed_domains = ["novaserrana.mg.gov.br"]
-    base_url = "https://www.novaserrana.mg.gov.br/portal/diario-oficial/"
+    base_url = "https://www.novaserrana.mg.gov.br/portal/diario-oficial"
     start_date = date(2015, 10, 27)  # edition_number 1
