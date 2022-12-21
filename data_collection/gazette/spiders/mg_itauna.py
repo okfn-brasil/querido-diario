60fa6b5276b2c64c5bc487a1164bcfa278f16418
Juliana Trevine <44185775+trevineju@users.noreply.github.com>
2022-12-21 18:27:06 -0300

    Update mg_itauna.py

--- data_collection/gazette/spiders/mg_itauna.py
+++ data_collection/gazette/spiders/mg_itauna.py
@@ -7,5 +7,5 @@ class MgItaunaSpider(BaseInstarSpider):
     TERRITORY_ID = "3133808"
     name = "mg_itauna"
     allowed_domains = ["itauna.mg.gov.br"]
-    base_url = "https://www.itauna.mg.gov.br/portal/diario-oficial/"
+    base_url = "https://www.itauna.mg.gov.br/portal/diario-oficial"
     start_date = date(2013, 6, 20)  # edition_number 907
