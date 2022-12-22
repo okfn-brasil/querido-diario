8d7e1aa4826d0d55a1fa495a448260ae814ab276
Juliana Trevine <44185775+trevineju@users.noreply.github.com>
2022-12-21 23:23:04 -0300

    Update rs_camaqua.py

--- data_collection/gazette/spiders/rs_camaqua.py
+++ data_collection/gazette/spiders/rs_camaqua.py
@@ -1,10 +1,10 @@
-import datetime
-from gazette.spiders.base.instar import BaseInstarSpider
+from datetime import date
 
+from gazette.spiders.base.instar import BaseInstarSpider
 
 class RsCamaquaSpider(BaseInstarSpider):
     TERRITORY_ID = "4303509"
     name = "rs_camaqua"
     allowed_domains = ["camaqua.rs.gov.br"]
     base_url = "https://www.camaqua.rs.gov.br/portal/diario-oficial"
-    start_date = datetime.date(2019, 7, 25)
+    start_date = date(2019, 7, 25)
