d469f8457c8e46f9b51dd0a2d22598f9a52a138b
Lucca Bortoloso <lucca.bortoloso@souunit.com.br>
2022-12-22 11:19:50 -0300

    Fix Arraial do Cabo-RJ

--- data_collection/gazette/spiders/rj_arraial_do_cabo.py
+++ data_collection/gazette/spiders/rj_arraial_do_cabo.py
@@ -1,8 +1,11 @@
+import datetime
+
 from gazette.spiders.base.instar import BaseInstarSpider
 
 
 class RjArraialdoCabopider(BaseInstarSpider):
     TERRITORY_ID = "3300258"
     name = "rj_arraial_do_cabo"
-    allowed_domains = ["rraial.rj.gov.br/"]
-    start_urls = ["https://www.arraial.rj.gov.br/portal/diario-oficial"]
+    allowed_domains = ["arraial.rj.gov.br"]
+    base_url = "https://www.arraial.rj.gov.br/portal/diario-oficial"
+    start_date = datetime.date(2019, 2, 7)
