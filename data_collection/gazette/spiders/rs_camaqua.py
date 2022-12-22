3bcc5b67beb8dd502c5235e261a0b5cbe0da14c9
Juliana Trevine <44185775+trevineju@users.noreply.github.com>
2022-12-21 23:35:23 -0300

    Update rs_camaqua.py

--- data_collection/gazette/spiders/rs_camaqua.py
+++ data_collection/gazette/spiders/rs_camaqua.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from gazette.spiders.base.instar import BaseInstarSpider
 
+
 class RsCamaquaSpider(BaseInstarSpider):
     TERRITORY_ID = "4303509"
     name = "rs_camaqua"
