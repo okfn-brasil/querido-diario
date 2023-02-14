8584ff3c4662c6c79d0fba43a99f65e9972c2335
Renne Rocha <renne@rennerocha.com>
2023-02-13 22:23:40 -0300

    Finish Aldeias Altas - MA

--- data_collection/gazette/spiders/ma_aldeias_altas.py
+++ data_collection/gazette/spiders/ma_aldeias_altas.py
@@ -12,35 +12,28 @@ class MaAldeiasAltasSpider(BaseGazetteSpider):
     name = "ma_aldeias_altas"
     allowed_domains = ["aldeiasaltas.ma.gov.br"]
     start_date = date(2017, 5, 3)
-    url_base = "http://aldeiasaltas.ma.gov.br/diario-oficial?data={}"  # 2017-05-03
+    url_base = "http://aldeiasaltas.ma.gov.br/diario-oficial?data={}"
     TERRITORY_ID = "2100303"
 
     def start_requests(self):
-        days = rrule(freq=DAILY, dtstart=self.start_date, until=date.today())
+        days = rrule(freq=DAILY, dtstart=self.start_date, until=self.end_date)
         for day in days:
             yield scrapy.Request(self.url_base.format(day.strftime("%Y-%m-%d")))
 
     def parse(self, response):
-        gazettes = response.xpath("//table/tbody/tr")
-        gazettes.reverse()
-        for gazette_number, gazette in enumerate(gazettes):
-            file_url = gazette.xpath("td[4]/a/@href").get()
-            if not (file_url):
-                continue
-            edition_info = gazette.xpath("td[1]/text()")
-            if edition_info.get() == "Não tem atos oficiais nesta data":
-                continue
+        gazettes = response.css("tr")
+        for gazette in gazettes:
+            gazette_url = gazette.css("a::attr(href)")
             gazette_date = datetime.strptime(
                 gazette.xpath("td[2]/text()").get(), "%d/%m/%Y"
             )
-            is_extra_edition = bool(gazette_number)
-            edition_number = edition_info.re_first(r"\d+\/\d+|\d+")
+            edition_number = gazette.xpath("td[1]/text()").re_first(r"\d+\/\d+|\d+")
+
             yield Gazette(
                 date=gazette_date.date(),
-                file_urls=[file_url],
+                file_urls=[
+                    gazette_url,
+                ],
                 edition_number=edition_number,
-                is_extra_edition=is_extra_edition,
-                territory_id=self.TERRITORY_ID,
                 power="executive",
-                scraped_at=datetime.utcnow(),
             )
