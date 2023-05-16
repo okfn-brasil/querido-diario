5b8c2ba664c79bc6a70f95eade86153d614645f6
Renne Rocha <renne@rocha.dev.br>
2023-05-15 21:33:51 -0300

    Update sp_santos.py
    - Utilizar dateutil para range de datas
    
    Signed-off-by: Renne Rocha <renne@rocha.dev.br>

--- data_collection/gazette/spiders/sp_santos.py
+++ data_collection/gazette/spiders/sp_santos.py
@@ -1,5 +1,7 @@
 import datetime
 
+from dateutil.rrule import DAILY, rrule
+
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
@@ -15,14 +17,13 @@ class SpSantosSpider(BaseGazetteSpider):
     def parse(self, response):
         dates = response.css("#datas.hidden::text").get()
 
-        while self.end_date >= self.start_date:
-            if str(parsing_date) in dates:
-                url = self.download_url.format(parsing_date)
+        for date in rrule(freq=DAILY, dtstart=self.start_date, until=self.end_date):
+            formatted_date = date.strftime("%Y-%m-%d")
+            if formatted_date in dates:
+                url = self.download_url.format(formatted_date)
                 yield Gazette(
-                    date=parsing_date,
+                    date=date,
                     file_urls=[url],
                     is_extra_edition=False,
                     power="executive_legislative",
                 )
-
-            parsing_date = parsing_date - datetime.timedelta(days=1)
