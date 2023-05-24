45891e637ea814fd3b0668ccab14fe7531fb8b83
trevineju <julianabtrevine@gmail.com>
2023-05-23 22:43:10 -0300

    Ajusta Mossoro-RN
    fix: correções de revisão

--- data_collection/gazette/spiders/rn/rn_mossoro.py
+++ data_collection/gazette/spiders/rn/rn_mossoro.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import re
 
 import scrapy
 
@@ -10,35 +9,50 @@ from gazette.spiders.base import BaseGazetteSpider
 class RnMossoroSpider(BaseGazetteSpider):
     TERRITORY_ID = "2408003"
     name = "rn_mossoro"
-    start_date = dt.date(2023, 1, 1)
+    start_date = dt.date(2023, 1, 2)
     allowed_domains = ["dom.mossoro.rn.gov.br"]
     start_urls = ["https://www.dom.mossoro.rn.gov.br/dom/edicoes"]
 
     def parse(self, response):
         for edition in response.css("div.edicoes-list div.col-md-3"):
-            url = edition.css("a::attr(href)").get()
             raw_date = edition.css("div.card-content p::text").get().strip()
-            date = dt.datetime.strptime(raw_date, "%d/%m/%Y").date()
-            raw_edition_number = edition.css("div.card-content h4::text").get().strip()
-            edition_number = re.findall(r"DOM N. (\d+)", raw_edition_number)
+            gazette_date = dt.datetime.strptime(raw_date, "%d/%m/%Y").date()
 
-            if date > self.end_date:
+            if gazette_date > self.end_date:
                 continue
-            elif date < self.start_date:
+            if self.start_date > gazette_date:
                 return
 
-            yield Gazette(
-                date=date,
-                edition_number=edition_number,
-                file_urls=[f"https://www.dom.mossoro.rn.gov.br{url}"],
-                is_extra_edition=False,
-                power="executive_legislative",
+            intermediary_page = edition.css("a::attr(href)").get()
+            yield scrapy.Request(
+                response.urljoin(intermediary_page),
+                callback=self.parse_gazette_page,
+                cb_kwargs={"gazette_date": gazette_date},
             )
 
         next_page_url = response.xpath(
             "//a[contains(text(), 'PRÓXIMA PÁGINA')]/@href"
         ).get()
         if next_page_url:
-            yield scrapy.Request(
-                f"https://www.dom.mossoro.rn.gov.br{next_page_url}", callback=self.parse
-            )
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def extra_edition_check(self, edition_number):
+        if edition_number.isdigit():
+            return False
+        return True
+
+    def parse_gazette_page(self, response, gazette_date):
+        edition_number = response.xpath(
+            "//div[@id='main-content']//li[3]//strong/text()"
+        ).get()
+        file_link = response.xpath(
+            "//div[@id='main-content']//a[contains(@href, '/pmm/uploads/publicacao/pdf')]/@href"
+        ).get()
+
+        yield Gazette(
+            date=gazette_date,
+            edition_number=edition_number,
+            file_urls=[response.urljoin(file_link)],
+            is_extra_edition=self.extra_edition_check(edition_number),
+            power="executive",
+        )
