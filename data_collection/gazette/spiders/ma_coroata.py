dc0d9b76bfd13c928dbbe3fe64c9cf0a3c9bad54
Giulio Carvalho <giulioccavalcante@gmail.com>
2023-06-16 16:51:24 -0300

    Adiciona Coroatá e São José dos Basílios (MA)

---
+++ data_collection/gazette/spiders/ma_coroata.py
@@ -0,0 +1,11 @@
+import datetime
+
+from gazette.spiders.base.siganet import BaseSiganetSpider
+
+
+class MaCoroata(BaseSiganetSpider):
+    TERRITORY_ID = "2103604"
+    name = "ma_coroata"
+    start_date = datetime.date(2017, 7, 28)
+    allowed_domains = ["coroata.ma.gov.br"]
+    BASE_URL = "https://coroata.ma.gov.br/diario/diario/"
