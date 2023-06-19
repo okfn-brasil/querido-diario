9b7d9425ab9f3edf64e1168c8e474d0b243f0ada
Valério Wittler <valeriow@gmail.com>
2023-06-19 17:37:21 -0300

    Alterada classe BaseSiganetSpider (siganet.py) para comportar a versão usada por ma_coroata e ma_sao_jose_dos_basilios  sem quebrar, juntamente com a versão usada por ma_sao_luis. Adicionado spider para ma_sao_luis.

---
+++ data_collection/gazette/spiders/ma_sao_luis.py
@@ -0,0 +1,12 @@
+import datetime
+
+from gazette.spiders.base.siganet import BaseSiganetSpider
+
+
+class MaSaoLuis(BaseSiganetSpider):
+    TERRITORY_ID = "2111300"
+    name = "ma_sao_luis"
+    start_date = datetime.date(1993, 1, 4)
+    allowed_domains = ["diariooficial.saoluis.ma.gov.br"]
+    BASE_URL = "https://diariooficial.saoluis.ma.gov.br/dom/dom"
+    P_VERSION = "2"  # see BaseSiganetSpider
