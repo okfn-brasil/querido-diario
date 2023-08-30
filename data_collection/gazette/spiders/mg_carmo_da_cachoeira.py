8b2c2f500ea57183b7e8b668af8112dd01e954b6
trevineju <julianabtrevine@gmail.com>
2023-08-29 23:38:26 -0300

    Atualiza 7 raspadores

--- data_collection/gazette/spiders/mg_carmo_da_cachoeira.py
+++ data_collection/gazette/spiders/mg_carmo_da_cachoeira.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from gazette.spiders.base.instar import BaseInstarSpider
 
 
@@ -5,4 +7,5 @@ class MgCarmodaCachoeiraSpider(BaseInstarSpider):
     TERRITORY_ID = "3113909"
     name = "mg_carmo_da_cachoeira"
     allowed_domains = ["carmodacachoeira.mg.gov.br"]
-    start_urls = ["https://www.carmodacachoeira.mg.gov.br/portal/diario-oficial"]
+    base_url = "https://www.carmodacachoeira.mg.gov.br/portal/diario-oficial"
+    start_date = date(2018, 1, 3)
--- data_collection/gazette/spiders/pr_jaboti.py
+++ data_collection/gazette/spiders/pr_jaboti.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from gazette.spiders.base.instar import BaseInstarSpider
 
 
@@ -5,4 +7,5 @@ class PrJabotiSpider(BaseInstarSpider):
     TERRITORY_ID = "4111704"
     name = "pr_jaboti"
     allowed_domains = ["jaboti.pr.gov.br"]
-    start_urls = ["https://www.jaboti.pr.gov.br/portal/diario-oficial"]
+    base_url = "https://www.jaboti.pr.gov.br/portal/diario-oficial"
+    start_date = date(2018, 1, 3)
