5a1d3128d406e016b154f42780cd9f6d1f9b4123
Winzen <luiz.sinx@gmail.com>
2023-10-02 18:15:05 -0300

    Teste e separação grupo 1 dosp

---
+++ data_collection/gazette/spiders/sp/sp_braganca_paulista.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+from gazette.spiders.base.dosp import DospGazetteSpider
+
+
+class SpBragancaPaulistaSpider(DospGazetteSpider):
+    TERRITORY_ID = "3507605"
+    name = "sp_braganca_paulista"
+    code = "4735"
+    start_date = date(2019, 1, 4)
