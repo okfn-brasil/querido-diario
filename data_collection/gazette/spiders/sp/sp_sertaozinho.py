6c85e4700140f55584875d3858f665cffb340cf0
Winzen <luiz.sinx@gmail.com>
2023-10-03 17:37:46 -0300

    Separação e teste grupo 2 DOSP

---
+++ data_collection/gazette/spiders/sp/sp_sertaozinho.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+from gazette.spiders.base.dosp import DospGazetteSpider
+
+
+class SpSertaozinhoSpider(DospGazetteSpider):
+    TERRITORY_ID = "3551702"
+    name = "sp_sertaozinho"
+    code = "5227"
+    start_date = date(2019, 8, 7)
