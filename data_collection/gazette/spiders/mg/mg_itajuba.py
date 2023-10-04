b58d0a644b45d94d33a42d09a69be648bb3ef284
Winzen <luiz.sinx@gmail.com>
2023-10-04 16:26:31 -0300

    Separação e teste grupo 3 DOSP

---
+++ data_collection/gazette/spiders/mg/mg_itajuba.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+from gazette.spiders.base.dosp import DospGazetteSpider
+
+
+class MgItajubaSpider(DospGazetteSpider):
+    TERRITORY_ID = "3132404"
+    name = "mg_itajuba"
+    code = "1908"
+    start_date = date(2023, 5, 15)
