3f832e48645eee8f5c319cfdd9f7334456a8f6a2
trevineju <julianabtrevine@gmail.com>
2023-10-05 19:31:05 -0300

    Ajusta sp_sarutaia.py

--- data_collection/gazette/spiders/sp/sp_sarutaia.py
+++ data_collection/gazette/spiders/sp/sp_sarutaia.py
@@ -8,4 +8,4 @@ class SpSarutaiaSpider(BaseInstarSpider):
     name = "sp_sarutaia"
     allowed_domains = ["sarutaia.sp.gov.br"]
     start_date = date(2020, 3, 27)
-    start_urls = "https://www.sarutaia.sp.gov.br/portal/diario-oficial"
+    base_url = "https://www.sarutaia.sp.gov.br/portal/diario-oficial"
