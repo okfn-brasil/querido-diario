2486a63aa90c7cc8e106a0404af0ea40ce3fd44d
Luciano Martins <zicadopv@gmail.com>
2023-10-17 19:56:09 -0300

    Finalizando a issue #695

--- data_collection/gazette/spiders/pa/pa_parauapebas.py
+++ data_collection/gazette/spiders/pa/pa_parauapebas.py
@@ -1,37 +1,47 @@
 import datetime as dt
 
 import scrapy
+from dateparser import parse
 
-from gazette.database.models import Gazette
+from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class PaParauapebasSpider(BaseGazetteSpider):
     TERRITORY_ID = "1505536"
     name = "pa_parauapebas"
-    allowed_domains = ["parauapebas.pa.gov.br"]
     start_urls = ["http://apps.ioepa.com.br/Parauapebas/Busca"]
     start_date = dt.date(2022, 1, 4)
 
     def parse(self, response):
-        month = 1
-        year = 2022
-        for day in range(4, 30):
-            data = f"{year}-{month}-{day}"
-            url = f"https://apps.ioepa.com.br/Parauapebas/Busca?data={data}"
-            yield scrapy.Request(
-                url,
-                callback=self.parse_gazette,
-                cb_kwargs={"data": data},
-            )
+        ano_atual = dt.datetime.now().year
+        for year in [2022, ano_atual]:
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    if len(str(day)) < 2:
+                        day = "".join(("0", str(day)))
+                    if len(str(month)) < 2:
+                        month = "".join(("0", str(month)))
+                    data = f"{year}-{month}-{day}"
+                    url = f"{response.url}?data={data}"
+                    yield scrapy.Request(
+                        url,
+                        callback=self.parse_gazette,
+                        cb_kwargs={"data": data},
+                    )
 
     def parse_gazette(self, response, data):
         link_diario = response.css("a[target]").css("::attr(href)").extract_first()
-        edition_number = 1
-        yield Gazette(
-            date=data,
-            edition_number=edition_number,
-            file_urls=[link_diario],
-            is_extra_edition=False,
-            power="executive",
-        )
+        data_do_diario = link_diario.split("/")[-1].split(".")[:-2]
+        data_no_formato_string = str(data)
+        data = data_no_formato_string.split("-")
+        if data_do_diario == data:
+            data_no_formato_date = parse(
+                data_no_formato_string, languages=["br"]
+            ).date()
+            yield Gazette(
+                date=data_no_formato_date,
+                file_urls=[link_diario],
+                is_extra_edition=False,
+                power="executive",
+            )
