56a46797ecb1c45ded832d46126e3cae15b26d0c
dani yumi <139833239+Daniyutak@users.noreply.github.com>
2023-10-24 21:57:15 +0000

    adiciona raspadores de tres cidades pr3 da issue 984

---
+++ data_collection/gazette/spiders/pr/pr_antonio_olinto.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+from gazette.spiders.base.instar import BaseInstarSpider
+
+
+class PrAntonioOlintoSpider(BaseInstarSpider):
+    TERRITORY_ID = "4101309"
+    name = "pr_antonio_olinto"
+    allowed_domains = ["antonioolinto.pr.gov.br"]
+    base_url = "https://antonioolinto.pr.gov.br/portal/diario-oficial"
+    start_date = date(2016, 6, 14)
