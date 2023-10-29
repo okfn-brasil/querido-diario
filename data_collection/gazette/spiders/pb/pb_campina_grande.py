0c8b2beafb171ef51ef3673cdd30688e92e3b625
Johnatan Licar <johnatanlicar@gmail.com>
2023-10-29 19:44:36 -0300

    :hammer: [Revisão retroativa]: Raspador de Campina Grande-PB

--- data_collection/gazette/spiders/pb/pb_campina_grande.py
+++ data_collection/gazette/spiders/pb/pb_campina_grande.py
@@ -1,5 +1,7 @@
 from dateutil.parser import parse
-
+from dateutil.rrule import MONTHLY, rrule
+import scrapy
+from datetime import date
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
@@ -8,33 +10,34 @@ class PbCampinaGrandeSpiderExecutive(BaseGazetteSpider):
     TERRITORY_ID = "2504009"
     allowed_domains = ["campinagrande.pb.gov.br"]
     name = "pb_campina_grande"
-    start_urls = ["https://campinagrande.pb.gov.br/semanario-oficial/"]
+    start_date = date(2017, 1, 1)
+
+    def start_requests(self):
+        dates = rrule(freq=MONTHLY, dtstart=self.start_date, until=date.today())
+        for date_url in dates:
+            for page in range(1,4):
+                yield scrapy.Request(
+                    f"https://campinagrande.pb.gov.br/semanario-oficial/semanario-oficial-{date_url.year}/page/{page}/?mes={date_url.month}",
+                )
 
     def parse(self, response):
-        years_urls = response.css(".secretaria-text a::attr(href)").getall()
-        yield from response.follow_all(years_urls, self.parse_year)
-
-    def parse_year(self, response):
-        months_urls = response.css(".secretaria-text a::attr(href)").getall()
+        months_urls = response.css(".component-archiveFiles .info a::attr(href)").getall()
         yield from response.follow_all(months_urls, self.parse_month)
 
     def parse_month(self, response):
-        editions = response.css(".td_module_1")
+        editions = response.css(".main_single__content")
         if not editions:
             self.logger.debug(f"No editions found at {response.url}")
         else:
             for edition in editions:
-                url = edition.css("a::attr(href)").get()
-                date = parse(edition.css("time::attr(datetime)").get()).date()
-                yield response.follow(url, self.parse_issue, meta={"date": date})
-
-    def parse_issue(self, response):
-        urls = response.css(".td-post-content a::attr(href)").getall()
-        title = response.css("h1::text").get().lower()
-
-        yield Gazette(
-            date=response.meta.get("date"),
-            file_urls=urls,
-            is_extra_edition="separata" in title,
-            power="executive",
-        )
+                urls_editions = edition.css(".main-content-single a::attr(href)").getall()
+                date_tmp = edition.css("div:nth-child(4) > p > strong::text").get()
+                date_edition = parse('/'.join(date_tmp.split()[0].split('/')[::-1])).date()
+                title_edition = response.css("h1.single_title::text").get().replace("\n","").lower()
+                
+                yield Gazette(
+                    date=date_edition,
+                    file_urls=urls_editions,
+                    is_extra_edition="separata" in title_edition,
+                    power="executive",
+                )
