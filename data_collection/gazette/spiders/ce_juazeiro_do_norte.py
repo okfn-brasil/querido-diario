25d4d76289dab1b908fdc7072aff439fb41aa941
Jefersonalves <ferreira.jefersonn@gmail.com>
2023-11-05 12:42:06 -0300

    Adicionar spider de Juazeiro do Norte-CE

---
+++ data_collection/gazette/spiders/ce_juazeiro_do_norte.py
@@ -0,0 +1,12 @@
+import datetime
+
+from gazette.spiders.base.adiarios import AdiariosGazetteSpider
+
+
+class JuazeiroDoNorte(AdiariosGazetteSpider):
+    name = "ce_juazeiro_do_norte"
+    allowed_domains = ["juazeirodonorte.ce.gov.br"]
+    start_date = datetime.date(2021, 1, 12)
+
+    TERRITORY_ID = "2307304"
+    BASE_URL = "http://www.juazeirodonorte.ce.gov.br"
