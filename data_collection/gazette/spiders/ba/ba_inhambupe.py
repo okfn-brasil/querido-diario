9567dfb1068eec647c4ef7406ad0901d9f8d42f1
trevineju <julianabtrevine@gmail.com>
2023-11-17 16:47:53 -0300

    Atualiza código parado e habilita novas cidades

--- data_collection/gazette/spiders/ba/ba_inhambupe.py
+++ data_collection/gazette/spiders/ba/ba_inhambupe.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from gazette.spiders.base.doem import DoemGazetteSpider
 
 
@@ -5,3 +7,4 @@ class BaInhambupeSpider(DoemGazetteSpider):
     TERRITORY_ID = "2913705"
     name = "ba_inhambupe"
     state_city_url_part = "ba/inhambupe"
+    start_date = date(2013, 1, 2)
