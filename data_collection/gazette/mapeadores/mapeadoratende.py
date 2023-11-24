efd2658ede8465c3e6aca81624cee6a3ba7fd076
Alex Silva <alexjbs@id.uff.br>
2023-11-24 08:45:53 -0300

    Inclui informação com o nome da cidade e a sigla do estado encontrados nas páginas visitadas, para mitigar o problema de vinculr a URL na cidade errada. Inclui os links para 'diario' encontrados nas páginas visitadas, de forma a ajudar na identificação do link correto, já que a validação não está muito precisa.

--- data_collection/gazette/mapeadores/mapeadoratende.py
+++ data_collection/gazette/mapeadores/mapeadoratende.py
@@ -1,3 +1,5 @@
+import scrapy
+
 from gazette.mapeadores.base.mapeador import Mapeador
 
 
@@ -6,6 +8,9 @@ class MapeadorAtende(Mapeador):
 
     # No mapeamento completo, todos os links http foram redirecionados para https.
     protocols = ["https"]
+
+    # A list with States to be mapped. If empty, defaults to map All.
+    # states_to_map = ['mg', 'pr', 'rs', 'sc', 'sp']
 
     def column(self):
         return "ATENDE"
@@ -25,6 +30,87 @@ class MapeadorAtende(Mapeador):
             f"{protocol}://{city}.atende.net/diariooficial",
             f"{protocol}://{city}.atende.net/cidadao/pagina/diario-oficial",
         ]
+
+    def start_requests(self):
+        # self.logger.debug(f"STATES_MAP: {repr(self.states_to_map)} {len(self.states_to_map)}")
+        self.read_csv("dados_mapeamento")
+        self.add_column_key()  # add column to fill with search results
+
+        for i in range(len(self.territories)):
+            # Raise TypeError: Mapeador.log() got an unexpected keyword argument 'level'.
+            # Should be renamed to print_status().
+            self.log(i)
+            # self.print_status(i)
+
+            name, state_code = self.format_str(
+                self.territories[i]["name"], self.territories[i]["state_code"]
+            )
+            # if len(self.states_to_map) > 0 and state_code not in self.states_to_map:
+            #     continue
+            for protocol_option in self.protocols:
+                for name_option in self.city_name_generator(name):
+                    for url_option in self.urls_pattern(
+                        protocol_option, name_option, state_code
+                    ):
+                        yield scrapy.Request(
+                            url_option, callback=self.parse, cb_kwargs=dict(index=i)
+                        )
+
+        self.save_csv("dados_mapeamento")
+
+    def parse(self, response, index):
+        # Get information regarding City and State.
+        gov_br_city_state = response.xpath(
+            '//a[contains(@href, "mailto:")][1]/@href'
+        ).re(r"@(\w+)\.(\w+)\.gov\.br")
+        address_city_state = response.css("div.mapa-texto span.linha::text").re(
+            r"\w.+- ?([\w\s]+)/(\w+)"
+        )
+        # Get information regarding 'diario' links.
+        links_diario_oficial = response.xpath(
+            '//a[contains(@href, "diario")][1]/@href'
+        ).getall()
+
+        if self.validation(response):
+            if response.url not in self.territories[index][self.column()]:
+                self.territories[index][self.column()].append(response.url)
+            if (
+                len(gov_br_city_state) > 0
+                and gov_br_city_state not in self.territories[index][self.column()]
+            ):
+                self.territories[index][self.column()].append(gov_br_city_state)
+            if (
+                len(address_city_state) > 0
+                and address_city_state not in self.territories[index][self.column()]
+            ):
+                self.territories[index][self.column()].append(address_city_state)
+            if (
+                len(links_diario_oficial) > 0
+                and links_diario_oficial not in self.territories[index][self.column()]
+            ):
+                self.territories[index][self.column()].append(links_diario_oficial)
+        elif response.url not in self.territories[index][self.backup_column()]:
+            self.territories[index][self.backup_column()].append(response.url)
+            if (
+                len(gov_br_city_state) > 0
+                and gov_br_city_state
+                not in self.territories[index][self.backup_column()]
+            ):
+                self.territories[index][self.backup_column()].append(gov_br_city_state)
+            if (
+                len(address_city_state) > 0
+                and address_city_state
+                not in self.territories[index][self.backup_column()]
+            ):
+                self.territories[index][self.backup_column()].append(address_city_state)
+            if (
+                len(links_diario_oficial) > 0
+                and links_diario_oficial
+                not in self.territories[index][self.backup_column()]
+            ):
+                self.territories[index][self.backup_column()].append(
+                    links_diario_oficial
+                )
 
     def validation(self, response):
         if "atende.net" in response.text:
