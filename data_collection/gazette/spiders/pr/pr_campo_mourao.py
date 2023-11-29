5158d721482abbb7f00f36745e499435d1406f54
Alex Silva <alexjbs@id.uff.br>
2023-11-29 19:58:45 -0300

    Ajusta o spider 'pr_campo_mourao' criado em #438 para trabalhar com o spider base do sistema replicável 'Atende'. Resolve #430 Adiciona spider para Campo Mourão - PR.

--- data_collection/gazette/spiders/pr/pr_campo_mourao.py
+++ data_collection/gazette/spiders/pr/pr_campo_mourao.py
@@ -1,52 +1,10 @@
-import datetime
+from datetime import date
 
-from dateparser import parse
-
-from gazette.items import Gazette
-from gazette.spiders.base import BaseGazetteSpider
+from gazette.spiders.base.atende import BaseAtendeT2Spider
 
 
-class PrCampoMouraoSpider(BaseGazetteSpider):
+class PrCampoMouraoSpider(BaseAtendeT2Spider):
     TERRITORY_ID = "4104303"
     name = "pr_campo_mourao"
-    start_date = datetime.date(2012, 3, 2)
-    start_urls = ["https://campomourao.atende.net/?pg=diariooficial&pagina=1"]
-
-    def parse(self, response, page=1):
-
-        gazettes = response.xpath("//div[@class='nova_listagem ']/div[@class='linha']")
-        follow_next_page = False if not gazettes else True
-
-        for gazette in gazettes:
-            date = gazette.xpath(".//div[@class='data']/text()").get()
-            date = parse(date, languages=["pt"]).date()
-
-            if date < self.start_date:
-                follow_next_page = False
-                break
-
-            edition_type = gazette.xpath(".//div[@class='tipo']/text()").get()
-            edition_number = gazette.xpath(".//div[@class='titulo']/text()").get()
-            edition_number = edition_number.split(" ")[1]
-
-            code = gazette.xpath("//button[@data-acao='download']/@data-codigo").get()
-            id = gazette.xpath("//button[@data-acao='download']/@data-id").get()
-
-            is_extra = True if edition_type == "Extraordinária" else False
-
-            url = f"https://campomourao.atende.net/atende.php?rot=54002&aca=737&processo=download&codigo={code}&hash={id}"
-
-            yield Gazette(
-                date=date,
-                edition_number=edition_number,
-                file_urls=[url],
-                is_extra_edition=is_extra,
-                power="executive_legislative",
-            )
-
-        if follow_next_page:
-            next_page = page + 1
-            yield response.follow(
-                f"https://campomourao.atende.net/?pg=diariooficial&pagina={next_page}",
-                cb_kwargs={"page": next_page},
-            )
+    start_date = date(2012, 2, 3)  # Edição 1511
+    city_subdomain = "campomourao"
