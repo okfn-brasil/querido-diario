8aa4867b5d83da100d91ca271b9e602a00e5a565
Alex Silva <alexjbs@id.uff.br>
2023-12-03 19:21:34 -0300

    Ajuste no spider base 'atende' para buscar a url de download na página de 'Detalhes da Edição' quando não encontrar na página padrão (layout Tipo 2). Caso conhecido: Araucária - PR (https://araucaria.atende.net/diariooficial)

--- data_collection/gazette/spiders/base/atende.py
+++ data_collection/gazette/spiders/base/atende.py
@@ -48,19 +48,23 @@ class BaseAtendeT2Spider(BaseGazetteSpider):
             )
             edition_number = line.css("div.titulo::text").get()
             # edition_number = lines.css("div.titulo::text").re_first(r"[^\s][\d.]+")
-            download_urls = line.css("button::attr(data-link)")
-            if len(download_urls) < 1:
-                self.logger.debug("Unable to find an url for download!")
-                continue
-            download_url = download_urls[-1].get()
-
-            yield Gazette(
+            gazette = Gazette(
                 date=date,
                 edition_number=edition_number,
-                file_urls=[download_url],
                 is_extra_edition=is_extra,
                 power=self.power,
             )
+            download_urls = line.css("button::attr(data-link)")
+            if len(download_urls) > 0:
+                gazette["file_urls"] = [download_urls[-1].get()]
+                yield gazette
+            else:
+                # self.logger.debug("Unable to find an url for download! Trying edition details.")
+                edition_id = line.css("span.bt_detalhes::attr(data-id)").get()
+                edition_url = f"{self.get_base_url()}&parametro=%7B%22codigoPlugin%22%3A2,%22filtroPlugin%22%3A%7B%22codigoEdicao%22%3A%22{edition_id}%22%7D%7D"
+                yield Request(
+                    edition_url, self.parse_edition, cb_kwargs={"gazette": gazette}
+                )
 
         if self.end_page < 1:
             pages = response.css("div#paginacao li.dst button::attr(value)").getall()
@@ -73,5 +77,15 @@ class BaseAtendeT2Spider(BaseGazetteSpider):
         if page <= self.end_page:
             yield response.follow(self.get_url(page), cb_kwargs={"page": page})
 
+    def parse_edition(self, response, gazette):
+        download_url = response.css(
+            "button.visualizacao_versao_completa::attr(data-link)"
+        ).get()
+        gazette["file_urls"] = [download_url]
+        yield gazette
+
+    def get_base_url(self):
+        return f"https://{self.city_subdomain}.atende.net/diariooficial/edicao/pagina/atende.php?rot=54015&aca=101&ajax=t&processo=loadPluginDiarioOficial"
+
     def get_url(self, page):
-        return f"https://{self.city_subdomain}.atende.net/diariooficial/edicao/pagina/atende.php?rot=54015&aca=101&ajax=t&processo=loadPluginDiarioOficial&parametro=%7B%22codigoPlugin%22%3A1,%22filtroPlugin%22%3A%7B%22pagina%22%3A%22{page}%22%7D%7D"
+        return f"{self.get_base_url()}&parametro=%7B%22codigoPlugin%22%3A1,%22filtroPlugin%22%3A%7B%22pagina%22%3A%22{page}%22%7D%7D"
