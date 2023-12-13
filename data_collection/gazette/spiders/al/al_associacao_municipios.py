5e36283de6ca03149ce0a31562d2d03afe93f8da
Giulio Carvalho <giulioccavalcante@gmail.com>
2023-12-13 13:28:01 -0300

    Adiciona raspador para AMA (AL)

--- data_collection/gazette/spiders/al/al_associacao_municipios.py
+++ data_collection/gazette/spiders/al/al_associacao_municipios.py
@@ -1,7 +1,10 @@
+import datetime
+
 from gazette.spiders.base.sigpub import SigpubGazetteSpider
 
 
 class AlAssociacaoMunicipiosSpider(SigpubGazetteSpider):
     name = "al_associacao_municipios"
     TERRITORY_ID = "2700000"
-    CALENDAR_URL = "https://www.diariomunicipal.com.br/ama"
+    CALENDAR_URL = "https://www.diariomunicipal.com.br/ama/"
+    start_date = datetime.date(2014, 4, 10)
