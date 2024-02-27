0869ce060c185fc47d2b9589c216e27950803d9d
trevineju <julianabtrevine@gmail.com>
2024-02-27 08:34:23 -0300

    agape

--- data_collection/gazette/mapeadores/mapeadoragape.py
+++ data_collection/gazette/mapeadores/mapeadoragape.py
@@ -10,6 +10,9 @@ class MapeadorAgape(Mapeador):
     def valid_urls(self):
         return "vAGAPE"
 
+    def current_status(self):
+        return "AGAPE_status"
+
     def urls_pattern(self, protocol, city, state_code):
         # casos conhecidos
         # https://agportal.agapesistemas.com.br/DiarioOficial/?alias=pmRIACHUELO
@@ -24,3 +27,15 @@ class MapeadorAgape(Mapeador):
             if 'div class="item"' in response.text or "bandeira" in response.text:
                 return True
         return False
+
+    def is_current(self, response):
+        raw = response.xpath(
+            '//*[@id="form:j_id39:0"]/table/tbody/tr/td/div/p/text()'
+        ).get()
+
+        if raw is None:
+            return "verificar"
+        elif "/2024" in raw:
+            return "atual"
+        elif "/20" in raw or "/19" in raw:
+            return "descontinuado"
