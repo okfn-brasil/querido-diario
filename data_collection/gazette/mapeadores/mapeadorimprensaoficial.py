200d361c543a138bc52487ddb916a361169c5d25
trevineju <julianabtrevine@gmail.com>
2024-02-27 08:30:09 -0300

    impoficial

--- data_collection/gazette/mapeadores/mapeadorimprensaoficial.py
+++ data_collection/gazette/mapeadores/mapeadorimprensaoficial.py
@@ -1,3 +1,5 @@
+import scrapy
+
 from gazette.mapeadores.base.mapeador import Mapeador
 
 
@@ -30,5 +32,29 @@ class MapeadorImprensaOficial(Mapeador):
                 return True
         return False
 
-    def is_current(self, response):
-        pass
+    def is_current(self, response, index):
+        if "Oops" not in response.text:
+            iscurrent = "atual"
+
+        elif ".org/20" in response.text or ".org/19" in response.text:
+            iscurrent = "descontinuado"
+
+        else:
+            iscurrent = "verificar"
+
+        self.territories[index][self.current_status()].append(iscurrent)
+
+    def parse(self, response, index):
+        if self.validation(response):
+            if response.url not in self.territories[index][self.pattern_name()]:
+                self.territories[index][self.pattern_name()].append(response.url)
+
+            link = response.url[:-16] + "2024/01"
+            yield scrapy.Request(
+                link,
+                callback=self.is_current,
+                cb_kwargs=dict(index=index),
+            )
+
+        elif response.url not in self.territories[index][self.valid_urls()]:
+            self.territories[index][self.valid_urls()].append(response.url)
