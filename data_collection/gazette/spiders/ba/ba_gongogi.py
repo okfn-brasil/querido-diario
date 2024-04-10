245966f341f7b350b9ff15e13e6b031229b56ef9
Claromes <claromes@hey.com>
2024-04-09 21:20:56 -0300

    Revisão de cidades que mudaram o sistema - 2

--- data_collection/gazette/spiders/ba/ba_gongogi.py
+++ data_collection/gazette/spiders/ba/ba_gongogi.py
@@ -3,9 +3,9 @@ from datetime import date
 from gazette.spiders.base.sai import SaiGazetteSpider
 
 
-class BaGongogiSaiGazetteSpider(SaiGazetteSpider):
-    name = "ba_gongogi_2024"
+class BaGongogi(SaiGazetteSpider):
+    name = "ba_gongogi"
     allowed_domains = ["gongogi.ba.gov.br"]
-    start_date = date(2024, 1, 1)
+    start_date = date(2005, 8, 15)
     base_url = "https://www.gongogi.ba.gov.br"
     TERRITORY_ID = "2911501"
--- data_collection/gazette/spiders/ba/ba_itaquara_2019.py
+++ data_collection/gazette/spiders/ba/ba_itaquara_2019.py
@@ -1,11 +1,15 @@
+"""The files between the years 2019 and 2022 are available using the Imprensa Oficial system.
+This city now uses the BR Transparência system, which covers other cities. The Base is not developed.
+"""
 from datetime import date
 
 from gazette.spiders.base.imprensa_oficial import ImprensaOficialSpider
 
 
 class BaItaquaraSpider(ImprensaOficialSpider):
-    name = "ba_itaquara"
+    name = "ba_itaquara_2019"
     allowed_domains = ["pmitaquaraba.imprensaoficial.org"]
     start_date = date(2019, 1, 1)
+    end_date = date(2022, 1, 4)
     city_domain = "http://pmitaquaraba.imprensaoficial.org"
     TERRITORY_ID = "2916708"
