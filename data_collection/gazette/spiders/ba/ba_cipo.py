2b3a163203bc0cfc083122110b17e62156e44ed9
trevineju <julianabtrevine@gmail.com>
2024-05-31 01:41:14 -0300

    Atualiza start_date de raspadores DOEM

--- data_collection/gazette/spiders/ba/ba_cipo.py
+++ data_collection/gazette/spiders/ba/ba_cipo.py
@@ -6,5 +6,5 @@ from gazette.spiders.base.doem import DoemGazetteSpider
 class BaCipoSpider(DoemGazetteSpider):
     TERRITORY_ID = "2907905"
     name = "ba_cipo"
-    start_date = date(2021, 1, 4)
     state_city_url_part = "ba/cipo"
+    start_date = date(2012, 1, 2)
--- data_collection/gazette/spiders/ba/ba_morro_do_chapeu.py
+++ data_collection/gazette/spiders/ba/ba_morro_do_chapeu.py
@@ -6,5 +6,5 @@ from gazette.spiders.base.doem import DoemGazetteSpider
 class BaMorroDoChapeuSpider(DoemGazetteSpider):
     TERRITORY_ID = "2921708"
     name = "ba_morro_do_chapeu"
-    start_date = date(2021, 1, 6)
     state_city_url_part = "ba/morrodochapeu"
+    start_date = date(2013, 3, 1)
