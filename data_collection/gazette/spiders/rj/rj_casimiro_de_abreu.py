b1c8b7dbf00404a3b6837cf0e478e4be39a29c8b
trevineju <julianabtrevine@gmail.com>
2024-06-10 10:54:08 -0300

    Consolida padrão ADIARIOS v2

--- data_collection/gazette/spiders/rj/rj_casimiro_de_abreu.py
+++ data_collection/gazette/spiders/rj/rj_casimiro_de_abreu.py
@@ -4,8 +4,8 @@ from gazette.spiders.base.adiarios_v2 import BaseAdiariosV2Spider
 
 
 class RjCasimiroDeAbreuSpider(BaseAdiariosV2Spider):
-    name = "rj_casimiro_de_abreu"
     TERRITORY_ID = "3301306"
-    start_date = date(2017, 1, 4)
+    name = "rj_casimiro_de_abreu"
     allowed_domains = ["casimirodeabreu.rj.gov.br"]
-    BASE_URL = "https://transparencia.casimirodeabreu.rj.gov.br"
+    BASE_URL = "https://www.transparencia.casimirodeabreu.rj.gov.br"
+    start_date = date(2017, 1, 4)
