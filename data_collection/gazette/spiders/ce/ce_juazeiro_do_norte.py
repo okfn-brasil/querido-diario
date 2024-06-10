ab5c8628ddd9dd4b6938108c6ecbbc9f68b77489
trevineju <julianabtrevine@gmail.com>
2024-06-10 10:52:37 -0300

    Consolida padrão ADIARIOS

--- data_collection/gazette/spiders/ce/ce_juazeiro_do_norte.py
+++ data_collection/gazette/spiders/ce/ce_juazeiro_do_norte.py
@@ -1,12 +1,11 @@
-import datetime
+from datetime import date
 
 from gazette.spiders.base.adiarios_v1 import BaseAdiariosV1Spider
 
 
 class CeJuazeiroDoNorteSpider(BaseAdiariosV1Spider):
+    TERRITORY_ID = "2307304"
     name = "ce_juazeiro_do_norte"
     allowed_domains = ["juazeirodonorte.ce.gov.br"]
-    start_date = datetime.date(2021, 1, 12)
-
-    TERRITORY_ID = "2307304"
     BASE_URL = "https://www.juazeirodonorte.ce.gov.br"
+    start_date = date(2021, 1, 12)
