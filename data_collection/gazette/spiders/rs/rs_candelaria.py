f8193c68484eb811892986c8610342b996d84b49
trevineju <julianabtrevine@gmail.com>
2024-06-10 11:10:01 -0300

    Consolida padrão ATENDE v2 - com alteração de start_date

--- data_collection/gazette/spiders/rs/rs_candelaria.py
+++ data_collection/gazette/spiders/rs/rs_candelaria.py
@@ -7,4 +7,8 @@ class RsCandelariaSpider(BaseAtendeV2Spider):
     TERRITORY_ID = "4304200"
     name = "rs_candelaria"
     city_subdomain = "candelaria"
+<<<<<<< HEAD
     start_date = date(2023, 5, 7)
+=======
+    start_date = date(2024, 5, 7)
+>>>>>>> Consolida padrão ATENDE v2 - com alteração de start_date
