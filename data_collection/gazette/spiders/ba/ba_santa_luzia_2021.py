956993615ff57a64878d88140532156c0738413f
trevineju <julianabtrevine@gmail.com>
2024-06-19 15:47:56 -0300

    Atualiza raspador para Santa Luzia-BA

--- data_collection/gazette/spiders/ba/ba_santa_luzia_2021.py
+++ data_collection/gazette/spiders/ba/ba_santa_luzia_2021.py
@@ -5,6 +5,7 @@ from gazette.spiders.base.doem import DoemGazetteSpider
 
 class BaSantaLuziaSpider(DoemGazetteSpider):
     TERRITORY_ID = "2928059"
-    name = "ba_santa_luzia"
-    start_date = date(2021, 1, 4)
+    name = "ba_santa_luzia_2021"
     state_city_url_part = "ba/santaluzia"
+    start_date = date(2021, 1, 4)
+    end_date = date(2024, 1, 29)
