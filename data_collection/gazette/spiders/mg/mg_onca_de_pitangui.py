9295094c6dcca40de2a47f3064c0e713a2e25e2b
trevineju <julianabtrevine@gmail.com>
2024-06-25 12:51:58 -0300

    Ajusta TERRITORY_ID para Onça de Pitangui

--- data_collection/gazette/spiders/mg/mg_onca_de_pitangui.py
+++ data_collection/gazette/spiders/mg/mg_onca_de_pitangui.py
@@ -3,9 +3,9 @@ from datetime import date
 from gazette.spiders.base.instar import BaseInstarSpider
 
 
-class MgOncaDoPitanguiSpider(BaseInstarSpider):
-    TERRITORY_ID = "3151404"
-    name = "mg_onca_do_pitangui"
+class MgOncaDePitanguiSpider(BaseInstarSpider):
+    TERRITORY_ID = "3145802"
+    name = "mg_onca_de_pitangui"
     allowed_domains = ["oncadopitangui.mg.gov.br"]
     base_url = "https://www.oncadopitangui.mg.gov.br/portal/diario-oficial"
     start_date = date(2021, 5, 24)
