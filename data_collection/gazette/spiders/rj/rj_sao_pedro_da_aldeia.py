906292444700fdaf4565d60f6696872d434b6e70
Fábio Lima <fabio.sza.lima@gmail.com>
2024-07-30 21:22:13 -0300

    Adiciona sistema modernização e municípios que adotam a solução
    Aplicando sugestão da revisão
    
    Co-authored-by: Juliana Trevine <44185775+trevineju@users.noreply.github.com>
    Signed-off-by: Fábio Lima <67182132+slfabio@users.noreply.github.com>
    
    Adiciona ajustes em spider base modernizacao

---
+++ data_collection/gazette/spiders/rj/rj_sao_pedro_da_aldeia.py
@@ -0,0 +1,11 @@
+import datetime as dt
+
+from gazette.spiders.base.modernizacao import BaseModernizacaoSpider
+
+
+class RjSaoPedroDaAldeiaSpider(BaseModernizacaoSpider):
+    TERRITORY_ID = "3305208"
+    name = "rj_sao_pedro_da_aldeia"
+    allowed_domains = ["transparencia.pmspa.rj.gov.br"]
+    start_date = dt.date(2018, 1, 15)
+    ver_subpath = "ver20240713"
