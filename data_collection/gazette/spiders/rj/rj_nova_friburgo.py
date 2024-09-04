56d868098024b3883bc0bfaed4a7542bdc4b8e46
trevineju <julianabtrevine@gmail.com>
2024-09-04 16:22:56 -0300

    Refatora dioenet e propaga ajustes em spiders que o utiliza

--- data_collection/gazette/spiders/rj/rj_nova_friburgo.py
+++ data_collection/gazette/spiders/rj/rj_nova_friburgo.py
@@ -1,9 +1,11 @@
 from datetime import date
 
-from gazette.spiders.base.dioenet import DionetGazetteSpider
+from gazette.spiders.base.dioenet import DioenetGazetteSpider
 
 
-class RjNovaFriburgoSpider(DionetGazetteSpider):
+class RjNovaFriburgoSpider(DioenetGazetteSpider):
     TERRITORY_ID = "3303401"
     name = "rj_nova_friburgo"
     start_date = date(2019, 10, 17)
+    BASE_URL = "https://plenussistemas.dioenet.com.br/list/nova-friburgo"
+    power = "executive_legislative"
--- data_collection/gazette/spiders/sp/sp_taubate.py
+++ data_collection/gazette/spiders/sp/sp_taubate.py
@@ -1,9 +1,11 @@
 from datetime import date
 
-from gazette.spiders.base.dioenet import DionetGazetteSpider
+from gazette.spiders.base.dioenet import DioenetGazetteSpider
 
 
-class SpTaubateSpider(DionetGazetteSpider):
+class SpTaubateSpider(DioenetGazetteSpider):
     TERRITORY_ID = "3554102"
     name = "sp_taubate"
     start_date = date(2022, 12, 8)
+    BASE_URL = "https://plenussistemas.dioenet.com.br/list/taubate"
+    power = "executive_legislative"
