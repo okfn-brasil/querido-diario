cc858e87d2335eaea419d21c66b47c6661a8825d
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-09-11 22:55:46 -0300

    fix: redução de complexidade de código removendo recursos desnecessários

--- data_collection/gazette/spiders/ma/ma_sao_jose_dos_basilios.py
+++ data_collection/gazette/spiders/ma/ma_sao_jose_dos_basilios.py
@@ -1,7 +1,7 @@
 import datetime
 
+import dateparser
 import scrapy
-from dateparser import parse
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -19,15 +19,25 @@ class MaSaoJoseDosBasiliosSpider(BaseGazetteSpider):
 
     def parse(self, response, page=1):
         """
-        Parse each page from the results page and yield the gazette issues available.
+        Parse each page from the results page and yield the gazette issues available and go to next page.
         """
         gazette_boxes = response.css(
             "div#edicoes-anteriores.table-responsive table.table.table-bordered tbody tr"
         )
 
         for gazette_box in gazette_boxes:
-            edition_number = self.get_edition_number(gazette_box)
-            date = self.get_gazette_date(gazette_box)
+            edition_number = (
+                gazette_boxes.css("td:nth-child(1) a::text")
+                .get()
+                .strip()
+                .split(" ")[3]
+                .split("/")[0]
+            )
+
+            date = dateparser.parse(
+                gazette_boxes.css("td:nth-child(3)::text").get().strip().split(",")[1],
+                languages=["pt"],
+            ).date()
 
             if date > self.end_date:
                 continue
@@ -46,18 +56,3 @@ class MaSaoJoseDosBasiliosSpider(BaseGazetteSpider):
             next_page_url = response.css("a.page-link[rel='next']::attr(href)").get()
             if next_page_url:
                 yield scrapy.Request(url=next_page_url)
-
-    def get_gazette_date(self, response_item):
-        """
-        Get the date for the gazette inside one of the 'div#edicoes-anteriores' table
-        """
-        date = response_item.css("td:nth-child(3)::text").get().strip().split(",")[1]
-        return parse(date, date_formats=["%d - %B - %Y"], languages=["pt"]).date()
-
-    def get_edition_number(self, response_item):
-        """
-        Get the edition number inside one of the 'div#edicoes-anteriores' table
-        """
-        text_edition = response_item.css("td:nth-child(1) a::text").get().strip()
-        split_number_year = text_edition.split(" ")[3].split("/")
-        return split_number_year[0]
