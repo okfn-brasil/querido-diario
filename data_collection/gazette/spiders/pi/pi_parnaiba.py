5856e4e68f8b946eeff9894aee3d33e663b84b6a
João Paulo Carvalho <joao.carvalho@jusbrasil.com.br>
2024-09-18 22:12:55 -0300

    fix: url do documento

--- data_collection/gazette/spiders/pi/pi_parnaiba.py
+++ data_collection/gazette/spiders/pi/pi_parnaiba.py
@@ -12,16 +12,17 @@ class PiParnaibaSpider(BaseGazetteSpider):
 
     name = "pi_parnaiba"
     allowed_domains = ["dom.parnaiba.pi.gov.br"]
-    start_urls = ["http://dom.parnaiba.pi.gov.br"]
+    start_urls = ["http://dom.parnaiba.pi.gov.br/"]
     start_date = date(2017, 8, 1)
 
     def parse_page(self, response: HtmlResponse):
         for entry in response.css(".table-diario tbody > tr"):
             edition, date, filename = entry.xpath("./td/text()").extract()
+            file_path = entry.xpath("./td/a/@href").extract_first()
             is_extra_edition = "extra" in filename.lower()
             yield Gazette(
                 date=datetime.strptime(date, "%d-%m-%Y").date(),
-                file_urls=entry.xpath("./td/a/@href").extract(),
+                file_urls=self.start_urls[0] + file_path,
                 edition_number=edition,
                 is_extra_edition=is_extra_edition,
                 territory_id=self.TERRITORY_ID,
@@ -29,5 +30,6 @@ class PiParnaibaSpider(BaseGazetteSpider):
             )
 
     def parse(self, response: HtmlResponse):
-        for url in response.css(".pagination a").extract():
+        pages_urls = response.xpath("//ul[@class='pagination']//a/@href")
+        for url in pages_urls.extract():
             yield Request(url, callback=self.parse_page)
