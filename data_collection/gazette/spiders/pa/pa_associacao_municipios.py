8b813ab9d3844e340c4e72d914c49c791ae0db67
trevineju <julianabtrevine@gmail.com>
2024-09-23 09:53:02 -0300

    padroniza nome de base: sigpub

--- data_collection/gazette/spiders/pa/pa_associacao_municipios.py
+++ data_collection/gazette/spiders/pa/pa_associacao_municipios.py
@@ -1,7 +1,7 @@
-from gazette.spiders.base.sigpub import SigpubGazetteSpider
+from gazette.spiders.base.sigpub import BaseSigpubSpider
 
 
-class PaAssociacaoMunicipiosSpider(SigpubGazetteSpider):
+class PaAssociacaoMunicipiosSpider(BaseSigpubSpider):
     name = "pa_associacao_municipios"
     TERRITORY_ID = "1500000"
     CALENDAR_URL = "https://www.diariomunicipal.com.br/famep"
--- data_collection/gazette/spiders/sp/sp_macatuba.py
+++ data_collection/gazette/spiders/sp/sp_macatuba.py
@@ -1,9 +1,9 @@
 import datetime
 
-from gazette.spiders.base.sigpub import SigpubGazetteSpider
+from gazette.spiders.base.sigpub import BaseSigpubSpider
 
 
-class SpMacatubaSpider(SigpubGazetteSpider):
+class SpMacatubaSpider(BaseSigpubSpider):
     name = "sp_macatuba"
     TERRITORY_ID = "3528007"
     CALENDAR_URL = "https://www.diariomunicipal.com.br/macatuba/"
