890d78e1ff1192bd3482ff3c63307c486ddee4c7
Joseph Reagle <109813833+jreagle@users.noreply.github.com>
2024-09-27 19:58:35 +0000

    Novo Spider: to_caseara.py (SpiderBaseDOBR)

---
+++ data_collection/gazette/spiders/to/to_caseara.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+from gazette.spiders.base.diariooficialbr import BaseDiarioOficialBRSpider
+
+
+class ToCasearaSpider(BaseDiarioOficialBRSpider):
+    TERRITORY_ID = "1703909"
+    name = "to_caseara"
+    allowed_domains = ["caseara.diariooficial.com.br"]
+    BASE_URL = "https://caseara.diariooficialbr.com.br"
+    start_date = date(2019, 6, 26)
