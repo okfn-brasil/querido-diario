2600325f93823b4bb82a4b15841dcc58010b6f81
Joseph Reagle <109813833+jreagle@users.noreply.github.com>
2024-09-27 19:59:00 +0000

    Novo Spider: to_itapiratins.py (SpiderBaseDOBR)

---
+++ data_collection/gazette/spiders/to/to_itapiratins.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+from gazette.spiders.base.diariooficialbr import BaseDiarioOficialBRSpider
+
+
+class ToItapiratinsSpider(BaseDiarioOficialBRSpider):
+    TERRITORY_ID = "1710904"
+    name = "to_itapiratins"
+    allowed_domains = ["itapiratins.diariooficialbr.com.br"]
+    BASE_URL = "https://itapiratins.diariooficialbr.com.br"
+    start_date = date(2017, 4, 3)
+    end_date = date(
+        2024, 1, 10
+    )  # Alert: Discontinued this date! (Verified in 2024-09-24)
