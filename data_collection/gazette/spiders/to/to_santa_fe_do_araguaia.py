92ad20f780610b5d300287482713c8b2c6f873f7
Joseph Reagle <109813833+jreagle@users.noreply.github.com>
2024-10-24 16:53:14 -0300

    Review: Remove forgotten comments
    Co-authored-by: Juliana Trevine <44185775+trevineju@users.noreply.github.com>
    Signed-off-by: Joseph Reagle <109813833+jreagle@users.noreply.github.com>

--- data_collection/gazette/spiders/to/to_santa_fe_do_araguaia.py
+++ data_collection/gazette/spiders/to/to_santa_fe_do_araguaia.py
@@ -5,10 +5,10 @@ from gazette.spiders.base.diariooficialbr import BaseDiarioOficialBRSpider
 
 class ToSantaFedoAraguaiaSpider(
     BaseDiarioOficialBRSpider
-):  # <= Fix: Remove "/uf" from folder name "uf/uf_city"
+): 
     TERRITORY_ID = "1718865"
     name = (
-        "to_santa_fe_do_araguaia"  # <= Fix: Remove "/uf" from folder name "uf/uf_city"
+        "to_santa_fe_do_araguaia" 
     )
     allowed_domains = ["santafedoaraguaia.diariooficialbr.com.br"]
     BASE_URL = "https://santafedoaraguaia.diariooficialbr.com.br"
