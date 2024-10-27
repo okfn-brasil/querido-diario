746c74711ea8d9e5bcd19a87e90a6d01d475b4a9
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2024-10-27 11:57:44 -0300

    Corrigido base url e allowed domains

--- data_collection/gazette/spiders/to/to_ponte_alta_do_tocantins.py
+++ data_collection/gazette/spiders/to/to_ponte_alta_do_tocantins.py
@@ -6,7 +6,7 @@ from gazette.spiders.base.barcodigital import BaseBarcoDigitalSpider
 class toPonteAltaDoTocantins(BaseBarcoDigitalSpider):
     name = "to_ponte_alta_do_tocantins"
     TERRITORY_ID = "1717909"
-    allowed_domains = ["api-lagoadotocantins.barcodigital.com.br"]
-    base_url = "https://api-lagoadotocantins.barcodigital.com.br"
+    allowed_domains = ["api-pontealtadotocantins.barcodigital.com.br"]
+    base_url = "https://api-pontealtadotocantins.barcodigital.com.br"
 
     start_date = date(year=2018, month=5, day=9)
