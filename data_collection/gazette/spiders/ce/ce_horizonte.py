e0116b7cf4092cd529255af767dc50fc376b1fde
trevineju <julianabtrevine@gmail.com>
2024-11-22 01:09:33 -0300

    feat: filename - reflete atualizacao em ce_horizonte (DOSP)

--- data_collection/gazette/spiders/ce/ce_horizonte.py
+++ data_collection/gazette/spiders/ce/ce_horizonte.py
@@ -4,7 +4,8 @@ from gazette.spiders.base.dosp import BaseDospSpider
 
 
 class CeHorizonteSpider(BaseDospSpider):
-    TERRITORY_ID = "2305233"
+    PUBLIC_ENTITY_ID = "2305233"
+    user_website = "https://imprensaoficialmunicipal.com.br/horizonte"
     name = "ce_horizonte"
     code = 687
     start_date = date(2023, 7, 3)
