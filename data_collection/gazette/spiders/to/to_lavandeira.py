deeab9ac171cabffdbe77191634b19d422eff9b5
Pablo Soares <slpkvol3@gmail.com>
2024-12-13 18:42:37 -0300

    Itera e baixa todos itens da tabela
    Também conserta url removendo espaços e adiciona {"DOWNLOAD_DELAY": 1.0}

--- data_collection/gazette/spiders/to/to_lavandeira.py
+++ data_collection/gazette/spiders/to/to_lavandeira.py
@@ -6,7 +6,6 @@ from typing import Iterable
 import scrapy
 from scrapy import Request
 from scrapy.selector import Selector
-from scrapy.shell import inspect_response
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -19,6 +18,7 @@ class ToLavandeira(BaseGazetteSpider):
     start_date = datetime.date(2020, 1, 23)
     BASE_URL = "https://www.lavandeira.to.gov.br/transparencia"
     page_size = 10
+    custom_settings = {"DOWNLOAD_DELAY": 1.0}
 
     def start_requests(self) -> Iterable[Request]:
         data = {
@@ -44,7 +44,6 @@ class ToLavandeira(BaseGazetteSpider):
 
         # O href e uma chamada dum metodo JS. Precisamos do terceiro parametro para continuar.
         # Este parametro tem um sufixo hexadecimal
-        modal_request_params = []
         for line in lines:
             modal_param_match = re.search(
                 r"(@SC_par@\d+?@SC_par@diarioeletronico_grid_cliente@SC_par@.+?)'", line
@@ -66,34 +65,28 @@ class ToLavandeira(BaseGazetteSpider):
                     .get()
                 )
 
-                modal_request_params.append(
-                    {
-                        "modal_param": modal_param_match.group(1),
-                        "doc_date": doc_date,
-                        "doc_edition": doc_edition,
-                    }
+                modal_params = modal_param_match.group(1)
+                item_params = {
+                    "doc_date": doc_date,
+                    "doc_edition": doc_edition,
+                }
+
+                modal_url = (
+                    f"{self.BASE_URL}/diarioeletronico_form_cliente?"
+                    f"&script_case_session={script_case_session}"
+                    f"&nmgp_outra_jan=true"
+                    f"&nmgp_url_saida=modal"
+                    f"&SC_lig_apl_orig=diarioeletronico_grid_cliente"
+                    f"&nmgp_parms={modal_params}"
                 )
 
-        # loop modal_request_params_list
-        modal_params = modal_request_params[0]
+                yield scrapy.Request(
+                    url=modal_url,
+                    callback=self.parse_modal,
+                    cb_kwargs=item_params,
+                )
 
-        modal_url = f"""
-            {self.BASE_URL}/diarioeletronico_form_cliente?
-            &script_case_session={script_case_session}
-            &nmgp_outra_jan=true
-            &nmgp_url_saida=modal
-            &SC_lig_apl_orig=diarioeletronico_grid_cliente
-            &nmgp_parms={modal_params['modal_param']}
-        """
-
-        yield scrapy.Request(
-            url=modal_url,
-            callback=self.parse_modal,
-            cb_kwargs=modal_params,
-        )
-
-    def parse_modal(self, response, doc_date, doc_edition, modal_param):
-        inspect_response(response, self)
+    def parse_modal(self, response, doc_date, doc_edition):
         # Extraia o href do link dentro do <div id="id_sc_loaded_anexo">
         script_case_init = response.xpath(
             '//form[@name="F1"]//input[@name="script_case_init"]/@value'
@@ -110,13 +103,13 @@ class ToLavandeira(BaseGazetteSpider):
 
         is_extra_edition = False
 
-        gazette_url = f"""
-            {self.BASE_URL}/diarioeletronico_form_cliente/diarioeletronico_form_cliente_doc.php?
-            script_case_init={script_case_init}
-            &nm_cod_doc=documento_db
-            &nm_nome_doc={doc_name}
-            &nm_cod_apl=diarioeletronico_form_cliente
-        """
+        gazette_url = (
+            f"{self.BASE_URL}/diarioeletronico_form_cliente/diarioeletronico_form_cliente_doc.php?"
+            f"script_case_init={script_case_init}"
+            f"&nm_cod_doc=documento_db"
+            f"&nm_nome_doc={doc_name}"
+            f"&nm_cod_apl=diarioeletronico_form_cliente"
+        )
 
         yield Gazette(
             date=doc_date,
