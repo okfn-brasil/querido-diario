02a073b95b51bb4e9b1e0d0df92c2e4d829d435f
trevineju <julianabtrevine@gmail.com>
2025-01-12 23:43:46 -0300

    Adiciona exceção caso link para documento ausente

--- data_collection/gazette/spiders/ma/ma_sao_luis.py
+++ data_collection/gazette/spiders/ma/ma_sao_luis.py
@@ -20,18 +20,24 @@ class MASaoLuisSpider(BaseGazetteSpider):
         for item in response.css(".box-publicacao"):
             raw_infos = "".join(item.css("::text").getall()).strip()
 
-            edition_number = re.search(r"(\d+)/", raw_infos).group(1)
-            is_extra_edition = "extra" in raw_infos.lower()
-
             raw_edition_date = re.search(r",(.+)\s", raw_infos).group(1).strip()
             edition_date = dateparser.parse(raw_edition_date, languages=["pt"]).date()
 
-            edition_path = item.css("a")[1].attrib["href"]
-            edition_url = (
-                urlparse(self.start_urls[0])._replace(path=edition_path).geturl()
-            )
-
             if self.start_date <= edition_date <= self.end_date:
+                edition_number = re.search(r"(\d+)/", raw_infos).group(1)
+                is_extra_edition = "extra" in raw_infos.lower()
+
+                try:
+                    edition_path = item.css("a")[1].attrib["href"]
+                    edition_url = (
+                        urlparse(self.start_urls[0])
+                        ._replace(path=edition_path)
+                        .geturl()
+                    )
+                except Exception:
+                    self.logger.error(f"Unable to retrieve PDF URL for {edition_date}.")
+                    continue
+
                 yield Gazette(
                     date=edition_date,
                     edition_number=edition_number,
