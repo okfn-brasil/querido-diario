3c1708e280fca197b3026aeed1b8f001998600d3
Pablo Soares <slpkvol3@gmail.com>
2025-01-17 12:33:53 -0300

    Usa métodos override na base para obter dados da página

--- data_collection/gazette/spiders/base/deto.py
+++ data_collection/gazette/spiders/base/deto.py
@@ -1,9 +1,8 @@
 import datetime
-import math
 import re
-import urllib
 
 import scrapy
+from scrapy import FormRequest
 from scrapy.selector import Selector
 
 from gazette.items import Gazette
@@ -25,8 +24,9 @@ class BaseDetoSpider(BaseGazetteSpider):
       - Os POSTs tem parâmetros no body. Os GETs tem parâmetros no hash
       - Umas retornam HTML, e outros, um JSON com diversos itens, que assumo servirem para gerenciar o estado do JS.
         - No JSON, HTML é retornado no array 'setValue'. Este tem itens com 'field' e 'value'. O item com
-        'field' == 'sc_grid_body' tem a string HTML como 'value'.
-      - O tipo do retorno de cada endpoint muda de acordo com parâmetros e eu não investiguei todas as possibilidades
+        'field' == 'sc_grid_body' tem a string HTML que será substituída no DOM como 'value'.
+      - O tipo do retorno de cada endpoint muda de acordo com parâmetros e eu não investiguei todas as possibilidades.
+        - As relevantes são listadas abaixo
       - 'opc' e 'parm' são os dois parâmetros principais. Seus valores definem o que será retornado pelo backend:
         - opc=muda_qt_linhas, parm=11
           - retorna HTML
@@ -34,8 +34,8 @@ class BaseDetoSpider(BaseGazetteSpider):
           - pode configurar uma quantidade acima do máximo descrito na UI
         - opc=muda_rec_linhas, parm=11
           - carrega uma página de itens a partir do 11º item,
-          - retorna JSON se o Content-Type application/x-www-form-urlencoded
-            - Sem o Header, HTML é retornado mas não obedece a paginação e sempre retorna a primeira página
+          - retorna JSON se o Content-Type for application/x-www-form-urlencoded
+            - Senão, HTML é retornado mas não obedece a paginação e sempre retorna a primeira página
         - opc=rec, parm=11
           - igual à 'muda_rec_linhas'
     """
@@ -48,15 +48,14 @@ class BaseDetoSpider(BaseGazetteSpider):
 
     def start_requests(self):
         data = {
+            "nmgp_parms": "nm_run_menu?#?1?@?nm_apl_menu?#?menu_diarioeletronico?@?script_case_init?#?1",
             "script_case_init": "1",
-            "opc": "muda_qt_linhas",
-            "parm": str(self.page_size),
+            "nm_apl_menu": "menu_diarioeletronico",
         }
 
-        yield scrapy.Request(
+        yield FormRequest(
             url=f"{self.BASE_URL}/diarioeletronico_grid_cliente/",
-            body=urllib.parse.urlencode(data),
-            method="POST",
+            formdata=data,
             callback=self.parse_table,
         )
 
@@ -74,20 +73,13 @@ class BaseDetoSpider(BaseGazetteSpider):
         # As requisições para as próximas paginas retornam json, com html dentro, e este não tem o rodapé.
         # Calculamos a quantidade total de itens e páginas para poder gerenciar o consumo das páginas nós mesmos
         if self.total_pages_count is None:
-            table_foot = response.xpath(
-                "(//table[contains(@class, 'scGridToolbar')]//span)[last()]/text()"
-            ).get()
-
-            if table_foot:
-                items_counter_search = re.search(r"(\d+?)\]$", table_foot)
-                total_items_count = int(items_counter_search.group(1))
-                self.total_pages_count = math.ceil(total_items_count / self.page_size)
+            self.total_pages_count = self.extract_total_items_count(response)
 
         # Este valor é usado em requisições seguintes. Pegamos ele aqui na primeira requisição
-        if self.script_case_session is None:
-            self.script_case_session = response.xpath(
-                '//form[@name="form_ajax_redir_1"]//input[@name="script_case_session"]/@value'
-            ).get()
+        # if self.script_case_session is None:
+        #     self.script_case_session = response.xpath(
+        #         '//form[@name="form_ajax_redir_1"]//input[@name="script_case_session"]/@value'
+        #     ).get()
 
         # Em algumas chamadas (todas além da primeira), o response é um JSON
         # Então, normalizo o objeto response para que se comporte como se este fosse um response derivado de html,
@@ -119,49 +111,49 @@ class BaseDetoSpider(BaseGazetteSpider):
         # Cada linha tem um link cujo href contem parâmetros para abrir o modal
         lines = response.xpath('//tr[starts-with(@id, "SC_ancor")]').getall()
 
-        # O href é uma chamada dum método JS. Precisamos do terceiro parâmetro para passar na próxima requisição.
-        # Este parâmetro tem um sufixo hexadecimal
         for line in lines:
-            modal_param_search = re.search(
-                r"(@SC_par@\d+?@SC_par@diarioeletronico_grid_cliente@SC_par@.+?)'", line
+            # O href é uma chamada dum método JS. Precisamos do terceiro parâmetro para passar na próxima requisição.
+            # Este parâmetro tem um sufixo hexadecimal
+            modal_params = self.extract_modal_params(line)
+
+            if modal_params is None:
+                continue
+
+            # Extrai a data da edição
+            date_str = (
+                Selector(text=line)
+                .xpath('//span[starts-with(@id, "id_sc_field_dataedicao_")]/text()')
+                .get()
+            )
+            doc_date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
+
+            # Extrai o número da edição
+            doc_edition = (
+                Selector(text=line)
+                .xpath('//span[starts-with(@id, "id_sc_field_numeroedicao_")]/text()')
+                .get()
             )
 
-            if modal_param_search:
-                date_str = (
-                    Selector(text=line)
-                    .xpath('//span[starts-with(@id, "id_sc_field_dataedicao_")]/text()')
-                    .get()
-                )
-                doc_date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
+            # Data e número da edição são enviados para o callback
+            item_params = {
+                "doc_date": doc_date,
+                "doc_edition": doc_edition,
+            }
 
-                doc_edition = (
-                    Selector(text=line)
-                    .xpath(
-                        '//span[starts-with(@id, "id_sc_field_numeroedicao_")]/text()'
-                    )
-                    .get()
-                )
+            modal_url = (
+                f"{self.BASE_URL}/diarioeletronico_form_cliente/?"
+                # f"script_case_session={self.script_case_session}&"
+                f"nmgp_outra_jan=true&"
+                f"nmgp_url_saida=modal&"
+                f"SC_lig_apl_orig=diarioeletronico_grid_cliente&"
+                f"nmgp_parms={modal_params}"
+            )
 
-                modal_params = modal_param_search.group(1)
-                item_params = {
-                    "doc_date": doc_date,
-                    "doc_edition": doc_edition,
-                }
-
-                modal_url = (
-                    f"{self.BASE_URL}/diarioeletronico_form_cliente/?"
-                    f"&script_case_session={self.script_case_session}"
-                    f"&nmgp_outra_jan=true"
-                    f"&nmgp_url_saida=modal"
-                    f"&SC_lig_apl_orig=diarioeletronico_grid_cliente"
-                    f"&nmgp_parms={modal_params}"
-                )
-
-                yield scrapy.Request(
-                    url=modal_url,
-                    callback=self.parse_modal_items,
-                    cb_kwargs=item_params,
-                )
+            yield scrapy.Request(
+                url=modal_url,
+                callback=self.parse_modal_items,
+                cb_kwargs=item_params,
+            )
 
     def maybe_request_next_page(self):
         """
@@ -175,7 +167,7 @@ class BaseDetoSpider(BaseGazetteSpider):
         if self.pages_consumed <= self.total_pages_count:
             has_next_page = True
 
-        if self.pages_consumed >= 3:
+        if self.pages_consumed >= 5:
             has_next_page = False
 
         if has_next_page:
@@ -188,12 +180,10 @@ class BaseDetoSpider(BaseGazetteSpider):
                 "parm": str(next_page_start),
             }
 
-            yield scrapy.Request(
+            yield FormRequest(
                 url=f"{self.BASE_URL}/diarioeletronico_grid_cliente/",
-                body=urllib.parse.urlencode(data),
-                method="POST",
+                formdata=data,
                 callback=self.parse_table,
-                headers={"Content-Type": "application/x-www-form-urlencoded"},
                 cb_kwargs={"has_json_response": True},
             )
 
@@ -206,16 +196,23 @@ class BaseDetoSpider(BaseGazetteSpider):
         doc_edition é o número da edição
         """
 
-        # Extraia o href do link dentro do <div id="id_sc_loaded_anexo">
+        # A requisição pode ser inválida apesar de retornar 200 OK
+        if "Dados inválidos" in response.text:
+            self.logger.error(
+                f"'Dados Inválidos' na resposta da requisição para modal da edição {doc_edition}. URL {response.request.url}"
+            )
+            self.logger.error("Abortando.")
+            return
+
+        # Extrai o href do link dentro do <div id="id_sc_loaded_anexo">
         script_case_init = response.xpath(
             '//form[@name="F1"]//input[@name="script_case_init"]/@value'
         ).get()
 
-        self.logger.info(f"form_client response head {response.text[:500]}")
-
         doc_href = response.xpath('//div[@id="id_sc_loaded_anexo"]//a/@href').get()
 
-        # O segundo parâmetro da chamada JavaScript tem o nome do arquivo que será usado na requisição
+        # Este href também é uma chamada JS.
+        # O segundo parâmetro tem o nome do arquivo que será usado na requisição
         doc_name = None
         if doc_href:
             match = re.search(r"'documento_db', '(.+?)'", doc_href)
