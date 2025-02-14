8605df595ef76a6da5727773104315dcf4afbf0e
Yasmin Poleto <37910192+ypoleto@users.noreply.github.com>
2025-02-14 12:35:07 -0300

    Update data_collection/gazette/spiders/pr/pr_arapongas.py
    Co-authored-by: Juliana Trevine <44185775+trevineju@users.noreply.github.com>
    Signed-off-by: Yasmin Poleto <37910192+ypoleto@users.noreply.github.com>

--- data_collection/gazette/spiders/pr/pr_arapongas.py
+++ data_collection/gazette/spiders/pr/pr_arapongas.py
@@ -3,7 +3,7 @@ from datetime import date
 from gazette.spiders.base.atende_v2 import BaseAtendeV2Spider
 
 
-class PrCampoMouraoSpider(BaseAtendeV2Spider):
+class PrArapongasSpider(BaseAtendeV2Spider):
     TERRITORY_ID = "4101507"
     name = "pr_arapongas"
     city_subdomain = "arapongas"
