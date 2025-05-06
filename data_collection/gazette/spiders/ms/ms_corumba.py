d90fd69c5eb682983a0013b99b11b4c5ad318e4d
trevineju <julianabtrevine@gmail.com>
2025-05-06 20:26:02 -0300

    [extra] Complementa base Dionet

--- data_collection/gazette/spiders/ms/ms_corumba.py
+++ data_collection/gazette/spiders/ms/ms_corumba.py
@@ -6,7 +6,6 @@ from gazette.spiders.base.dionet import BaseDionetSpider
 class MsCorumba(BaseDionetSpider):
     TERRITORY_ID = "5003207"
     name = "ms_corumba"
-    allowed_domains = ["do.corumba.ms.gov.br"]
     start_date = date(2012, 6, 26)
 
     BASE_URL = "https://do.corumba.ms.gov.br"
