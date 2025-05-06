b0386f1e7cfcca710ecb1c535ad62e45d6dbce3b
trevineju <julianabtrevine@gmail.com>
2025-05-06 19:58:25 -0300

    [extra] Complementa base DiarioOficialBR

--- data_collection/gazette/spiders/to/to_tocantinia.py
+++ data_collection/gazette/spiders/to/to_tocantinia.py
@@ -6,6 +6,5 @@ from gazette.spiders.base.diariooficialbr import BaseDiarioOficialBRSpider
 class ToTocantiniaSpider(BaseDiarioOficialBRSpider):
     TERRITORY_ID = "1721109"
     name = "to_tocantinia"
-    allowed_domains = ["diariooficialbr.com.br"]
     BASE_URL = "https://tocantinia.diariooficialbr.com.br"
     start_date = date(2017, 3, 21)
