4615b5326bdaa597850e2b2554b2467bb21d21c0
trevineju <julianabtrevine@gmail.com>
2025-05-22 03:30:07 -0300

    Corrige nome de arquivos e de spiders

--- data_collection/gazette/spiders/to/to_lagoa_do_tocantins.py
+++ data_collection/gazette/spiders/to/to_lagoa_do_tocantins.py
@@ -3,8 +3,8 @@ from datetime import date
 from gazette.spiders.base.barcodigital import BaseBarcoDigitalSpider
 
 
-class ToLagoaDeTocatinsSpider(BaseBarcoDigitalSpider):
-    name = "to_lagoa_de_tocantins"
+class ToLagoaDoTocatinsSpider(BaseBarcoDigitalSpider):
+    name = "to_lagoa_do_tocantins"
     TERRITORY_ID = "1711951"
     allowed_domains = ["api-lagoadotocantins.barcodigital.com.br"]
     base_url = "https://api-lagoadotocantins.barcodigital.com.br"
