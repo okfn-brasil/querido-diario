ac0bc91f65dda1b24ae7683ab1172ff3fcec7477
trevineju <julianabtrevine@gmail.com>
2025-05-24 04:23:19 -0300

    Atualiza base DiarioOficialBR

--- data_collection/gazette/spiders/to/to_santa_fe_do_araguaia.py
+++ data_collection/gazette/spiders/to/to_santa_fe_do_araguaia.py
@@ -6,6 +6,5 @@ from gazette.spiders.base.diariooficialbr import BaseDiarioOficialBRSpider
 class ToSantaFedoAraguaiaSpider(BaseDiarioOficialBRSpider):
     TERRITORY_ID = "1718865"
     name = "to_santa_fe_do_araguaia"
-    allowed_domains = ["santafedoaraguaia.diariooficialbr.com.br"]
     BASE_URL = "https://santafedoaraguaia.diariooficialbr.com.br"
     start_date = date(2021, 2, 5)
